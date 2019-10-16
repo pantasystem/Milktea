6f6baa074b449cf89b7c3045c823dd563cc51416
kinoshita0623 <kinoshita062323@gmail.com>
2019-10-17 03:23:00 +0900

    ImagePreviewを雑に実装

--- app/src/main/java/jp/panta/misskeyandroidclient/view/notes/ImagePreviewHelper.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/notes/ImagePreviewHelper.kt
@@ -2,9 +2,12 @@ package jp.panta.misskeyandroidclient.view.notes
 
 import android.databinding.BindingAdapter
 import android.util.Log
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import com.bumptech.glide.Glide
 import jp.panta.misskeyandroidclient.model.files.FileProperty
+import java.lang.IndexOutOfBoundsException
 
 object ImagePreviewHelper {
 
@@ -15,8 +18,34 @@ object ImagePreviewHelper {
         /*if(imagePreviewTopLeft != null){
             Log.d("ImagePreview", "${imagePreviewTopLeft::class.java}")
         }*/
+        Log.d("ImagePreview", "画面サイズ ${this.width}")
+        //this.parent
+        val a = this.parent
 
+        val previews = listOf(imagePreviewTopLeft, imagePreviewTopRight, imagePreviewBottomLeft, imagePreviewBottomRight)
 
+        if(previewFiles.isNullOrEmpty()){
+            previews.forEach{
+                it.visibility = View.GONE
+            }
+            return
+        }
+
+        for(n in 0.until(previews.size)){
+            try{
+                val file = previewFiles[n]
+                val view = previews[n]
+                view.visibility = View.VISIBLE
+                Glide
+                    .with(this.context)
+                    .load(file.thumbnailUrl)
+                    .centerCrop()
+                    .into(view)
+            }catch(e: IndexOutOfBoundsException){
+                previews[n].visibility = View.GONE
+            }
+        }
+        //for(n in 0.until(previewFiles?.size?: 0))
     }
 
 }
