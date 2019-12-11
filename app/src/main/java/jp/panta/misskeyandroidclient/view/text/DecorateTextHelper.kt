c0d2897b2ea5f625637f0cc68010fa8cbb180f31
kinoshita0623 <kinoshita062323@gmail.com>
2019-12-11 19:00:12 +0900

    URL化するコンテンツとそうでないコンテンツで分けれるようにした ハッシュタグは検索が実装できていないので未実装

--- app/src/main/java/jp/panta/misskeyandroidclient/view/text/DecorateTextHelper.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/text/DecorateTextHelper.kt
@@ -10,37 +10,40 @@ import jp.panta.misskeyandroidclient.model.emoji.Emoji
 import java.util.regex.Pattern
 
 object DecorateTextHelper {
-    const val HASH_TAG_PETTERN = """#([^\s.,!?'"#:\/\[\]【】@]+)"""
+    const val HASH_TAG_PETTERN = """#([^\s.,!?'"#:/\[\]【】@]+)"""
     const val MENSTION_PETTERN = """@\w([\w-]*\w)?(?:@[\w.\-]+\w)?"""
-    const val WEB_URL_PATTERN = """http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?"""
+    const val WEB_URL_PATTERN = """(https?|ftp)(://[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#]+)"""
 
     const val SCHEME = "misskey:"
 
     val hashTagPattern = Pattern.compile(HASH_TAG_PETTERN)
-    val mentionPattern = Pattern.compile(MENSTION_PETTERN)
+    val mentionPattern = Pattern.compile(MENSTION_PETTERN, Pattern.MULTILINE)
     val webUrlPattern = Pattern.compile(WEB_URL_PATTERN)
 
-    @BindingAdapter("text", "emojis")
+    @BindingAdapter("text", "emojis", "clickableLink")
     @JvmStatic
-    fun TextView.decorate(text: String?, emojis: List<Emoji>?){
+    fun TextView.decorate(text: String?, emojis: List<Emoji>?, clickableLink: Boolean? = null){
         text?: return
         val span = CustomEmojiDecorator()
             .decorate(emojis, text, this)
         this.text = span
-        decorateLink(this)
+        if(clickableLink == true){
+            decorateLink(this)
+        }
     }
 
     private fun decorateLink(textView: TextView){
-        Linkify.addLinks(textView, mentionPattern, SCHEME, null, Linkify.TransformFilter { match, url ->
+        Linkify.addLinks(textView, mentionPattern, SCHEME, null, Linkify.TransformFilter { _, url ->
             val builder = Uri.Builder()
             builder.authority("user")
             builder.appendQueryParameter("userName", url)
             builder.build().toString()
         })
 
-        Linkify.addLinks(textView, hashTagPattern, SCHEME, null, Linkify.TransformFilter { match, url ->
+        Linkify.addLinks(textView, hashTagPattern, SCHEME, null, Linkify.TransformFilter { _, url ->
             val builder = Uri.Builder()
                 .authority("search")
+                .appendQueryParameter("keyword", url)
             builder.path(url).toString()
         })
 
