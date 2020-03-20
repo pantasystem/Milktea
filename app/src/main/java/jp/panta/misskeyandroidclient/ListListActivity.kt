e28f0cfd38ac715188d2509f8a9530d93003c32c
kinoshita <kinoshita062323@gmail.com>
2020-03-21 00:07:20 +0900

    List一覧用のActivityとそのレイアウトをを追加

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/ListListActivity.kt
@@ -0,0 +1,12 @@
+package jp.panta.misskeyandroidclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_list)
+    }
+}
