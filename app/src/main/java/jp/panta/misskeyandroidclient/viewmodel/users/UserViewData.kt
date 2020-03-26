1af477614d9d1fabedf22eb64c5769fe78313ac6
kinoshita <kinoshita062323@gmail.com>
2020-03-27 01:27:22 +0900

    UserのViewDataを追加

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/users/UserViewData.kt
@@ -0,0 +1,53 @@
+package jp.panta.misskeyandroidclient.viewmodel.users
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import jp.panta.misskeyandroidclient.model.api.MisskeyAPI
+import jp.panta.misskeyandroidclient.model.users.RequestUser
+import jp.panta.misskeyandroidclient.model.users.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class UserViewData{
+
+    val userId: String
+    val user: MutableLiveData<User?> = object : MutableLiveData<User?>(){
+        override fun onActive() {
+            super.onActive()
+            val userField = value
+            if(userField == null){
+                api?.invoke()?.enqueue(accept)
+            }
+        }
+
+    }
+
+
+    constructor(userId: String){
+        this.userId = userId
+    }
+
+    constructor(user: User): this(user.id){
+        this.user.postValue(user)
+    }
+
+    val accept = object : Callback<User>{
+        override fun onResponse(call: Call<User>, response: Response<User>) {
+            user.postValue(response.body())
+        }
+
+        override fun onFailure(call: Call<User>, t: Throwable) {
+            Log.e("UserViewData", "user load error", t)
+        }
+    }
+
+
+    var api: (()-> Call<User>)? = null
+
+    fun setApi(i: String, misskeyAPI: MisskeyAPI){
+        api = {
+            misskeyAPI.showUser(RequestUser(i = i, userId = userId))
+        }
+    }
+}
