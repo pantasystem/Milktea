97682dd3955febcf9fd4877a0197775f13991178
kinoshita <kinoshita062323@gmail.com>
2020-03-28 04:57:57 +0900

    Chipを表示するためのAdapterを追加

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/users/UserChipListAdapter.kt
@@ -0,0 +1,42 @@
+package jp.panta.misskeyandroidclient.view.users
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import jp.panta.misskeyandroidclient.R
+import jp.panta.misskeyandroidclient.databinding.ItemUserChipBinding
+import jp.panta.misskeyandroidclient.viewmodel.users.UserViewData
+
+class UserChipListAdapter(val lifecycleOwner: LifecycleOwner) : ListAdapter<UserViewData, UserChipListAdapter.VH>(Diff()){
+    class VH(val binding: ItemUserChipBinding) : RecyclerView.ViewHolder(binding.root)
+    class Diff : DiffUtil.ItemCallback<UserViewData>(){
+        override fun areContentsTheSame(oldItem: UserViewData, newItem: UserViewData): Boolean {
+            return oldItem.userId == newItem.userId && oldItem.user.value == newItem.user.value
+        }
+
+        override fun areItemsTheSame(oldItem: UserViewData, newItem: UserViewData): Boolean {
+            return oldItem.userId == newItem.userId
+        }
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.binding.userViewData = getItem(position)
+        holder.binding.lifecycleOwner = lifecycleOwner
+        holder.binding.executePendingBindings()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        return VH(
+            DataBindingUtil.inflate<ItemUserChipBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.item_user_chip,
+                parent,
+                false
+            )
+        )
+    }
+}
