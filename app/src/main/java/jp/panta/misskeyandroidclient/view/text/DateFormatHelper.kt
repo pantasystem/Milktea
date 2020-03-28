f20b72176dbcadb6a675d1a48d2b7c1b5a975333
kinoshita <kinoshita062323@gmail.com>
2020-03-28 12:46:38 +0900

    ノートの投稿経過時間を表示

--- app/src/main/java/jp/panta/misskeyandroidclient/view/text/DateFormatHelper.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/text/DateFormatHelper.kt
@@ -3,6 +3,8 @@ package jp.panta.misskeyandroidclient.view.text
 import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import jp.panta.misskeyandroidclient.R
+import jp.panta.misskeyandroidclient.view.SimpleElapsedTime
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -22,5 +24,24 @@ object DateFormatHelper {
         val date = timeOnly?: Date()
         val sdf = SimpleDateFormat("HH:mm", Locale.getDefault())
         this.text = sdf.format(date)
+    }
+
+    @BindingAdapter("elapsedTime")
+    @JvmStatic
+    fun TextView.setElapsedTime(elapsedTime: Date?){
+        val date = elapsedTime?: Date()
+        val simpleElapsedTime = SimpleElapsedTime{
+            when(it){
+                SimpleElapsedTime.TimeUnit.YEAR -> context.getString(R.string.year_ago)
+                SimpleElapsedTime.TimeUnit.MONTH -> context.getString(R.string.month_ago)
+                SimpleElapsedTime.TimeUnit.DATE -> context.getString(R.string.date_ago)
+                SimpleElapsedTime.TimeUnit.HOUR -> context.getString(R.string.minute_ago)
+                SimpleElapsedTime.TimeUnit.MINUTE -> context.getString(R.string.minute_ago)
+                SimpleElapsedTime.TimeUnit.SECOND -> context.getString(R.string.second_ago)
+                SimpleElapsedTime.TimeUnit.NOW -> context.getString(R.string.now)
+                SimpleElapsedTime.TimeUnit.FUTURE -> context.getString(R.string.future)
+            }
+        }
+        this.text = simpleElapsedTime.format(date)
     }
 }
