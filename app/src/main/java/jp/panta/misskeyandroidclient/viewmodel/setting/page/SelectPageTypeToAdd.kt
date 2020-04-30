18b73a2dbd8795208b3fdc8103c8fd8c48233c9b
kinoshita0623 <kuutarousii@gmail.com>
2020-05-01 00:08:49 +0900

    ページを加えるためのインターフェースを追加

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/setting/page/SelectPageTypeToAdd.kt
@@ -0,0 +1,8 @@
+package jp.panta.misskeyandroidclient.viewmodel.setting.page
+
+import jp.panta.misskeyandroidclient.model.PageType
+
+interface SelectPageTypeToAdd {
+
+    fun add(type: PageType)
+}
