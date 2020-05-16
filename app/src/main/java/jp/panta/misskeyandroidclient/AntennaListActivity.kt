616c32d37c2c7d1a170d188ba8ff0df2b1952ca9
kinoshita0623 <kuutarousii@gmail.com>
2020-05-16 21:12:43 +0900

    upボタンを実装した

--- app/src/main/java/jp/panta/misskeyandroidclient/AntennaListActivity.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/AntennaListActivity.kt
@@ -2,11 +2,22 @@ package jp.panta.misskeyandroidclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_antenna_list.*
 
 class AntennaListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_antenna_list)
+        setSupportActionBar(antennaListToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
