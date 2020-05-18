8b537940dee9c24c36339837632f4339d7493193
kinoshita0623 <kuutarousii@gmail.com>
2020-05-19 03:01:18 +0900

    AntennaEditor用のFragmentを追加した それに伴い入力用のstyleを追加した

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/antenna/AntennaEditorFragment.kt
@@ -0,0 +1,14 @@
+package jp.panta.misskeyandroidclient.view.antenna
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import jp.panta.misskeyandroidclient.R
+
+class AntennaEditorFragment : Fragment(R.layout.fragment_antenna_editor){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
