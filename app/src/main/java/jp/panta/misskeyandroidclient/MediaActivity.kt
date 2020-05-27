ef51a0149ed53eddff5cdfb9c9e7848e18648d4d
kinoshita <kinoshita062323@gmail.com>
2020-05-27 21:58:12 +0900

    メディア表示画面のUPボタンが正常に実装されていないのを修正した fix #7

--- app/src/main/java/jp/panta/misskeyandroidclient/MediaActivity.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/MediaActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.webkit.MimeTypeMap
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentPagerAdapter
@@ -94,6 +95,13 @@ class MediaActivity : AppCompatActivity() {
         mediaViewPager.adapter = pagerAdapter
 
         mediaViewPager.currentItem = filePropertyListCurrentIndex
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
