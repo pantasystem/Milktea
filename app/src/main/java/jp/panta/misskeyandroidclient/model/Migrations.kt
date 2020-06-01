2f715baf7dd57eb8e45a11ac346cc3c19f903b3c
kinoshita0623 <kuutarousii@gmail.com>
2020-06-01 20:50:39 +0900

    適当にMigration修正した 多分動かない

--- app/src/main/java/jp/panta/misskeyandroidclient/model/Migrations.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/Migrations.kt
@@ -30,8 +30,8 @@ val MIGRATION_36_37 = object : Migration(36, 37){
 val MIGRATION_1_2 = object : Migration(1, 2){
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("create table 'draft_note'('draft_note_id' INTEGER primary key autoincrement,'accountId' TEXT not null, 'visibility' TEXT not null, 'text' TEXT, 'cw' TEXT, 'viaMobile' INTEGER, 'localOnly' INTEGER, 'noExtractMentions' INTEGER, 'noExtractHashtags' INTEGER, 'noExtractEmojis' INTEGER, 'replyId' TEXT, 'renoteId' TEXT, 'expiresAt' INTEGER, 'multiple' INTEGER)")
-        database.execSQL("create table 'user_id'('userId' TEXT not null, 'draft_note_id' INTEGER not null, foreign key('userId') references'draft_note'('draft_note_id') on delete cascade on update no action, primary key('userId', 'draft_note_id'))")
-        database.execSQL("create table 'poll_choice'('choice' TEXT not null, 'weight' INTEGER not null, 'draft_note_id' INTEGER not null, foreign key('draft_note_id') references 'draft_note'('draft_note_id') on delete cascade on update no action, primary key('choice', 'weight', 'draft_note_id'))")
+        database.execSQL("create table 'user_id'('userId' TEXT not null, 'draft_note_id' INTEGER not null, foreign key('userId') references'draft_note'('draft_note_id') on delete cascade on update cascade, primary key('userId', 'draft_note_id'))")
+        database.execSQL("create table 'poll_choice'('choice' TEXT not null, 'weight' INTEGER not null, 'draft_note_id' INTEGER not null, foreign key('draft_note_id') references 'draft_note'('draft_note_id') on delete cascade on update cascade, primary key('choice', 'weight', 'draft_note_id'))")
         database.execSQL("create table 'draft_file'('remote_file_id' TEXT, 'file_path' TEXT, 'draft_note_id' INTEGER not null, 'file_id' INTEGER not null primary key autoincrement)")
     }
 }
