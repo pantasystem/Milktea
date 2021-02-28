1aa26eb8665b2135056e67e24d07c99fb569a63b
kinoshita <kinoshita062323@gmail.com>
2021-03-01 08:14:15 +0900

    UserRepositoryのイベントをFlowに変換するクラスを追加した

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/users/UserRepositoryEventToFlow.kt
@@ -0,0 +1,59 @@
+package jp.panta.misskeyandroidclient.model.users
+
+import jp.panta.misskeyandroidclient.model.notes.NoteRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlin.reflect.KClass
+
+/**
+ * UserRepositoryのイベントをFlowに変換する
+ */
+class UserRepositoryEventToFlow : UserRepository.Listener{
+
+    private val userIdWithListener = mutableMapOf<User.Id, MutableSet<(e: UserRepository.Event)->Unit>>()
+
+    @ExperimentalCoroutinesApi
+    fun from(userId: User.Id): Flow<UserRepository.Event> {
+        return channelFlow {
+            val callback: (UserRepository.Event)-> Unit = { ev ->
+                offer(ev)
+            }
+
+            listen(userId, callback)
+            awaitClose {
+                unListen(userId, callback)
+            }
+        }
+    }
+
+    private fun listen(userId: User.Id, callback: (e: UserRepository.Event)->Unit) {
+        synchronized(userIdWithListener) {
+            val listeners = userIdWithListener[userId]
+                ?: mutableSetOf()
+
+            listeners.add(callback)
+            userIdWithListener[userId] = listeners
+        }
+    }
+
+    private fun unListen(userId: User.Id, callback: (e: UserRepository.Event)->Unit) {
+        synchronized(userIdWithListener) {
+            val listeners = userIdWithListener[userId]
+            listeners?.remove(callback)
+            listeners?.let{
+                userIdWithListener[userId] = listeners
+            }
+        }
+    }
+
+    override fun on(e: UserRepository.Event) {
+        synchronized(userIdWithListener) {
+            userIdWithListener[e.userId]?.forEach {
+                it.invoke(e)
+            }
+        }
+    }
+
+}
