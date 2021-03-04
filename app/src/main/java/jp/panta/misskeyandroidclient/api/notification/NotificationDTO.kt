6f774ba25441b108d294c6e4a8222a9dde5b62f0
kinoshita <kinoshita062323@gmail.com>
2021-03-05 03:52:27 +0900

    Kotlin serializationに対応した

--- app/src/main/java/jp/panta/misskeyandroidclient/api/notification/NotificationDTO.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/api/notification/NotificationDTO.kt
@@ -2,14 +2,17 @@ package jp.panta.misskeyandroidclient.api.notification
 
 import jp.panta.misskeyandroidclient.api.notes.NoteDTO
 import jp.panta.misskeyandroidclient.api.users.UserDTO
+import jp.panta.misskeyandroidclient.serializations.DateSerializer
+import kotlinx.serialization.Serializable
 import java.util.*
 
+@Serializable
 data class NotificationDTO(
     val id: String,
-    val createdAt: Date,
+    @kotlinx.serialization.Serializable(with = DateSerializer::class) val createdAt: Date,
     val type: String,
     val userId: String,
     val user: UserDTO,
-    val note: NoteDTO?,
-    val reaction: String?
+    val note: NoteDTO? = null,
+    val reaction: String? = null
 )
