8c3cdfbb4d88eccecd73f8a58ab85478b0944d14
kinoshita <kinoshita062323@gmail.com>
2021-03-17 09:41:55 +0900

    UserDTOへの依存を無くした

--- app/src/main/java/jp/panta/misskeyandroidclient/SearchAndSelectUserActivity.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/SearchAndSelectUserActivity.kt
@@ -1,36 +1,41 @@
 package jp.panta.misskeyandroidclient
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import jp.panta.misskeyandroidclient.databinding.ActivitySearchAndSelectUserBinding
+import jp.panta.misskeyandroidclient.model.users.User
 import jp.panta.misskeyandroidclient.view.users.selectable.SelectableUsersAdapter
 import jp.panta.misskeyandroidclient.viewmodel.MiCore
 import jp.panta.misskeyandroidclient.viewmodel.users.search.SearchUserViewModel
 import jp.panta.misskeyandroidclient.viewmodel.users.selectable.SelectedUserViewModel
-import kotlinx.android.synthetic.main.activity_search_and_select_user.*
+import java.io.Serializable
 
 class SearchAndSelectUserActivity : AppCompatActivity() {
 
     companion object{
-        const val EXTRA_SELECTABLE_MAXIMUM_SIZE = "jp.panta.misskeyandroidclient.EXTRA_SELECTABLE_MAXIMUM_SIZE"
-        const val EXTRA_SELECTED_USER_IDS = "jp.panta.misskeyandroidclient.EXTRA_SELECTED_USER_IDS"
-        const val EXTRA_SELECTED_USERS = "jp.panta.misskeyandroidclient.EXTRA_SELECTED_USERS"
+        private const val EXTRA_SELECTABLE_MAXIMUM_SIZE = "jp.panta.misskeyandroidclient.EXTRA_SELECTABLE_MAXIMUM_SIZE"
+        private const val EXTRA_SELECTED_USER_IDS = "jp.panta.misskeyandroidclient.EXTRA_SELECTED_USER_IDS"
 
-        const val EXTRA_ADDED_USER_IDS = "jp.panta.misskeyandroidclient.EXTRA_ADDED_USER_IDS"
-        const val EXTRA_REMOVED_USER_IDS = "jp.panta.misskeyandroidclient.EXTRA_REMOVED_USER_IDS"
+        const val EXTRA_SELECTED_USER_CHANGED_DIFF = "jp.panta.misskeyandroidclient.EXTRA_SELECTED_USER_CHANGED_DIFF"
+
+        fun newIntent(context: Context, selectableMaximumSize: Int = Int.MAX_VALUE, selectedUserIds: List<User.Id> = emptyList()) : Intent{
+            return Intent(context, SearchAndSelectUserActivity::class.java).apply {
+                putExtra(EXTRA_SELECTABLE_MAXIMUM_SIZE, selectableMaximumSize)
+                putExtra(EXTRA_SELECTED_USER_IDS, ArrayList<Serializable>(selectedUserIds))
+            }
+        }
     }
 
-    private var mSelectedUserIds: List<String>? = null
+    private var mSelectedUserIds: List<User.Id>? = null
 
     private var mSelectedUserViewModel: SelectedUserViewModel? = null
 
@@ -46,7 +51,10 @@ class SearchAndSelectUserActivity : AppCompatActivity() {
         BottomSheetBehavior.from(activitySearchAndSelectUserBinding.selectedUsersView.selectedUsersBottomSheet)
 
         val selectableMaximumSize = intent.getIntExtra(EXTRA_SELECTABLE_MAXIMUM_SIZE, Int.MAX_VALUE)
-        val selectedUserIdList = intent.getStringArrayExtra(EXTRA_SELECTED_USER_IDS)?.toList()?: emptyList()
+        val selectedUserIdList =
+            (intent.getSerializableExtra(EXTRA_SELECTED_USER_IDS) as? ArrayList<*>)?.mapNotNull {
+                it as? User.Id
+            }
         mSelectedUserIds = selectedUserIdList
         Log.d(this.localClassName, "selected user ids :$selectedUserIdList")
 
@@ -72,11 +80,11 @@ class SearchAndSelectUserActivity : AppCompatActivity() {
         activitySearchAndSelectUserBinding.selectedUsersView.selectedUsersView.adapter = selectedUsersAdapter
         activitySearchAndSelectUserBinding.selectedUsersView.selectedUsersView.layoutManager = LinearLayoutManager(this)
 
-        searchUserViewModel.getUsers().observe(this, Observer {
+        searchUserViewModel.getUsers().observe(this, {
             selectableUsersAdapter.submitList(it)
         })
 
-        selectedUserViewModel.selectedUsers.observe(this, Observer {
+        selectedUserViewModel.selectedUsers.observe(this, {
             selectedUsersAdapter.submitList(it)
         })
         mSelectedUserViewModel = selectedUserViewModel
@@ -91,17 +99,12 @@ class SearchAndSelectUserActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        //super.onBackPressed()
-
         setResultFinish()
     }
 
     private fun setResultFinish(){
         val selectedDiff = mSelectedUserViewModel?.getSelectedUserIdsChangedDiff()
-        val selectedUsers =
-            (mSelectedUserViewModel?.selectedUsers?.value ?: emptyList()).mapNotNull {
-                it.user.value
-            }
+
         if(selectedDiff == null){
             setResult(Activity.RESULT_CANCELED)
             finish()
@@ -110,10 +113,7 @@ class SearchAndSelectUserActivity : AppCompatActivity() {
         val intent = Intent()
 
         Log.d("SearchAndSelectAC", "新たに追加:${selectedDiff.added}, 削除:${selectedDiff.removed}")
-        intent.putExtra(EXTRA_SELECTED_USER_IDS, selectedDiff.selected.toTypedArray())
-        intent.putExtra(EXTRA_ADDED_USER_IDS, selectedDiff.added.toTypedArray())
-        intent.putExtra(EXTRA_REMOVED_USER_IDS, selectedDiff.removed.toTypedArray())
-        intent.putExtra(EXTRA_SELECTED_USERS, ArrayList(selectedUsers))
+        intent.putExtra(EXTRA_SELECTED_USER_CHANGED_DIFF, selectedDiff)
         setResult(RESULT_OK, intent)
         finish()
 
