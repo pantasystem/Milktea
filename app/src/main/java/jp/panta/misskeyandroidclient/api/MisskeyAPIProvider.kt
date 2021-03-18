99ab436804e6e4b3baf032a41dc9af97e8b39aed
kinoshita <kinoshita062323@gmail.com>
2021-03-19 00:31:11 +0900

    APIをいい感じに生成して管理してくれるクラスを作成した

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/api/MisskeyAPIProvider.kt
@@ -0,0 +1,30 @@
+package jp.panta.misskeyandroidclient.api
+
+
+import jp.panta.misskeyandroidclient.model.api.MisskeyAPI
+import jp.panta.misskeyandroidclient.model.api.Version
+
+/**
+ * MisskeyAPIとBaseURLとVersionをいい感じに管理する
+ */
+class MisskeyAPIProvider {
+
+
+    private val baseURLAndMisskeyAPI = mutableMapOf<String, MisskeyAPI>()
+    private val baseURLAndVersion = mutableMapOf<String, Version?>()
+
+    fun get(baseURL: String, version: Version? = null): MisskeyAPI {
+        synchronized(baseURLAndMisskeyAPI) {
+            var api = baseURLAndMisskeyAPI[baseURL]
+
+            // NOTE BaseURLに対応するインスタンスが生成されていない＆＆鯖のバージョンに対応するインスタンスが生成されていなければ生成する
+            if(api == null) {
+                api = if(version == null) MisskeyAPIServiceBuilder.build(baseURL) else MisskeyAPIServiceBuilder.build(baseURL, version)
+            }else if((baseURLAndVersion[baseURL] == null || baseURLAndVersion[baseURL] != version) && version != null) {
+                api = MisskeyAPIServiceBuilder.build(baseURL, version)
+            }
+            baseURLAndMisskeyAPI[baseURL] = api
+            return api
+        }
+    }
+}
