a0c7ff31213aa2bb721d8f24e8638a1112869796
kinoshita <kinoshita062323@gmail.com>
2021-03-19 04:04:47 +0900

    エラーを修正した

--- app/src/main/java/jp/panta/misskeyandroidclient/FollowFollowerActivity.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/FollowFollowerActivity.kt
@@ -1,24 +1,39 @@
 package jp.panta.misskeyandroidclient
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentPagerAdapter
-import jp.panta.misskeyandroidclient.api.users.UserDTO
+import androidx.lifecycle.ViewModelProvider
+import jp.panta.misskeyandroidclient.model.users.User
+import jp.panta.misskeyandroidclient.view.TitleSettable
 import jp.panta.misskeyandroidclient.view.users.FollowFollowerFragment
 import jp.panta.misskeyandroidclient.viewmodel.users.FollowFollowerViewModel
+import jp.panta.misskeyandroidclient.viewmodel.users.UserDetailViewModel
+import jp.panta.misskeyandroidclient.viewmodel.users.UserDetailViewModelFactory
 import kotlinx.android.synthetic.main.activity_follow_follower.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-class FollowFollowerActivity : AppCompatActivity() {
+class FollowFollowerActivity : AppCompatActivity(), TitleSettable {
 
     companion object{
-        const val EXTRA_USER = "jp.panta.misskeyandroidclient.FollowFollowerActivity.EXTRA_USER"
-        const val EXTRA_VIEW_CURRENT = "jp.panta.misskeyandroidclient.FollowFollowerActivity.EXTRA_VIEW_CURRENT"
-        const val FOLLOWING_VIEW_MODE = 0
-        const val FOLLOWER_VIEW_MODE = 1
+        private const val EXTRA_USER_ID = "jp.panta.misskeyandroidclient.FollowFollowerActivity.EXTRA_USER_ID"
+        private const val EXTRA_VIEW_CURRENT = "jp.panta.misskeyandroidclient.FollowFollowerActivity.EXTRA_VIEW_CURRENT"
+        private const val FOLLOWING_VIEW_MODE = 0
+        private const val FOLLOWER_VIEW_MODE = 1
+
+        fun newIntent(context: Context, userId: User.Id, isFollowing: Boolean): Intent {
+            return Intent(context, FollowFollowerActivity::class.java).apply {
+                putExtra(EXTRA_USER_ID, userId)
+                putExtra(EXTRA_VIEW_CURRENT, if(isFollowing) FOLLOWING_VIEW_MODE else FOLLOWER_VIEW_MODE)
+            }
+        }
     }
 
+    @ExperimentalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme()
@@ -26,14 +41,24 @@ class FollowFollowerActivity : AppCompatActivity() {
         setSupportActionBar(follow_follower_toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val user = intent.getSerializableExtra(EXTRA_USER) as UserDTO?
+        //val user = intent.getSerializableExtra(EXTRA_USER) as UserDTO?
+        val userId = intent.getSerializableExtra(EXTRA_USER_ID) as User.Id
 
-        follow_follower_pager.adapter = FollowFollowerPagerAdapter(user)
+        val miApplication = application as MiApplication
+        val userDetailViewModel = ViewModelProvider(this, UserDetailViewModelFactory(miApplication, userId, null))[UserDetailViewModel::class.java]
+        userDetailViewModel.user.observe(this) {
+            setTitle(it.getDisplayName())
+        }
+
+        follow_follower_pager.adapter = FollowFollowerPagerAdapter(userId)
         follow_follower_tab.setupWithViewPager(follow_follower_pager)
         follow_follower_pager.currentItem = intent.getIntExtra(EXTRA_VIEW_CURRENT, FOLLOWER_VIEW_MODE)
 
-        supportActionBar?.title = user?.getDisplayUserName()
 
+    }
+
+    override fun setTitle(text: String) {
+        supportActionBar?.title = text
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -44,7 +69,7 @@ class FollowFollowerActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    inner class FollowFollowerPagerAdapter(val user: UserDTO?) : FragmentPagerAdapter(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
+    inner class FollowFollowerPagerAdapter(val userId: User.Id) : FragmentPagerAdapter(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
 
         private val titleList = arrayOf(getString(R.string.following), getString(R.string.follower))
 
@@ -58,9 +83,9 @@ class FollowFollowerActivity : AppCompatActivity() {
 
         override fun getItem(position: Int): Fragment {
             return if(position == 0){
-                FollowFollowerFragment.newInstance(FollowFollowerViewModel.Type.FOLLOWING, user)
+                FollowFollowerFragment.newInstance(FollowFollowerViewModel.Type.FOLLOWING, userId)
             }else{
-                FollowFollowerFragment.newInstance(FollowFollowerViewModel.Type.FOLLOWER, user)
+                FollowFollowerFragment.newInstance(FollowFollowerViewModel.Type.FOLLOWER, userId)
             }
         }
     }
--- app/src/main/java/jp/panta/misskeyandroidclient/view/users/FollowFollowerFragment.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/users/FollowFollowerFragment.kt
@@ -9,22 +9,23 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import jp.panta.misskeyandroidclient.*
-import jp.panta.misskeyandroidclient.api.users.UserDTO
+import jp.panta.misskeyandroidclient.model.users.User
 import jp.panta.misskeyandroidclient.viewmodel.users.FollowFollowerViewModel
 import jp.panta.misskeyandroidclient.viewmodel.users.ToggleFollowViewModel
+import jp.panta.misskeyandroidclient.viewmodel.users.UserDetailViewModel
+import jp.panta.misskeyandroidclient.viewmodel.users.UserDetailViewModelFactory
 import kotlinx.android.synthetic.main.fragment_follow_follwer.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class FollowFollowerFragment : Fragment(R.layout.fragment_follow_follwer){
 
     companion object{
-        private const val EXTRA_USER = "jp.panta.misskeyandroidclient.view.users.FollowFollowerFragment.EXTRA_USER"
+        private const val EXTRA_USER_ID = "jp.panta.misskeyandroidclient.view.users.FollowFollowerFragment.EXTRA_USER_ID"
         private const val EXTRA_TYPE = "jp.panta.misskeyandroidclient.view.users.FollowFollowerFragment.EXTRA_TYPE"
-        fun newInstance(type: FollowFollowerViewModel.Type, user: UserDTO? = null) : FollowFollowerFragment{
+        fun newInstance(type: FollowFollowerViewModel.Type, userId: User.Id) : FollowFollowerFragment{
             return FollowFollowerFragment().apply{
                 arguments = Bundle().apply{
-                    if(user != null){
-                        putSerializable(EXTRA_USER, user)
-                    }
+                    putSerializable(EXTRA_USER_ID, userId)
                     putInt(EXTRA_TYPE, type.ordinal)
                 }
             }
@@ -35,12 +36,14 @@ class FollowFollowerFragment : Fragment(R.layout.fragment_follow_follwer){
     private var mViewModel: FollowFollowerViewModel? = null
     private lateinit var mLinearLayoutManager: LinearLayoutManager
 
+    @ExperimentalCoroutinesApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val typeOrdinal = arguments?.getInt(EXTRA_TYPE)?: 0
 
         val type = FollowFollowerViewModel.Type.values()[typeOrdinal]
-        val user = arguments?.getSerializable(EXTRA_USER) as UserDTO?
+        //val user = arguments?.getSerializable(EXTRA_USER) as UserDTO?
+        val userId = arguments?.getSerializable(EXTRA_USER_ID) as User.Id
 
         mLinearLayoutManager = LinearLayoutManager(view.context)
 
@@ -49,36 +52,33 @@ class FollowFollowerFragment : Fragment(R.layout.fragment_follow_follwer){
 
 
         val miApplication = context?.applicationContext as MiApplication
-        miApplication.getCurrentAccount().observe(viewLifecycleOwner, { accountRelation ->
-            val encryption = miApplication.getEncryption()
-            val followFollowerViewModel = ViewModelProvider(this, FollowFollowerViewModel.Factory(accountRelation, miApplication, user, type, encryption))[FollowFollowerViewModel::class.java]
-            mViewModel = followFollowerViewModel
-            val adapter = FollowableUserListAdapter(
-                viewLifecycleOwner, followFollowerViewModel, ViewModelProvider(this, ToggleFollowViewModel.Factory(miApplication))[ToggleFollowViewModel::class.java]
-            )
 
-            follow_follower_list.adapter = adapter
-
-            followFollowerViewModel.users.observe(viewLifecycleOwner,  {
-                adapter.submitList(it)
-            })
-
-            followFollowerViewModel.loadInit()
-
-            mViewModel?.isInitializing?.observe(viewLifecycleOwner,  {
-                swipe_refresh.isRefreshing = it
-            })
-
-            mViewModel?.showUserEventBus?.observe(viewLifecycleOwner,  {
-                val intent = Intent(activity, UserDetailActivity::class.java)
-                intent.putActivity(Activities.ACTIVITY_IN_APP)
+        val followFollowerViewModel = ViewModelProvider(this, FollowFollowerViewModel.Factory(userId, type, miApplication))[FollowFollowerViewModel::class.java]
+        mViewModel = followFollowerViewModel
 
 
-                intent.putExtra(UserDetailActivity.EXTRA_USER_ID, it.id)
-                activity?.startActivity(intent)
-            })
+        val adapter = FollowableUserListAdapter(
+            viewLifecycleOwner, followFollowerViewModel, ViewModelProvider(this, ToggleFollowViewModel.Factory(miApplication))[ToggleFollowViewModel::class.java]
+        )
+
+        follow_follower_list.adapter = adapter
+
+        followFollowerViewModel.users.observe(viewLifecycleOwner,  {
+            adapter.submitList(it)
         })
 
+        followFollowerViewModel.loadInit()
+
+        mViewModel?.isInitializing?.observe(viewLifecycleOwner,  {
+            swipe_refresh.isRefreshing = it
+        })
+
+        mViewModel?.showUserEventBus?.observe(viewLifecycleOwner,  {
+            val intent = UserDetailActivity.newInstance(requireActivity(), userId = it)
+            intent.putActivity(Activities.ACTIVITY_IN_APP)
+
+            requireActivity().startActivity(intent)
+        })
 
         swipe_refresh.setOnRefreshListener {
             mViewModel?.loadInit()
