053802ef579097606b0c9d5b62679d4a539c4234
kinoshita <kinoshita062323@gmail.com>
2021-03-19 04:29:28 +0900

    Factoryは使われていなかったので削除した

--- app/src/main/java/jp/panta/misskeyandroidclient/api/messaging/MessageAction.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/api/messaging/MessageAction.kt
@@ -13,39 +13,4 @@ data class MessageAction(
     val text: String?,
     val fileId: String?,
     val messageId: String?
-): JavaSerializable{
-    class Factory(val account: Account, val message: MessageDTO){
-        fun actionCreateMessage(text: String?, fileId: String?, encryption: Encryption): MessageAction {
-            return MessageAction(
-                account.getI(encryption),
-                if(message.isGroup()) null else message.opponentUser(account)?.id,
-                message.group?.id,
-                text,
-                fileId,
-            null
-            )
-        }
-
-        fun actionDeleteMessage(message: MessageDTO, encryption: Encryption): MessageAction {
-            return MessageAction(
-                account.getI(encryption),
-                null,
-                null,
-                null,
-                null,
-                message.id
-            )
-        }
-
-        fun actionRead(message: MessageDTO, encryption: Encryption): MessageAction {
-            return MessageAction(
-                account.getI(encryption),
-                null,
-                null,
-                null,
-                null,
-                message.id
-            )
-        }
-    }
-}
+): JavaSerializable
