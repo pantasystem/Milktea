6850296de665a513af65fadeb23664f6379e37af
kinoshita <kinoshita062323@gmail.com>
2021-03-19 00:46:51 +0900

    MetaをメモリとDB両方から管理できるようにした。

--- app/src/main/java/jp/panta/misskeyandroidclient/model/instance/db/MediatorMetaRepository.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/instance/db/MediatorMetaRepository.kt
@@ -4,31 +4,31 @@ import jp.panta.misskeyandroidclient.model.instance.Meta
 import jp.panta.misskeyandroidclient.model.instance.MetaRepository
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import java.util.concurrent.ConcurrentHashMap
 
-class InMemoryMetaRepository : MetaRepository{
+class MediatorMetaRepository(
+    private val roomMetaRepository: RoomMetaRepository,
+    private val inMemoryMetaRepository: InMemoryMetaRepository,
+) : MetaRepository{
 
-    private val instanceDomainAndMeta = ConcurrentHashMap<String, Meta>()
     private val lock = Mutex()
 
     override suspend fun add(meta: Meta): Meta {
         lock.withLock {
-            instanceDomainAndMeta[meta.uri] = meta
-            return meta
+            return inMemoryMetaRepository.add(roomMetaRepository.add(meta))
         }
-
-
     }
 
     override suspend fun delete(meta: Meta) {
         lock.withLock {
-            instanceDomainAndMeta.remove(meta.uri)
+            inMemoryMetaRepository.delete(meta)
+            roomMetaRepository.delete(meta)
         }
     }
 
     override suspend fun get(instanceDomain: String): Meta? {
         lock.withLock {
-            return instanceDomainAndMeta[instanceDomain]
+            return inMemoryMetaRepository.get(instanceDomain)?: roomMetaRepository.get(instanceDomain)
         }
     }
+
 }
