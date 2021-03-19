9d86abdc16e21e5a9e35b5a7e3ca9178d4bf1310
kinoshita <kinoshita062323@gmail.com>
2021-03-20 02:31:45 +0900

    エラーを修正した

--- app/src/main/java/jp/panta/misskeyandroidclient/UserListDetailActivity.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/UserListDetailActivity.kt
@@ -1,5 +1,6 @@
 package jp.panta.misskeyandroidclient
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,7 +15,8 @@ import androidx.lifecycle.ViewModelProvider
 import jp.panta.misskeyandroidclient.model.account.page.Page
 import jp.panta.misskeyandroidclient.model.account.Account
 import jp.panta.misskeyandroidclient.model.account.page.Pageable
-import jp.panta.misskeyandroidclient.api.list.UserList
+import jp.panta.misskeyandroidclient.api.list.UserListDTO
+import jp.panta.misskeyandroidclient.model.list.UserList
 import jp.panta.misskeyandroidclient.view.list.UserListDetailFragment
 import jp.panta.misskeyandroidclient.view.notes.ActionNoteHandler
 import jp.panta.misskeyandroidclient.view.notes.TimelineFragment
@@ -31,8 +33,8 @@ class UserListDetailActivity : AppCompatActivity(), UserListEditorDialog.OnSubmi
 
     companion object {
         private const val TAG = "UserListDetailActivity"
-        const val EXTRA_LIST_ID = "jp.panta.misskeyandroidclient.EXTRA_LIST_ID"
-        const val EXTRA_ACCOUNT_ID = "jp.panta.misskeyandroidclient.extra.ACCOUNT_ID"
+        private const val EXTRA_LIST_ID = "jp.panta.misskeyandroidclient.EXTRA_LIST_ID"
+        private const val EXTRA_ACCOUNT_ID = "jp.panta.misskeyandroidclient.extra.ACCOUNT_ID"
 
         private const val SELECT_USER_REQUEST_CODE = 252
 
@@ -40,10 +42,16 @@ class UserListDetailActivity : AppCompatActivity(), UserListEditorDialog.OnSubmi
         const val ACTION_EDIT_NAME = "ACTION_EDIT_NAME"
 
         const val EXTRA_UPDATED_USER_LIST = "EXTRA_UPDATED_USER_LIST"
+
+        fun newIntent(context: Context, listId: UserList.Id): Intent {
+            return Intent(context, UserListDetailActivity::class.java).apply {
+                putExtra(EXTRA_LIST_ID, listId)
+            }
+        }
     }
 
     private var account: Account? = null
-    private var mListId: String? = null
+    private var mListId: UserList.Id? = null
     private var mUserListDetailViewModel: UserListDetailViewModel? = null
 
     private var mIsNameUpdated: Boolean = false
@@ -56,19 +64,15 @@ class UserListDetailActivity : AppCompatActivity(), UserListEditorDialog.OnSubmi
 
         setSupportActionBar(userListToolbar)
 
-        val listId = intent.getStringExtra(EXTRA_LIST_ID)
-        val accountId = intent.getLongExtra(EXTRA_ACCOUNT_ID, -1)
+        val listId = intent.getSerializableExtra(EXTRA_LIST_ID) as UserList.Id
+        //val accountId = intent.getLongExtra(EXTRA_ACCOUNT_ID, -1)
 
-        if(accountId < 0 || listId == null){
-            Toast.makeText(this, R.string.auth_error, Toast.LENGTH_SHORT).show()
-            return finish()
-        }
 
         mListId = listId
         val miCore = application as MiCore
         val notesViewModel = ViewModelProvider(this, NotesViewModelFactory(application as MiApplication))[NotesViewModel::class.java]
 
-        val userListDetailViewModel = ViewModelProvider(this, UserListDetailViewModel.Factory(accountId, listId, miCore))[UserListDetailViewModel::class.java]
+        val userListDetailViewModel = ViewModelProvider(this, UserListDetailViewModel.Factory(listId, miCore))[UserListDetailViewModel::class.java]
         mUserListDetailViewModel = userListDetailViewModel
 
         ActionNoteHandler(this, notesViewModel, ViewModelProvider(this)[ConfirmViewModel::class.java]).initViewModelListener()
@@ -102,7 +106,7 @@ class UserListDetailActivity : AppCompatActivity(), UserListEditorDialog.OnSubmi
         menuInflater.inflate(R.menu.menu_user_list_detail, menu)
         val addToTabItem = menu?.findItem(R.id.action_add_to_tab)
         val page = account?.pages?.firstOrNull {
-            (it.pageable() as? Pageable.UserListTimeline)?.listId == mListId && mListId != null
+            (it.pageable() as? Pageable.UserListTimeline)?.listId == mListId?.userListId && mListId != null
         }
         if(page == null){
             addToTabItem?.setIcon(R.drawable.ic_add_to_tab_24px)
@@ -165,7 +169,7 @@ class UserListDetailActivity : AppCompatActivity(), UserListEditorDialog.OnSubmi
 
     private fun showEditUserListDialog(){
         val listId = mListId?: return
-        val dialog = UserListEditorDialog.newInstance(listId, mUserListName)
+        val dialog = UserListEditorDialog.newInstance(listId.userListId, mUserListName)
         dialog.show(supportFragmentManager, "")
     }
 
@@ -174,7 +178,7 @@ class UserListDetailActivity : AppCompatActivity(), UserListEditorDialog.OnSubmi
         val page = account?.pages?.firstOrNull {
             val pageable = it.pageable()
             if(pageable is Pageable.UserListTimeline){
-                pageable.listId == mListId && mListId != null
+                pageable.listId == mListId?.userListId && mListId != null
             }else{
                 false
             }
@@ -182,7 +186,7 @@ class UserListDetailActivity : AppCompatActivity(), UserListEditorDialog.OnSubmi
         val miCore = application as MiCore
         if(page == null){
             miCore.addPageInCurrentAccount(
-                Page(account?.accountId?: - 1, mUserListName, weight = -1, pageable = Pageable.UserListTimeline(mListId!!))
+                Page(account?.accountId?: - 1, mUserListName, weight = -1, pageable = Pageable.UserListTimeline(mListId?.userListId!!))
             )
         }else{
             miCore.removePageInCurrentAccount(page)
@@ -205,7 +209,7 @@ class UserListDetailActivity : AppCompatActivity(), UserListEditorDialog.OnSubmi
         finish()
     }
 
-    private fun createdResultFinish(userList: UserList){
+    private fun createdResultFinish(userList: UserListDTO){
         val data = Intent().apply{
             putExtra(ListListActivity.EXTRA_USER_LIST_ID, userList.id)
             putExtra(ListListActivity.EXTRA_USER_LIST_NAME, userList.name)
@@ -216,7 +220,7 @@ class UserListDetailActivity : AppCompatActivity(), UserListEditorDialog.OnSubmi
         finish()
     }
 
-    inner class PagerAdapter(val listId: String) : FragmentPagerAdapter(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
+    inner class PagerAdapter(val listId: UserList.Id) : FragmentPagerAdapter(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
         private val titles = listOf(getString(R.string.timeline), getString(R.string.user_list))
         override fun getCount(): Int {
             return titles.size
@@ -226,7 +230,7 @@ class UserListDetailActivity : AppCompatActivity(), UserListEditorDialog.OnSubmi
             return when(position){
                 0 ->{
                     TimelineFragment.newInstance(
-                        Pageable.UserListTimeline(listId = listId)
+                        Pageable.UserListTimeline(listId = listId.userListId)
                     )
                 }
                 1 ->{
@@ -236,7 +240,7 @@ class UserListDetailActivity : AppCompatActivity(), UserListEditorDialog.OnSubmi
             }
         }
 
-        override fun getPageTitle(position: Int): CharSequence? {
+        override fun getPageTitle(position: Int): CharSequence {
             return titles[position]
         }
     }
