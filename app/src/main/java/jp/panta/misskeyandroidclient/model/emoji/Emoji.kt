67612d21268dbcb3925ba91457f7252a24f21c16
kinoshita <kinoshita062323@gmail.com>
2021-03-21 02:52:08 +0900

    Kotlin Serializationでエラーが発生しないようにした。

--- app/src/main/java/jp/panta/misskeyandroidclient/model/emoji/Emoji.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/emoji/Emoji.kt
@@ -4,13 +4,13 @@ import java.io.Serializable
 
 @kotlinx.serialization.Serializable
 data class Emoji(
-    val id: String?,
+    val id: String? = null,
     val name: String,
-    val host: String?,
-    val url: String?,
-    val uri: String?,
-    val type: String?,
-    val category: String?
+    val host: String? = null,
+    val url: String? = null,
+    val uri: String? = null,
+    val type: String? = null,
+    val category: String? = null
     //val aliases: List<String>
 
 ): Serializable{
