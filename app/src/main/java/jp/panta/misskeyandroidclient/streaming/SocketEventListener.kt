6fd09ae3de1be42ff7f92c860fa023a50e9f793f
kinoshita <kinoshita062323@gmail.com>
2021-03-21 20:56:14 +0900

    イベントリスナーを二つの系統に分離した

--- app/src/main/java/jp/panta/misskeyandroidclient/streaming/SocketEventListener.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/streaming/SocketEventListener.kt
@@ -3,15 +3,21 @@ package jp.panta.misskeyandroidclient.streaming
 import jp.panta.misskeyandroidclient.streaming.Socket
 import jp.panta.misskeyandroidclient.streaming.StreamingEvent
 
-/**
- * Streamingのメッセージを処理するHandler
- */
-interface SocketEventListener {
 
-    /**
-     * @return これ以降のメッセージの処理を中断する場合は true, 続行する場合はfalseを返します
-     */
-    fun onMessage(e: StreamingEvent): Boolean
+
+fun interface SocketStateEventListener {
 
     fun onStateChanged(e: Socket.State)
+}
+
+fun interface SocketMessageEventListener {
+
+    fun onMessage(e: StreamingEvent): Boolean
+}
+
+fun interface BeforeConnectListener {
+    /**
+     * @return 接続処理を継続する場合は true そうでない場合はfalseを返します
+     */
+    fun onBeforeConnect(socket: Socket): Boolean
 }
