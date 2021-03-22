4e59d4b7d82b21949cf9369581b03f3cd2722105
kinoshita <kinoshita062323@gmail.com>
2021-03-22 22:21:31 +0900

    Mutexを利用するようにした

--- app/src/main/java/jp/panta/misskeyandroidclient/model/notes/NoteCaptureAPIWithAccountProvider.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/notes/NoteCaptureAPIWithAccountProvider.kt
@@ -4,6 +4,9 @@ import jp.panta.misskeyandroidclient.Logger
 import jp.panta.misskeyandroidclient.model.account.Account
 import jp.panta.misskeyandroidclient.streaming.SocketWithAccountProvider
 import jp.panta.misskeyandroidclient.streaming.notes.NoteCaptureAPI
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 /**
  * NoteCaptureAPIのインスタンスをAccountに基づきいい感じに取得や生成をできるようにする。
@@ -14,20 +17,22 @@ class NoteCaptureAPIWithAccountProvider(
 ) {
 
     private val accountIdWithNoteCaptureAPI = mutableMapOf<Long, NoteCaptureAPI>()
+    private val lock = Mutex()
 
-    fun get(account: Account) : NoteCaptureAPI {
-        synchronized(accountIdWithNoteCaptureAPI) {
+    fun get(account: Account) : NoteCaptureAPI = runBlocking{
+        lock.withLock {
             var channelAPI = accountIdWithNoteCaptureAPI[account.accountId]
             if(channelAPI != null) {
-                return channelAPI
+                return@runBlocking channelAPI
             }
 
             val socket = socketWithAccountProvider.get(account)
             channelAPI = NoteCaptureAPI(socket, loggerFactory)
             accountIdWithNoteCaptureAPI[account.accountId] = channelAPI
 
-            return channelAPI
+            return@runBlocking channelAPI
         }
+
     }
 
 }
