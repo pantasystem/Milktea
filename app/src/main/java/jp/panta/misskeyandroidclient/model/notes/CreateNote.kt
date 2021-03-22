4875259e4cd32e288bd709e0a6f8c55775a2247c
kinoshita <kinoshita062323@gmail.com>
2021-03-22 23:21:56 +0900

    Visibilityを外に切り出した

--- app/src/main/java/jp/panta/misskeyandroidclient/model/notes/CreateNote.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/notes/CreateNote.kt
@@ -4,10 +4,6 @@ package jp.panta.misskeyandroidclient.model.notes
 import jp.panta.misskeyandroidclient.model.account.Account
 import jp.panta.misskeyandroidclient.model.file.File
 import jp.panta.misskeyandroidclient.model.notes.poll.CreatePoll
-import jp.panta.misskeyandroidclient.model.users.User
-import java.lang.IllegalArgumentException
-import java.util.*
-import kotlin.jvm.Throws
 
 /**
  * @param noExtractEmojis 本文からカスタム絵文字を展開しないか否か
@@ -32,74 +28,3 @@ data class CreateNote(
 
 )
 
-sealed class Visibility {
-    data class Public(
-        override val isLocalOnly: Boolean
-    ) : CanLocalOnly, Visibility()
-
-    data class Home(
-        override val isLocalOnly: Boolean
-    ) : Visibility(), CanLocalOnly
-
-    data class Followers(
-        override val isLocalOnly: Boolean
-    ) : Visibility(), CanLocalOnly
-
-    data class Specified(
-        val visibleUserIds: List<User.Id>
-    ) : Visibility()
-
-    /*
-    NOTE: LocalOnlyはPub,Ho,Fのいずれかを選択し、そのうちLocalのユーザーに限定に公開されるというものなので、LocalOnlyというVisibilityは存在しない
-    object LocalOnly : Visibility(), CanLocalOnly {
-        override val isLocalOnly: Boolean = true
-    }*/
-
-}
-
-interface CanLocalOnly {
-    val isLocalOnly: Boolean
-}
-
-fun Visibility.type(): String {
-    return when(this) {
-        is Visibility.Public -> {
-            "public"
-        }
-        is Visibility.Home -> {
-            "home"
-        }
-        is Visibility.Followers -> {
-            "followers"
-        }
-        is Visibility.Specified -> {
-            "specified"
-        }
-    }
-}
-
-@Throws(IllegalArgumentException::class)
-fun Visibility(type: String, isLocalOnly: Boolean, visibleUserIds: List<User.Id>? = null): Visibility {
-    return when(type.toLowerCase(Locale.US)){
-        "public" -> Visibility.Public(isLocalOnly)
-        "home" -> Visibility.Home(isLocalOnly)
-        "followers" -> Visibility.Followers(isLocalOnly)
-        "specified" -> Visibility.Specified(visibleUserIds?: emptyList())
-        else -> throw IllegalArgumentException("public, home, followers, specified以外許可されていません。")
-    }
-    /*require((isLocalOnly && visibility is CanLocalOnly) || !isLocalOnly) {
-        "$type では localOnlyは指定できません。"
-    }*/
-
-
-}
-
-fun Visibility.isLocalOnly(): Boolean {
-    return (this as? CanLocalOnly)?.isLocalOnly?: false
-}
-
-fun CreateNote.visibleUserIds(): List<String>? {
-    return (this.visibility as? Visibility.Specified)?.visibleUserIds?.map {
-        it.id
-    }
-}
