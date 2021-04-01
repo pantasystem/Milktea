e79254880a9dd0569c103274f64a7ee41a137127
kinoshita <kinoshita062323@gmail.com>
2021-04-02 00:03:14 +0900

    リファクタリングした

--- app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/auth/app/AppAuthViewModel.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/auth/app/AppAuthViewModel.kt
@@ -3,35 +3,39 @@ package jp.panta.misskeyandroidclient.viewmodel.auth.app
 import android.util.Log
 import androidx.lifecycle.*
 import jp.panta.misskeyandroidclient.api.MisskeyAPIServiceBuilder
+import jp.panta.misskeyandroidclient.api.throwIfHasError
 import jp.panta.misskeyandroidclient.model.api.MisskeyGetMeta
+import jp.panta.misskeyandroidclient.model.api.Version
 import jp.panta.misskeyandroidclient.model.auth.AppSecret
 import jp.panta.misskeyandroidclient.model.auth.Session
-import jp.panta.misskeyandroidclient.model.auth.custom.App
-import jp.panta.misskeyandroidclient.model.auth.custom.CreateApp
-import jp.panta.misskeyandroidclient.model.auth.custom.CustomAuthBridge
-import jp.panta.misskeyandroidclient.model.auth.custom.CustomAuthStore
+import jp.panta.misskeyandroidclient.model.auth.custom.*
 import jp.panta.misskeyandroidclient.model.instance.Meta
+import jp.panta.misskeyandroidclient.viewmodel.MiCore
 import jp.panta.misskeyandroidclient.viewmodel.auth.DefaultPermission
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
 import java.util.*
 import java.util.regex.Pattern
 
 @Suppress("UNCHECKED_CAST")
 class AppAuthViewModel(
-    val customAuthStore: CustomAuthStore
+    val customAuthStore: CustomAuthStore,
+    miCore: MiCore
 ) : ViewModel(){
     companion object{
         const val CALL_BACK_URL = "misskey://app_auth_callback"
     }
 
-    class Factory(val customAuthStore: CustomAuthStore) : ViewModelProvider.Factory{
+    class Factory(val customAuthStore: CustomAuthStore, private val miCore: MiCore) : ViewModelProvider.Factory{
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return AppAuthViewModel(customAuthStore) as T
+            return AppAuthViewModel(customAuthStore, miCore) as T
         }
     }
 
@@ -52,6 +56,9 @@ class AppAuthViewModel(
 
     val generatingToken = MutableLiveData<Boolean>(false)
 
+    private val metaStore = miCore.getMetaStore()
+    private val misskeyAPIProvider = miCore.getMisskeyAPIProvider()
+
     init{
         meta.addSource(instanceDomain){ base ->
             val url = if(base.startsWith("https://")){
@@ -61,21 +68,16 @@ class AppAuthViewModel(
             }.replace(" ", "").replace("\t", "").replace("　", "")
             if(urlPattern.matcher(url).find()){
 
-                try{
-                    MisskeyGetMeta.getMeta(url).enqueue(object : Callback<Meta>{
-                        override fun onResponse(call: Call<Meta>, response: Response<Meta>) {
-                            meta.postValue(response.body())
-                        }
-
-                        override fun onFailure(call: Call<Meta>, t: Throwable) {
-                            meta.postValue(null)
-                        }
-                    })
-                }catch(e: IllegalArgumentException){
-                    Log.w("AppAuthViewModel", "不正なURLの可能性があります", e)
+                viewModelScope.launch(Dispatchers.IO) {
+                    runCatching {
+                        metaStore.get(url)
+                    }.onFailure {
+                        meta.postValue(null)
+                    }.onSuccess {
+                        meta.postValue(it)
+                    }
                 }
             }
-
         }
         validatedInstanceDomain.addSource(meta){
             validatedInstanceDomain.value = it != null
@@ -83,54 +85,51 @@ class AppAuthViewModel(
     }
 
 
-    fun startAuth(){
+    fun auth(){
         generatingToken.value = true
         val url = this.instanceDomain.value?: return
         val instanceBase = "https://$url"
         val appName = this.appName.value?: return
         val meta = this.meta.value?: return
-        val misskeyAPI = MisskeyAPIServiceBuilder.build(instanceBase, meta.getVersion())
         viewModelScope.launch(Dispatchers.IO){
-            try{
-                val app = misskeyAPI.createApp(
-                    CreateApp(
-                        null,
-                        appName,
-                        "misskey android application",
-                        CALL_BACK_URL,
-                        permission = DefaultPermission.defaultPermission
-                    )
-                ).execute()?.body()
-                Log.d("AppAuthViewModel", "created app: $app")
+            runCatching {
+                val app = createApp(instanceBase, meta.getVersion(), appName)
                 this@AppAuthViewModel.app.postValue(app)
-                app?: return@launch
                 val secret = app.secret
                 val authApi = MisskeyAPIServiceBuilder.buildAuthAPI(instanceBase)
                 val session = authApi.generateSession(AppSecret(secret!!)).execute().body()
-                this@AppAuthViewModel.session.postValue(session)
-                session?: return@launch
-
+                    ?: throw IllegalStateException("セッションの作成に失敗しました。")
                 customAuthStore.setCustomAuthBridge(
-                    CustomAuthBridge(
-                        secret = secret,
-                        instanceDomain = instanceBase,
-                        session = session,
-                        enabledDateEnd = Date(Date().time + 60 * 1000 * 2),
-                        viaName = appName
-
-                    )
+                    app.createAuth(instanceBase, session)
                 )
+                session
+            }.onSuccess { session ->
+                this@AppAuthViewModel.session.postValue(session)
 
-            }catch(e: Exception){
-                generatingToken.postValue(false)
-            }finally{
+            }.onFailure {
+                Log.e("AppAuthViewModel", "認証開始処理失敗", it)
+            }.also {
                 generatingToken.postValue(false)
             }
-
 
         }
 
 
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    private fun createApp(url: String, version: Version, appName: String): App {
+        val misskeyAPI = misskeyAPIProvider.get(url, version)
+        return misskeyAPI.createApp(
+            CreateApp(
+                null,
+                appName,
+                "misskey android application",
+                CALL_BACK_URL,
+                permission = DefaultPermission.defaultPermission
+            )
+        ).execute().throwIfHasError().body()
+            ?: throw IllegalStateException("Appの作成に失敗しました。")
     }
 
 
