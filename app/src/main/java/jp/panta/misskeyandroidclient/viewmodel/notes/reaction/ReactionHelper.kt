06c9b09ad98440a53480f76ec9fd8221022d2937
kinoshita <kinoshita062323@gmail.com>
2021-04-02 18:54:27 +0900

    他のインスタンスの場合背景色を変更するようにした

--- app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/notes/reaction/ReactionHelper.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/notes/reaction/ReactionHelper.kt
@@ -1,5 +1,8 @@
 package jp.panta.misskeyandroidclient.viewmodel.notes.reaction
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.widget.LinearLayout
 import androidx.databinding.BindingAdapter
 import jp.panta.misskeyandroidclient.R
@@ -7,9 +10,17 @@ import jp.panta.misskeyandroidclient.viewmodel.notes.PlaneNoteViewData
 
 object ReactionHelper {
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @JvmStatic
     @BindingAdapter("reactionNote", "reactionBackground")
     fun LinearLayout.setBackground(note: PlaneNoteViewData, reaction: Pair<String, Int>){
+
+        val r = reaction.first.replace(":", "")
+        val host = r.split("@").getOrNull(1)
+        if(host != null && host != ".") {
+            this.background = ColorDrawable(Color.argb(0,0,0,0))
+            return
+        }
         if(note.myReaction.value != null && note.myReaction.value == reaction.first){
             this.background = context.resources.getDrawable(R.drawable.shape_selected_reaction_background, context.theme)
         }else{
