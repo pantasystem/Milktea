de3fac894021a8239ba4ee95077938d7fda50982
kinoshita <kinoshita062323@gmail.com>
2021-04-03 09:02:45 +0900

    ページネーションをするためのクラスを追加した

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/reaction/ReactionHistoryPaginator.kt
@@ -0,0 +1,19 @@
+package jp.panta.misskeyandroidclient.model.reaction
+
+/**
+ * リアクションの履歴をページネーションするためのインターフェース
+ * 読み込んだデータはReactionHistoryDataSourceへ注入する
+ */
+interface ReactionHistoryPaginator {
+    val reactionHistoryRequest: ReactionHistoryRequest
+
+    /**
+     * 未来のリアクション履歴を取得します。
+     */
+    suspend fun loadFuture()
+
+    /**
+     * 過去のリアクション履歴を取得します。
+     */
+    suspend fun loadPast()
+}
