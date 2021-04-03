e4bcd0b246b7593e077e8f0217e5792c25dcf8b4
kinoshita <kinoshita062323@gmail.com>
2021-04-03 10:11:54 +0900

    Paginatorを実装した テストしてないので動くかは知らん

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/reaction/impl/ReactionHistoryPaginatorImpl.kt
@@ -0,0 +1,104 @@
+package jp.panta.misskeyandroidclient.model.reaction.impl
+
+import jp.panta.misskeyandroidclient.api.MisskeyAPIProvider
+import jp.panta.misskeyandroidclient.api.reaction.RequestReactionHistoryDTO
+import jp.panta.misskeyandroidclient.api.throwIfHasError
+import jp.panta.misskeyandroidclient.api.users.toUser
+import jp.panta.misskeyandroidclient.model.Encryption
+import jp.panta.misskeyandroidclient.model.account.Account
+import jp.panta.misskeyandroidclient.model.account.AccountRepository
+import jp.panta.misskeyandroidclient.model.reaction.ReactionHistory
+import jp.panta.misskeyandroidclient.model.reaction.ReactionHistoryDataSource
+import jp.panta.misskeyandroidclient.model.reaction.ReactionHistoryPaginator
+import jp.panta.misskeyandroidclient.model.reaction.ReactionHistoryRequest
+import jp.panta.misskeyandroidclient.model.users.UserDataSource
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+class ReactionHistoryPaginatorImpl(
+    override val reactionHistoryRequest: ReactionHistoryRequest,
+    private val reactionHistoryDataSource: ReactionHistoryDataSource,
+    private val misskeyAPIProvider: MisskeyAPIProvider,
+    private val accountRepository: AccountRepository,
+    private val encryption: Encryption,
+    private val userDataSource: UserDataSource
+) : ReactionHistoryPaginator {
+
+
+    val lock = Mutex()
+
+    private var sinceId: ReactionHistory.Id? = null
+    private var untilId: ReactionHistory.Id? = null
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    override suspend fun loadFuture() {
+        lock.withLock {
+            val account: Account = accountRepository.get(reactionHistoryRequest.noteId.accountId)
+            val misskeyAPI = misskeyAPIProvider.get(account.instanceDomain)
+            val body = misskeyAPI.reactions(
+                RequestReactionHistoryDTO(
+                    sinceId= sinceId?.reactionId,
+                    i = account.getI(encryption),
+                    type = reactionHistoryRequest.type,
+                    noteId = reactionHistoryRequest.noteId.noteId
+                )
+            ).execute().throwIfHasError().body()?: emptyList()
+            val histories = body.map {
+                val user = it.user.toUser(account, false)
+                userDataSource.add(user)
+                ReactionHistory(
+                    ReactionHistory.Id(it.id, reactionHistoryRequest.noteId.accountId),
+                    reactionHistoryRequest.noteId,
+                    it.createdAt,
+                    user,
+                    it.type
+                )
+            }
+            reactionHistoryDataSource.addAll(histories)
+
+            histories.lastOrNull()?.let {
+                untilId = it.id
+            }
+
+            if(sinceId == null) {
+                sinceId = histories.firstOrNull()?.id
+            }
+        }
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    override suspend fun loadPast() {
+        lock.withLock {
+            val account: Account = accountRepository.get(reactionHistoryRequest.noteId.accountId)
+            val misskeyAPI = misskeyAPIProvider.get(account.instanceDomain)
+            val body = misskeyAPI.reactions(
+                RequestReactionHistoryDTO(
+                    untilId = untilId?.reactionId,
+                    i = account.getI(encryption),
+                    type = reactionHistoryRequest.type,
+                    noteId = reactionHistoryRequest.noteId.noteId
+                )
+            ).execute().throwIfHasError().body()?: emptyList()
+            val histories = body.map {
+                val user = it.user.toUser(account, false)
+                userDataSource.add(user)
+                ReactionHistory(
+                    ReactionHistory.Id(it.id, reactionHistoryRequest.noteId.accountId),
+                    reactionHistoryRequest.noteId,
+                    it.createdAt,
+                    user,
+                    it.type
+                )
+            }
+            reactionHistoryDataSource.addAll(histories)
+
+            histories.lastOrNull()?.let {
+                untilId = it.id
+            }
+
+            if(sinceId == null) {
+                sinceId = histories.firstOrNull()?.id
+            }
+        }
+    }
+}
