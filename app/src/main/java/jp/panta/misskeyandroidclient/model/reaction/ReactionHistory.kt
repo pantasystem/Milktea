428b729647e7aaaeba153e29b60033a09d08dec2
kinoshita <kinoshita062323@gmail.com>
2021-04-03 10:09:36 +0900

    noteIdを外部キーに設定した

--- app/src/main/java/jp/panta/misskeyandroidclient/model/reaction/ReactionHistory.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/reaction/ReactionHistory.kt
@@ -2,12 +2,13 @@ package jp.panta.misskeyandroidclient.model.reaction
 
 import jp.panta.misskeyandroidclient.model.Entity
 import jp.panta.misskeyandroidclient.model.EntityId
+import jp.panta.misskeyandroidclient.model.notes.Note
 import jp.panta.misskeyandroidclient.model.users.User
 import java.util.*
 
 data class ReactionHistory (
     val id: Id,
-    val noteId: String,
+    val noteId: Note.Id,
     val createdAt: Date,
     val user: User,
     val type: String
