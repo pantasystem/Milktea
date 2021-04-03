f4630ad8df3dd4a048a6b2b3442d3f57d3f8c369
kinoshita <kinoshita062323@gmail.com>
2021-04-03 09:01:59 +0900

    リアクションの履歴エンティティを追加

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/reaction/ReactionHistory.kt
@@ -0,0 +1,19 @@
+package jp.panta.misskeyandroidclient.model.reaction
+
+import jp.panta.misskeyandroidclient.model.Entity
+import jp.panta.misskeyandroidclient.model.EntityId
+import jp.panta.misskeyandroidclient.model.users.User
+import java.util.*
+
+data class ReactionHistory (
+    val id: Id,
+    val createdAt: Date,
+    val user: User,
+    val type: String
+) : Entity{
+
+    data class Id(
+        val reactionId: String,
+        val accountId: Long
+    ) : EntityId
+}
