a891bd42ad10dbf1cbb2e348c831d5121367603a
kinoshita <kinoshita062323@gmail.com>
2021-04-03 10:11:00 +0900

    認証パラメータを追加した

--- app/src/main/java/jp/panta/misskeyandroidclient/api/reaction/RequestReactionHistoryDTO.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/api/reaction/RequestReactionHistoryDTO.kt
@@ -1,10 +1,11 @@
 package jp.panta.misskeyandroidclient.api.reaction
 
 data class RequestReactionHistoryDTO (
+    val i: String,
     val noteId: String,
     val type: String?,
     val limit: Int = 20,
     val offset: Int? = null,
     val sinceId: String? = null,
-    val untilId: String? = null
+    val untilId: String? = null,
 )
