9a6a5418479aeb715b667f8d1105341fe10c80fb
kinoshita <kinoshita062323@gmail.com>
2021-04-27 23:30:05 +0900

    テストしやすくするためにInterfaceとして切り出した

--- app/src/main/java/jp/panta/misskeyandroidclient/model/notes/NoteCaptureAPIWithAccountProvider.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/notes/NoteCaptureAPIWithAccountProvider.kt
@@ -4,6 +4,7 @@ import jp.panta.misskeyandroidclient.Logger
 import jp.panta.misskeyandroidclient.model.account.Account
 import jp.panta.misskeyandroidclient.streaming.SocketWithAccountProvider
 import jp.panta.misskeyandroidclient.streaming.notes.NoteCaptureAPI
+import jp.panta.misskeyandroidclient.streaming.notes.NoteCaptureAPIImpl
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
@@ -27,7 +28,7 @@ class NoteCaptureAPIWithAccountProvider(
             }
 
             val socket = socketWithAccountProvider.get(account)
-            channelAPI = NoteCaptureAPI(socket, loggerFactory)
+            channelAPI = NoteCaptureAPIImpl(socket, loggerFactory)
             accountIdWithNoteCaptureAPI[account.accountId] = channelAPI
 
             return@runBlocking channelAPI
