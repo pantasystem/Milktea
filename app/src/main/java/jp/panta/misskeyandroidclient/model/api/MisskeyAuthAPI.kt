4f11a5c5d00ea72e45f9a65846eb2768b8431bcc
kinoshita <kinoshita062323@gmail.com>
2021-05-02 17:20:51 +0900

    suspend funへ移行した

--- app/src/main/java/jp/panta/misskeyandroidclient/model/api/MisskeyAuthAPI.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/api/MisskeyAuthAPI.kt
@@ -5,14 +5,15 @@ import jp.panta.misskeyandroidclient.api.auth.AppSecret
 import jp.panta.misskeyandroidclient.api.auth.Session
 import jp.panta.misskeyandroidclient.api.auth.UserKey
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface MisskeyAuthAPI {
     //auth
     @POST("api/auth/session/generate")
-    fun generateSession(@Body appSecret: AppSecret): Call<Session>
+    suspend fun generateSession(@Body appSecret: AppSecret): Response<Session>
 
     @POST("api/auth/session/userkey")
-    fun getAccessToken(@Body userKey: UserKey): Call<AccessToken>
+    suspend fun getAccessToken(@Body userKey: UserKey): Response<AccessToken>
 }
