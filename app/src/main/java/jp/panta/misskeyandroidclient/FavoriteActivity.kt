6ac0ec381228432003fde086f41b88b2f7baf797
kinoshita <kinoshita062323@gmail.com>
2021-05-11 05:34:51 +0900

    DataBindingへ移行した

--- app/src/main/java/jp/panta/misskeyandroidclient/FavoriteActivity.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/FavoriteActivity.kt
@@ -3,22 +3,24 @@ package jp.panta.misskeyandroidclient
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import jp.panta.misskeyandroidclient.databinding.ActivityFavoriteBinding
 import jp.panta.misskeyandroidclient.model.account.page.Pageable
 import jp.panta.misskeyandroidclient.view.notes.ActionNoteHandler
 import jp.panta.misskeyandroidclient.view.notes.TimelineFragment
 import jp.panta.misskeyandroidclient.viewmodel.confirm.ConfirmViewModel
 import jp.panta.misskeyandroidclient.viewmodel.notes.NotesViewModel
 import jp.panta.misskeyandroidclient.viewmodel.notes.NotesViewModelFactory
-import kotlinx.android.synthetic.main.activity_favorite.*
 
 class FavoriteActivity : AppCompatActivity() {
 
+    lateinit var mBinding: ActivityFavoriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme()
-        setContentView(R.layout.activity_favorite)
-        setSupportActionBar(favorite_toolbar)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_favorite)
+        setSupportActionBar(mBinding.favoriteToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = getString(R.string.favorite)
 
