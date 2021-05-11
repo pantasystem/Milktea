d03284beb5022474532915895318b2b1911ff07f
kinoshita <kinoshita062323@gmail.com>
2021-05-11 19:34:15 +0900

    databinding-ktxを利用するようにした

--- app/src/main/java/jp/panta/misskeyandroidclient/view/notes/editor/PollEditorFragment.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/notes/editor/PollEditorFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wada811.databinding.dataBinding
