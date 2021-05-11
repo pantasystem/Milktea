5cefb963f6033c50583bce4c554a3e81473de8e1
kinoshita <kinoshita062323@gmail.com>
2021-05-11 19:25:54 +0900

    databinding-ktxを利用するようにした

--- app/src/main/java/jp/panta/misskeyandroidclient/view/notes/draft/DraftNotesFragment.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/notes/draft/DraftNotesFragment.kt
@@ -1,6 +1,5 @@
 package jp.panta.misskeyandroidclient.view.notes.draft
 
-import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -8,10 +7,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.wada811.databinding.dataBinding
 import jp.panta.misskeyandroidclient.MediaActivity
 import jp.panta.misskeyandroidclient.MiApplication
 import jp.panta.misskeyandroidclient.NoteEditorActivity
 import jp.panta.misskeyandroidclient.R
+import jp.panta.misskeyandroidclient.databinding.FragmentDraftNotesBinding
 import jp.panta.misskeyandroidclient.model.confirm.ConfirmCommand
 import jp.panta.misskeyandroidclient.model.confirm.ResultType
 import jp.panta.misskeyandroidclient.model.file.File
@@ -20,7 +21,6 @@ import jp.panta.misskeyandroidclient.view.confirm.ConfirmDialog
 import jp.panta.misskeyandroidclient.viewmodel.confirm.ConfirmViewModel
 import jp.panta.misskeyandroidclient.viewmodel.file.FileListener
 import jp.panta.misskeyandroidclient.viewmodel.notes.draft.DraftNotesViewModel
-import kotlinx.android.synthetic.main.fragment_draft_notes.*
 
 class DraftNotesFragment : Fragment(R.layout.fragment_draft_notes), DraftNoteActionCallback, FileListener{
 
@@ -30,6 +30,9 @@ class DraftNotesFragment : Fragment(R.layout.fragment_draft_notes), DraftNoteAct
 
     private var mDraftNotesViewModel: DraftNotesViewModel? = null
     private var mConfirmViewModel: ConfirmViewModel? = null
+
+    private val binding: FragmentDraftNotesBinding by dataBinding()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -38,24 +41,24 @@ class DraftNotesFragment : Fragment(R.layout.fragment_draft_notes), DraftNoteAct
         mDraftNotesViewModel = viewModel
 
         val adapter = DraftNoteListAdapter(this, this, viewLifecycleOwner)
-        draftNotesView.adapter = adapter
-        draftNotesView.layoutManager = LinearLayoutManager(view.context)
+        binding.draftNotesView.adapter = adapter
+        binding.draftNotesView.layoutManager = LinearLayoutManager(view.context)
 
-        viewModel.draftNotes.observe(viewLifecycleOwner, Observer {
+        viewModel.draftNotes.observe(viewLifecycleOwner, {
             adapter.submitList(it)
         })
 
-        draftNotesSwipeRefresh.setOnRefreshListener {
+        binding.draftNotesSwipeRefresh.setOnRefreshListener {
             viewModel.loadDraftNotes()
         }
-        viewModel.isLoading.observe( viewLifecycleOwner, Observer {
-            draftNotesSwipeRefresh.isRefreshing = it
+        viewModel.isLoading.observe( viewLifecycleOwner, {
+            binding.draftNotesSwipeRefresh.isRefreshing = it
         })
 
         val confirmViewModel = ViewModelProvider(requireActivity())[ConfirmViewModel::class.java]
         mConfirmViewModel = confirmViewModel
 
-        confirmViewModel.confirmedEvent.observe(viewLifecycleOwner, Observer {
+        confirmViewModel.confirmedEvent.observe(viewLifecycleOwner, {
             if(it.eventType == EV_DELETE_DRAFT_NOTE && it.resultType == ResultType.POSITIVE){
                 (it.args as? DraftNote)?.let{ dn ->
                     mDraftNotesViewModel?.deleteDraftNote(dn)
@@ -63,7 +66,7 @@ class DraftNotesFragment : Fragment(R.layout.fragment_draft_notes), DraftNoteAct
             }
         })
 
-        confirmViewModel.confirmEvent.observe(viewLifecycleOwner, Observer {
+        confirmViewModel.confirmEvent.observe(viewLifecycleOwner, {
             ConfirmDialog().show(parentFragmentManager, "confirm")
         })
     }
