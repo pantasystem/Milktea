f05b553a6622a4793f50ae076577e51e8e93256e
kinoshita <kinoshita062323@gmail.com>
2021-05-14 01:05:49 +0900

    メソッドの引数を間違えていたので修正した

--- app/src/main/java/jp/panta/misskeyandroidclient/model/gallery/GalleryRepository.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/gallery/GalleryRepository.kt
@@ -2,7 +2,7 @@ package jp.panta.misskeyandroidclient.model.gallery
 
 interface GalleryRepository {
 
-    suspend fun create() : GalleryPost
+    suspend fun create(createGalleryPost: CreateGalleryPost) : GalleryPost
 
     suspend fun find(id: GalleryPost.Id) : GalleryPost
 
@@ -12,6 +12,6 @@ interface GalleryRepository {
 
     suspend fun delete(id: GalleryPost)
 
-    suspend fun update(id: GalleryPost) : GalleryPost
+    suspend fun update(updateGalleryPost: UpdateGalleryPost) : GalleryPost
 
 }
