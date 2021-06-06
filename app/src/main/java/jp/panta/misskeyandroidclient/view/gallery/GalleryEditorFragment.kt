80f015dabf27e353286f2d7b7277273282bc2e76
kinoshita <kinoshita062323@gmail.com>
2021-06-07 02:59:45 +0900

    Toolbarを実装した

--- app/src/main/java/jp/panta/misskeyandroidclient/view/gallery/GalleryEditorFragment.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/gallery/GalleryEditorFragment.kt
@@ -2,12 +2,23 @@ package jp.panta.misskeyandroidclient.view.gallery
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.wada811.databinding.dataBinding
 import jp.panta.misskeyandroidclient.R
+import jp.panta.misskeyandroidclient.databinding.FragmentGalleryEditorBinding
 
 class GalleryEditorFragment : Fragment(R.layout.fragment_gallery_editor) {
 
+    val binding: FragmentGalleryEditorBinding by dataBinding()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        (requireActivity() as AppCompatActivity).also { appCompatActivity ->
+            appCompatActivity.setSupportActionBar(binding.toolbar)
+            appCompatActivity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
     }
 }
