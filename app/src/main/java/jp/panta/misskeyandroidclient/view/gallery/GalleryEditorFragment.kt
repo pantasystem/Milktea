291baedc8b1a369d812530fc2f078d995be143b7
kinoshita <kinoshita062323@gmail.com>
2021-06-07 02:42:50 +0900

    レイアウトを作成した

--- app/src/main/java/jp/panta/misskeyandroidclient/view/gallery/GalleryEditorFragment.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/gallery/GalleryEditorFragment.kt
@@ -1,6 +1,13 @@
 package jp.panta.misskeyandroidclient.view.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import jp.panta.misskeyandroidclient.R
 
-class GalleryEditorFragment : Fragment() {
+class GalleryEditorFragment : Fragment(R.layout.fragment_gallery_editor) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
