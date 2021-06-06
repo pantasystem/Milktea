184edbdc887a5cc733c5d973f42a739787d5e57b
kinoshita <kinoshita062323@gmail.com>
2021-06-06 21:27:45 +0900

    Gallery一覧、お気に入り、自分の投稿を表示するタブFragmentを実装した

--- app/src/main/java/jp/panta/misskeyandroidclient/GalleryPostsActivity.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/GalleryPostsActivity.kt
@@ -9,9 +9,17 @@ import jp.panta.misskeyandroidclient.databinding.ActivityGalleryPostsBinding
 
 class GalleryPostsActivity : AppCompatActivity() {
 
+    companion object {
+        private const val EDITOR = "gallery_posts_editor"
+        private const val GALLERY_POSTS_TAB = "gallery_posts_tab"
+
+        private const val CURRENT_FRAGMENT_TAG = "CURRENT_FRAGMENT"
+    }
 
 
     val binding: ActivityGalleryPostsBinding by dataBinding()
+
+    private var currentFragmentTag = GALLERY_POSTS_TAB
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +28,31 @@ class GalleryPostsActivity : AppCompatActivity() {
 
         val action = intent.action ?: Intent.ACTION_VIEW
 
+        if(action == Intent.ACTION_EDIT) {
+            currentFragmentTag = EDITOR
+        }
+        changeFragment(currentFragmentTag)
+    }
+
+    private fun changeFragment(tag: String) {
+        //val ft = supportFragmentManager.beginTransaction()
+        //ft.replace(R.id.base, )
+    }
+
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        if(supportFragmentManager.backStackEntryCount > 1) {
+            supportFragmentManager.popBackStack()
+        }else{
+            finish()
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(currentFragmentTag, currentFragmentTag)
     }
 }
