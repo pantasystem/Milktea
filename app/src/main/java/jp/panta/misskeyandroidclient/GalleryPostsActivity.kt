e85edab4cc77b7c12935fc8b7ec785eb546fc4e6
kinoshita <kinoshita062323@gmail.com>
2021-06-07 01:35:53 +0900

    画面遷移を実装した

--- app/src/main/java/jp/panta/misskeyandroidclient/GalleryPostsActivity.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/GalleryPostsActivity.kt
@@ -4,24 +4,23 @@ package jp.panta.misskeyandroidclient
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.wada811.databinding.dataBinding
 import jp.panta.misskeyandroidclient.databinding.ActivityGalleryPostsBinding
+import jp.panta.misskeyandroidclient.view.gallery.GalleryEditorFragment
 import jp.panta.misskeyandroidclient.view.gallery.GalleryPostTabFragment
+import jp.panta.misskeyandroidclient.viewmodel.gallery.Action
+import jp.panta.misskeyandroidclient.viewmodel.gallery.GalleryPostActionViewModel
 
 class GalleryPostsActivity : AppCompatActivity() {
 
-    companion object {
-        private const val EDITOR = "gallery_posts_editor"
-        private const val GALLERY_POSTS_TAB = "gallery_posts_tab"
-
-        private const val CURRENT_FRAGMENT_TAG = "CURRENT_FRAGMENT"
-    }
 
 
     val binding: ActivityGalleryPostsBinding by dataBinding()
-
-    private var currentFragmentTag = GALLERY_POSTS_TAB
+    val actionViewModel: GalleryPostActionViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,47 +29,48 @@ class GalleryPostsActivity : AppCompatActivity() {
 
         val action = intent.action ?: Intent.ACTION_VIEW
 
-        if(action == Intent.ACTION_EDIT) {
-            currentFragmentTag = EDITOR
-        }
-        //changeFragment(currentFragmentTag)
         val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.base, GalleryPostTabFragment())
+        if(action == Intent.ACTION_EDIT) {
+            ft.replace(R.id.base, GalleryEditorFragment())
+        }else{
+            ft.replace(R.id.base, GalleryPostTabFragment())
+        }
         ft.commit()
+
+        actionViewModel.viewAction.observe(this) {
+            when(it) {
+                is Action.OpenCreationEditor -> {
+                    Log.d("GalleryPostsActivity", "アクションがあった")
+                    val t = supportFragmentManager.beginTransaction()
+                    t.replace(R.id.base, GalleryEditorFragment())
+                    t.addToBackStack(null)
+                    t.commit()
+                }
+            }
+
+        }
+
     }
 
 
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-
-        pop()
-
-    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             android.R.id.home ->{
-                if(currentFragmentTag == EDITOR) {
-                    supportFragmentManager.popBackStack(GALLERY_POSTS_TAB, 0)
-                }else{
-                    finish()
-                }
+                pop()
             }
         }
         return super.onOptionsItemSelected(item)
     }
 
     private fun pop() {
-        if(supportFragmentManager.backStackEntryCount > 1) {
+        if(supportFragmentManager.backStackEntryCount > 0) {
+            Log.d("count", "backStackEntryCount:${supportFragmentManager.backStackEntryCount}")
             supportFragmentManager.popBackStack()
         }else{
             finish()
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(currentFragmentTag, currentFragmentTag)
-    }
+
 }
---
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/gallery/GalleryEditorFragment.kt
@@ -0,0 +1,6 @@
+package jp.panta.misskeyandroidclient.view.gallery
+
+import androidx.fragment.app.Fragment
+
+class GalleryEditorFragment : Fragment() {
+}
