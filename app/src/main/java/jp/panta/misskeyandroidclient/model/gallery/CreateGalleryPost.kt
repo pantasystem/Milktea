ec66075c3bd6b2f630c7089c1393e3d469554705
kinoshita <kinoshita062323@gmail.com>
2021-06-12 23:16:41 +0900

    GalleryPostの作成タスク

--- app/src/main/java/jp/panta/misskeyandroidclient/model/gallery/CreateGalleryPost.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/gallery/CreateGalleryPost.kt
@@ -1,5 +1,6 @@
 package jp.panta.misskeyandroidclient.model.gallery
 
+import jp.panta.misskeyandroidclient.model.ITask
 import jp.panta.misskeyandroidclient.model.account.Account
 import jp.panta.misskeyandroidclient.model.file.File
 
@@ -11,3 +12,17 @@ class CreateGalleryPost (
     val isSensitive: Boolean,
     val tags: List<String>
 )
+
+
+class CreateGalleryPostTask(
+    val createGalleryPost: CreateGalleryPost,
+    private val galleryPostRepository: GalleryRepository
+) : ITask<GalleryPost> {
+    override suspend fun execute(): GalleryPost {
+        return galleryPostRepository.create(createGalleryPost)
+    }
+}
+
+fun CreateGalleryPost.toTask(galleryPostRepository: GalleryRepository) : CreateGalleryPostTask {
+    return CreateGalleryPostTask(this, galleryPostRepository)
+}
