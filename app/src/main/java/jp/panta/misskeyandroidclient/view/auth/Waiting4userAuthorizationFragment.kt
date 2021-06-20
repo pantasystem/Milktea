d14b309d925dee6ae44e76505be10c73d8280bc1
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-20 21:40:06 +0900

    認証先のURLを表示するEditTextをフォーカスは当たるが、編集はできないようにしました。

--- app/src/main/java/jp/panta/misskeyandroidclient/view/auth/Waiting4userAuthorizationFragment.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/auth/Waiting4userAuthorizationFragment.kt
@@ -2,10 +2,7 @@ package jp.panta.misskeyandroidclient.view.auth
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -16,7 +13,6 @@ import jp.panta.misskeyandroidclient.model.auth.Authorization
 import jp.panta.misskeyandroidclient.viewmodel.MiCore
 import jp.panta.misskeyandroidclient.viewmodel.auth.AuthViewModel
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.onEach
 
@@ -30,6 +26,11 @@ class Waiting4userAuthorizationFragment : Fragment(R.layout.fragment_waiting_4_u
 
         val miCore = context?.applicationContext as MiCore
         val authViewModel = ViewModelProvider(requireActivity(), AuthViewModel.Factory(miCore))[AuthViewModel::class.java]
+
+        // EditTextにフォーカスが当たるようにしたいが、編集はできないようにしたいためこのようにしています。
+        binding.authenticationUrlViewEditText.setOnKeyListener { _, _, _ ->
+            return@setOnKeyListener true
+        }
 
         authViewModel.authorization.mapNotNull {
             it as? Authorization.Waiting4UserAuthorization
