0c53b50a29f2a126570f486e550239e1d2097199
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-23 03:02:08 +0900

    [change] validate

--- PushToFCM/index.js
+++ PushToFCM/index.js
@@ -7,7 +7,9 @@ app.post('/webpushcallback', (req, res)=>{
     console.log();
     let deviceToken = req.query.deviceToken
     let accountId = req.query.accountId;
-    
+    if(!(deviceToken && accountId)) {
+        return cres.status(422).end();
+    }
     console.log(`deviceToken:${deviceToken}, accountId:${accountId}`);
     res.json({status: 'ok'});
 });
