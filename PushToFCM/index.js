90b902ad7ef6ccde7ddbdc396b4ec2c13ea4bdd7
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-23 22:59:38 +0900

    [add]

--- PushToFCM/index.js
+++ PushToFCM/index.js
@@ -4,7 +4,7 @@ const Concat = require('concat-stream');
 const fs = require('fs');
 const webPushDecipher = require('./webPushDecipher.js');
 
-const AUTH_SECRET = fs.readFileSync('./key/auth_secret.txt');
+const AUTH_SECRET = fs.readFileSync('./key/auth_secret.txt', 'utf8');
 const PUBLICK_KEY = fs.readFileSync('./key/public_key.txt', 'utf8');
 const PRIVATE_KEY = fs.readFileSync('./key/private_key.txt', 'utf8');
 
