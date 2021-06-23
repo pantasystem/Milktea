722854397687f20454bf29df024b99c67b05884a
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-23 10:33:15 +0900

    [fix]

--- PushToFCM/index.js
+++ PushToFCM/index.js
@@ -35,7 +35,7 @@ app.post('/webpushcallback', rawBodyMiddlware, decodeBodyMiddleware ,(req, res)=
     let deviceToken = req.query.deviceToken
     let accountId = req.query.accountId;
     let rawJson = req.rawJson;
-    console.log(rawJson);
+    console.log(JSON.parse(rawJson));
     if(!(deviceToken && accountId)) {
         return cres.status(422).end();
     }
