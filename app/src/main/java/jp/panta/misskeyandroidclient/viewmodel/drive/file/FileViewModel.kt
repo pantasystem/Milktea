e99203de6f4d21d94ee97671cb543820286448da
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-30 16:06:10 +0900

    実装した！！

--- app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/drive/file/FileViewModel.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/drive/file/FileViewModel.kt
@@ -8,198 +8,120 @@ import jp.panta.misskeyandroidclient.api.throwIfHasError
 import jp.panta.misskeyandroidclient.model.Encryption
 import jp.panta.misskeyandroidclient.api.MisskeyAPI
 import jp.panta.misskeyandroidclient.model.account.Account
-import jp.panta.misskeyandroidclient.model.drive.FileUploader
 import jp.panta.misskeyandroidclient.api.drive.RequestFile
-import jp.panta.misskeyandroidclient.model.drive.FileProperty
-import jp.panta.misskeyandroidclient.model.drive.FilePropertyDataSource
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import jp.panta.misskeyandroidclient.model.account.CurrentAccountWatcher
+import jp.panta.misskeyandroidclient.model.drive.*
+import jp.panta.misskeyandroidclient.model.file.File
+import jp.panta.misskeyandroidclient.viewmodel.MiCore
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
+/**
+ * 選択状態とFileの読み込み＆表示を担当する
+ */
+@ExperimentalCoroutinesApi
 class FileViewModel(
-    private val account: Account,
-    private val misskeyAPI: MisskeyAPI,
-    private val selectedFileMapLiveData: MutableLiveData<Map<FileProperty.Id, FileViewData>>?,
-    private val maxSelectableItemSize: Int,
-    folderId: String?,
-    private val encryption: Encryption,
-    private val filePropertyDataSource: FilePropertyDataSource
+    private val currentAccountWatcher: CurrentAccountWatcher,
+    currentDirectoryId: String?,
+    private val miCore: MiCore,
+    private val path: DirectoryPath,
+    private val selectedFilePropertyIds: SelectedFilePropertyIds
+
 ) : ViewModel(){
 
-    val filesLiveData = MutableLiveData<List<FileViewData>>()
-    val isSelectable = MutableLiveData<Boolean>(selectedFileMapLiveData != null)
-
-    val isRefreshing = MutableLiveData<Boolean>(false)
-
-    val currentFolder = MutableLiveData<String>(folderId)
+    private val filePropertiesPagingStore = miCore.filePropertyPagingStore(null, currentDirectoryId)
+    private val _error = MutableStateFlow<Throwable?>(null)
+    val error: StateFlow<Throwable?> get() = _error
 
 
+    @ExperimentalCoroutinesApi
+    private val account = currentAccountWatcher.account.shareIn(viewModelScope, SharingStarted.Eagerly, replay = 1)
 
-    private var isLoading = false
+    val state = filePropertiesPagingStore.state.map { state ->
+        state.convert {
+            runBlocking {
+                it.map { id ->
+                    miCore.getFilePropertyDataSource().find(id)
+                }
+            }
+        }
+    }.combine(selectedFilePropertyIds.state) { p, ids ->
+        p.convert {
+            it.map { property ->
+                FileViewData(property, ids.contains(property.id))
+            }
+        }
+    }
 
-    fun getSelectedItems(): List<FileViewData>?{
-        //return selectedItemMap.values.toList()
-        return selectedFileMapLiveData?.value?.values?.toList()
+
+    init {
+        /**
+         * DirectoryPathの現在のCurrentDirをFilePropertiesPagingStoreへ伝達します。
+         */
+        path.route.onEach {
+            filePropertiesPagingStore.setCurrentDirectory(it.lastOrNull())
+            filePropertiesPagingStore.loadPrevious()
+        }.launchIn(viewModelScope + Dispatchers.IO)
+
+        /**
+         * アカウントの状態をDirectoryPath, FilePropertiesPagingStoreへ伝達します。
+         */
+        account.distinctUntilChangedBy {
+            it.accountId
+        }.onEach {
+            path.clear()
+            filePropertiesPagingStore.setAccount(it)
+        }.launchIn(viewModelScope + Dispatchers.IO)
+
+
     }
 
     fun loadInit(){
-        if(isLoading){
+        if(filePropertiesPagingStore.isLoading) {
             return
         }
-        isLoading = true
-        isRefreshing.postValue(true)
-        val request = RequestFile(i = account.getI(encryption), folderId = currentFolder.value, limit = 20)
         viewModelScope.launch(Dispatchers.IO) {
-            runCatching{
-                val rawList = misskeyAPI.getFiles(request).throwIfHasError().body()
-                if(rawList == null){
-                    isLoading = false
-                    isRefreshing.postValue(false)
-                    return@launch
-                }
-
-                val entities  = rawList.map {
-                    it.toFileProperty(account)
-                }
-                filePropertyDataSource.addAll(entities)
-                entities.map{
-                    FileViewData(it)
-                }
-            }.onSuccess { viewDataList ->
-                filesLiveData.postValue(viewDataList)
-                //selectedItemMap.clear()
-                viewDataList.forEach{
-                    //val selected = selectedItemMap[it.id]
-                    val selected = selectedFileMapLiveData?.value?.get(it.id)
-                    if(selected != null){
-                        it.isSelect.postValue(true)
-                    }else if((selectedFileMapLiveData?.value?.size ?: 0) >= maxSelectableItemSize){
-                        it.isEnabledSelect.postValue(false)
-                    }
-
-                }
-
-                isLoading = false
-                isRefreshing.postValue(false)
+            runCatching {
+                filePropertiesPagingStore.clear()
+                filePropertiesPagingStore.loadPrevious()
             }.onFailure {
-                isLoading = false
-                isRefreshing.postValue(false)
+                _error.value = it
             }
         }
 
     }
 
     fun loadNext(){
-        if(isLoading){
+        if(filePropertiesPagingStore.isLoading) {
             return
         }
-        isLoading = true
-        val beforeList = filesLiveData.value
-        val untilId  = beforeList?.lastOrNull()?.id
-        if(beforeList == null || untilId == null){
-            isLoading = false
-            return
-        }
-        val request = RequestFile(i = account.getI(encryption), folderId = currentFolder.value, limit = 20, untilId = untilId.fileId)
-
         viewModelScope.launch(Dispatchers.IO) {
             runCatching {
-                val rawList = misskeyAPI.getFiles(request).throwIfHasError().body()
-
-                requireNotNull(rawList)
-                require(rawList.isNotEmpty())
-                val entities = rawList.map {
-                    it.toFileProperty(account)
-                }
-                filePropertyDataSource.addAll(entities)
-
-                entities.map{
-                    FileViewData(it).apply{
-                        val selected = selectedFileMapLiveData?.value?.get(it.id)
-                        if(selected != null){
-                            isSelect.postValue(true)
-                        }else{
-                            isEnabledSelect.postValue(
-                                (selectedFileMapLiveData?.value?.size ?: 0) < maxSelectableItemSize
-                            )
-                        }
-                    }
-                }
-            }.onSuccess { viewDataList ->
-                filesLiveData.postValue(ArrayList(beforeList).also {
-                    it.addAll(viewDataList)
-                })
+                filePropertiesPagingStore.loadPrevious()
             }.onFailure {
-                isLoading = false
+                _error.value = it
             }
-
         }
-
     }
 
-    fun changeSelectItemState(fileViewData: FileViewData){
-        /*if(selectedItemMap.size >= maxSelectableItemSize)
-            return*/
 
-        //nullはfalseとして扱う
-        val tmp = selectedFileMapLiveData?.value
-        val selectedItemMap =
-            if(tmp != null){
-            HashMap<FileProperty.Id, FileViewData>(tmp)
+    fun toggleSelect(id: FileProperty.Id) {
+        if (selectedFilePropertyIds.exists(id)) {
+            selectedFilePropertyIds.remove(id)
         }else{
-            HashMap()
-        }
-        val isSelect = fileViewData.isSelect.value
-        if(isSelect == null){
-            fileViewData.isSelect.postValue(true)
-            selectedItemMap[fileViewData.id] = fileViewData
-            selectedFileMapLiveData?.value = selectedItemMap
-            if(selectedItemMap.size >= maxSelectableItemSize){
-                allDisabledSelect()
-            }
-            return
-        }
-
-        if(isSelect){
-            selectedItemMap.remove(fileViewData.id)
-            fileViewData.isSelect.postValue(false)
-            selectedFileMapLiveData?.value = selectedItemMap
-            allEnabledSelect()
-            Log.d("FileViewModel", "解除した")
-            /*if(selectedItemMap.size < maxSelectableItemSize){
-            }*/
-        }else{
-            selectedItemMap[fileViewData.id] = fileViewData
-            fileViewData.isSelect.postValue(true)
-            selectedFileMapLiveData?.value = selectedItemMap
-            if(selectedItemMap.size >= maxSelectableItemSize){
-                allDisabledSelect()
-            }
+            selectedFilePropertyIds.add(id)
         }
     }
 
-    private fun allDisabledSelect(){
-        filesLiveData.value?.forEach{
-            //val item = selectedItemMap[it.id]
-            val item = selectedFileMapLiveData?.value?.get(it.id)
-            if(item == null){
-                it.isEnabledSelect.postValue(false)
-            }
-        }
-    }
-
-    private fun allEnabledSelect(){
-        filesLiveData.value?.forEach{
-            it.isEnabledSelect.postValue(true)
-        }
-    }
-
-    fun uploadFile(file: jp.panta.misskeyandroidclient.model.file.File, fileUploader: FileUploader){
-        val uploadFile = file.copy(folderId = currentFolder.value)
+    fun uploadFile(file: File){
+        val uploadFile = file.copy(folderId = path.route.value.lastOrNull()?.id)
 
         viewModelScope.launch(Dispatchers.IO) {
             try{
-                fileUploader.upload(uploadFile, true).let {
-                    filePropertyDataSource.add(it.toFileProperty(account))
+                val account = currentAccountWatcher.getAccount()
+                val uploader = miCore.getFileUploaderProvider().get(account)
+                uploader.upload(uploadFile, true).let {
+                    miCore.getFilePropertyDataSource().add(it.toFileProperty(account))
                 }
             }catch(e: Exception){
                 Log.d("DriveViewModel", "ファイルアップロードに失敗した")
