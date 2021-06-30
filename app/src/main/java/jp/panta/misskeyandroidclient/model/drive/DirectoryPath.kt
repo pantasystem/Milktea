bbf4ce4c4cea3e81462d57a50e3623aa6bcaa1f4
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-30 17:24:28 +0900

    Statelessにしました。

--- app/src/main/java/jp/panta/misskeyandroidclient/model/drive/DirectoryPath.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/drive/DirectoryPath.kt
@@ -1,50 +1,50 @@
 package jp.panta.misskeyandroidclient.model.drive
 
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
 /**
  * Rootから現在にいる経路を表すPath
  */
-class DirectoryPath {
+data class DirectoryPath(
+    val path: List<Directory>
+) {
 
-    private val _route = MutableStateFlow<List<Directory>>(emptyList())
-    val route: StateFlow<List<Directory>> get() = _route
-
-    fun push(directory: Directory) {
-        if(route.value.lastOrNull()?.id != directory.parentId) {
-            throw IllegalArgumentException("不正なDirectory")
-        }
-        _route.value = _route.value.toMutableList().also {
-            it.add(directory)
-        }
+    fun push(directory: Directory) : DirectoryPath {
+       require(path.lastOrNull()?.id == directory.parentId) {
+           "親子関連のない不正なDirectory"
+       }
+       return this.copy(
+           path = this.path.toMutableList().also {
+               it.add(directory)
+           }
+       )
     }
 
-    fun pop() : Boolean {
-        val list = _route.value
-        if(list.isEmpty()) {
-            return false
+    fun pop() : DirectoryPath {
+        if(path.isEmpty()) {
+            return this
         }
-        this._route.value = list.subList(0, list.size - 1)
-        return true
+        return this.copy(path = path.subList(0, path.size - 1))
     }
 
-    fun popUntil(directory: Directory) : Boolean{
-        val list = _route.value
-        val index = list.lastIndexOf(directory)
+
+    fun popUntil(directory: Directory) : DirectoryPath{
+        val index = path.lastIndexOf(directory)
         if(index == -1) {
-            return false
+            return this
         }
-        this._route.value = this.route.value.subList(0, index)
-        return true
+        return this.copy(
+            path = this.path.subList(0, index)
+        )
     }
 
-    fun clear() {
-        this._route.value = emptyList()
+    fun clear() : DirectoryPath{
+        return this.copy(
+            path = emptyList()
+        )
     }
 
     fun isCurrentRoot() : Boolean{
-        return this.route.value.isEmpty()
+        return this.path.isEmpty()
     }
 
 }
