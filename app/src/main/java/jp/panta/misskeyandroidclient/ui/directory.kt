61d8c3177075959e1c6980ac2de8b57cc19274ba
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-07-04 00:27:59 +0900

    Composeのビルドが通るように修正しました。

--- app/src/main/java/jp/panta/misskeyandroidclient/ui/directory.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/directory.kt
@@ -1,8 +1,13 @@
 package jp.panta.misskeyandroidclient.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import jp.panta.misskeyandroidclient.model.drive.Directory
@@ -11,6 +16,9 @@ import androidx.compose.runtime.getValue
 
 
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import jp.panta.misskeyandroidclient.viewmodel.drive.DriveViewModel
 import jp.panta.misskeyandroidclient.viewmodel.drive.directory.DirectoryViewData
 
@@ -30,21 +38,33 @@ fun DirectoryListScreen(viewModel: DirectoryViewModel, driveViewModel: DriveView
 
 @Composable
 fun DirectoryListTile(directory: Directory, onClick:()->Unit) {
-    Column {
-        Text(
-            "hogehoeg"
-        )
+    Card (
+        shape = RoundedCornerShape(0.dp),
+        modifier = Modifier.padding(0.5.dp)
+    ){
+        Column(
+            Modifier
+                .clickable(onClick = onClick)
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text(
+                directory.name,
+                fontSize = 20.sp
+            )
+        }
     }
+
 }
 
 @Composable
 fun DirectoryListView(directories: List<Directory>, onDirectorySelected: (Directory)->Unit) {
     LazyColumn {
-        this.items(directories, {
-            it.id
-        }){
-            DirectoryListTile(it) {
-                onDirectorySelected.invoke(it)
+        this.itemsIndexed(directories, { index, _ ->
+            directories[index].id
+        }){ _, item ->
+            DirectoryListTile(item) {
+                onDirectorySelected.invoke(item)
             }
         }
     }
