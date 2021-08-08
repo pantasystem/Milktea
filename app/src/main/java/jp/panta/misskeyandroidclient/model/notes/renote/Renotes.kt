6be484a08993398d0c3ee7eea2e1175d2ec6604d
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-08-09 00:17:58 +0900

    ページネーションするためのServiceを作成＆実装しました

--- app/src/main/java/jp/panta/misskeyandroidclient/model/notes/renote/Renotes.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/notes/renote/Renotes.kt
@@ -17,12 +17,6 @@ data class Renotes (
     val renotes: List<Renote>,
     val targetNoteId: Note.Id
 ) {
-    @CheckResult()
-    fun addAllLast(list: List<Renote>) : Renotes{
-        return this.copy(renotes = renotes.toMutableList().also{
-            it.addAll(list)
-        })
-    }
 
     val quotes: List<Renote.Quote>
         get() = this.renotes.mapNotNull {
