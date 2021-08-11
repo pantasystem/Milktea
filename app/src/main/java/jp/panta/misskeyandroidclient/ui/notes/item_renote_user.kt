17036a456045cb4921f60ad12a27161a121dccb7
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-08-11 16:08:26 +0900

    アイコンを円形にしました。

--- app/src/main/java/jp/panta/misskeyandroidclient/ui/notes/item_renote_user.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/notes/item_renote_user.kt
@@ -3,6 +3,7 @@ package jp.panta.misskeyandroidclient.ui.notes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
@@ -10,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -51,7 +53,8 @@ fun ItemRenoteUser(
             Image(
                 painter = rememberImagePainter(note.user.avatarUrl),
                 contentDescription = null,
-                modifier = Modifier.size(48.dp),
+                modifier = Modifier.size(48.dp)
+                    .clip(CircleShape),
                 contentScale = ContentScale.Crop
             )
             Spacer(modifier = Modifier.width(16.dp))
