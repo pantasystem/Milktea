47540d70570d5559ef6d876c14cae5a5445ece2f
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-08-11 16:42:19 +0900

    時間を表示するようにしました

--- app/src/main/java/jp/panta/misskeyandroidclient/ui/notes/item_renote_user.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/notes/item_renote_user.kt
@@ -13,11 +13,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
+import jp.panta.misskeyandroidclient.R
 import jp.panta.misskeyandroidclient.model.notes.NoteCaptureAPIAdapter
 import jp.panta.misskeyandroidclient.model.notes.NoteRelation
 import jp.panta.misskeyandroidclient.ui.components.CustomEmojiText
+import jp.panta.misskeyandroidclient.view.SimpleElapsedTime
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.launchIn
@@ -32,6 +35,21 @@ fun ItemRenoteUser(
     isUserNameDefault: Boolean = false
 ) {
 
+    val stringResourceMap = mapOf<SimpleElapsedTime.TimeUnit, String>(
+        SimpleElapsedTime.TimeUnit.YEAR to stringResource(R.string.year_ago),
+        SimpleElapsedTime.TimeUnit.MONTH to stringResource(R.string.month_ago),
+        SimpleElapsedTime.TimeUnit.DATE to stringResource(R.string.date_ago),
+        SimpleElapsedTime.TimeUnit.HOUR to stringResource(R.string.hour_ago),
+        SimpleElapsedTime.TimeUnit.MINUTE to stringResource(R.string.minute_ago),
+        SimpleElapsedTime.TimeUnit.SECOND to stringResource(R.string.second_ago),
+        SimpleElapsedTime.TimeUnit.NOW to stringResource(R.string.now),
+        SimpleElapsedTime.TimeUnit.FUTURE to stringResource(R.string.future)
+    )
+
+    val createdAt = SimpleElapsedTime{
+        stringResourceMap[it] ?: throw IllegalStateException("対応する文字列リソースを発見することができませんでした")
+    }.format(note.note.createdAt)
+
     LaunchedEffect(key1 = true,){
         withContext(Dispatchers.IO) {
             noteCaptureAPIAdapter?.capture(note.note.id)?.launchIn(this)
@@ -40,36 +58,49 @@ fun ItemRenoteUser(
 
     Card(
         shape = RoundedCornerShape(0.dp),
-        modifier = Modifier.padding(0.5.dp).clickable {
-            onClick.invoke()
-        }
+        modifier = Modifier
+            .padding(0.5.dp)
+            .clickable {
+                onClick.invoke()
+            }
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
+
         ) {
-            Image(
-                painter = rememberImagePainter(note.user.avatarUrl),
-                contentDescription = null,
-                modifier = Modifier.size(48.dp)
-                    .clip(CircleShape),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            Column {
-                if(isUserNameDefault){
-                    CustomEmojiText(text = note.user.getDisplayUserName(), emojis = note.user.emojis)
-                }else{
-                    CustomEmojiText(text = note.user.getDisplayName(), emojis = note.user.emojis)
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = rememberImagePainter(note.user.avatarUrl),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Column {
+                    if(isUserNameDefault){
+                        CustomEmojiText(text = note.user.getDisplayUserName(), emojis = note.user.emojis)
+                    }else{
+                        CustomEmojiText(text = note.user.getDisplayName(), emojis = note.user.emojis)
+                    }
+                    if(isUserNameDefault){
+                        Text(text = note.user.getDisplayName())
+                    }else{
+                        Text(text = note.user.getDisplayUserName())
+                    }
                 }
-                if(isUserNameDefault){
-                    Text(text = note.user.getDisplayName())
-                }else{
-                    Text(text = note.user.getDisplayUserName())
-                }
+
+
             }
+            Text(createdAt)
 
         }
     }
