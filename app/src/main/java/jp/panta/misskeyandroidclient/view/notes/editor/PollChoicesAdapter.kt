0ece92c755e79650853b901ff32dfd105500debb
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-11-24 12:52:05 +0900

    投票の候補をリストで線形表示できるようにするためのAdapterを再実装しました。

--- app/src/main/java/jp/panta/misskeyandroidclient/view/notes/editor/PollChoicesAdapter.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/view/notes/editor/PollChoicesAdapter.kt
@@ -1,7 +1,11 @@
 package jp.panta.misskeyandroidclient.view.notes.editor
 
+import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.DiffUtil
@@ -9,32 +13,59 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import jp.panta.misskeyandroidclient.R
 import jp.panta.misskeyandroidclient.databinding.ItemEditPollChoiceBinding
-import jp.panta.misskeyandroidclient.viewmodel.notes.editor.poll.PollChoice
-import jp.panta.misskeyandroidclient.viewmodel.notes.editor.poll.PollEditor
+import jp.panta.misskeyandroidclient.model.notes.PollChoiceState
+import java.util.*
 
-class PollChoicesAdapter(private val pollEditor: PollEditor, private val lifecycleOwner: LifecycleOwner) : ListAdapter<PollChoice, PollChoicesAdapter.ChoiceHolder>(ItemCallback()){
-    class ItemCallback() : DiffUtil.ItemCallback<PollChoice>(){
-        override fun areContentsTheSame(oldItem: PollChoice, newItem: PollChoice): Boolean {
+class PollChoicesAdapter(
+    private val lifecycleOwner: LifecycleOwner,
+    private val onChoiceTextChangedListener: (UUID, String)-> Unit,
+    private val onChoiceDeleteButtonClickListener: (UUID) -> Unit
+) : ListAdapter<PollChoiceState, PollChoicesAdapter.ChoiceHolder>(ItemCallback()){
+    class ItemCallback() : DiffUtil.ItemCallback<PollChoiceState>(){
+        override fun areContentsTheSame(oldItem: PollChoiceState, newItem: PollChoiceState): Boolean {
             return oldItem == newItem
         }
 
-        override fun areItemsTheSame(oldItem: PollChoice, newItem: PollChoice): Boolean {
+        override fun areItemsTheSame(oldItem: PollChoiceState, newItem: PollChoiceState): Boolean {
             return oldItem.id == newItem.id
         }
     }
-    class ChoiceHolder(val binding: ItemEditPollChoiceBinding) : RecyclerView.ViewHolder(binding.root)
+    class ChoiceHolder(
+        val binding: ItemEditPollChoiceBinding,
+        private val onChoiceTextChangedListener: (UUID, String) -> Unit,
+        private val onChoiceDeleteButtonClickListener: (UUID) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        private var choice: PollChoiceState? = null
+
+        val textChangedListener: (Editable?)->Unit = {
+            choice?.let{ choice ->
+                onChoiceTextChangedListener.invoke(choice.id, it?.toString()?: "")
+            }
+        }
+        fun bind(choice: PollChoiceState) {
+            this.choice = choice
+            binding.deleteButton.setOnClickListener {
+                onChoiceDeleteButtonClickListener.invoke(choice.id)
+            }
+            binding.choice = choice
+
+        }
+    }
 
     override fun onBindViewHolder(holder: ChoiceHolder, position: Int) {
-        holder.binding.choice = getItem(position)
-        holder.binding.pollViewModel = pollEditor
+        holder.bind(getItem(position))
+        Log.d("PollChoicesAdapter", "onBindViewHolder")
         holder.binding.lifecycleOwner = lifecycleOwner
-        holder.binding.executePendingBindings()
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChoiceHolder {
         val binding = DataBindingUtil.inflate<ItemEditPollChoiceBinding>(LayoutInflater.from(parent.context), R.layout.item_edit_poll_choice, parent, false)
-        return ChoiceHolder(binding)
+        val holder = ChoiceHolder(binding, onChoiceTextChangedListener, onChoiceDeleteButtonClickListener)
+        binding.inputChoiceText.addTextChangedListener {
+            holder.textChangedListener.invoke(it)
+        }
+        return holder
     }
 
 }
