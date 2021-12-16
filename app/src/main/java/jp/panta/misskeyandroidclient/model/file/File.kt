497457c02608fd0fcb85097dd5212660da3048bd
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-12-16 22:27:09 +0900

    [change] ローカルのファイルなのかリモートのファイルなのか識別しにくかったため 識別しやすいようにRemote, Localで識別できるAppFileを作成しました。

--- app/src/main/java/jp/panta/misskeyandroidclient/model/file/File.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/model/file/File.kt
@@ -3,13 +3,33 @@ package jp.panta.misskeyandroidclient.model.file
 import jp.panta.misskeyandroidclient.model.drive.FileProperty
 import java.io.Serializable
 
+sealed interface AppFile {
+
+    data class Local(
+        val name: String,
+        val path: String,
+        val type: String,
+        val thumbnailUrl: String?,
+        val isSensitive: Boolean,
+        val id: Long = 0,
+        ) : AppFile
+    data class Remote(
+        val id: FileProperty.Id,
+    ) : AppFile
+
+}
+
 data class File(
     val name: String,
-    val path: String,
+    val path: String?,
     val type: String?,
     val remoteFileId: FileProperty.Id?,
     val localFileId: Long?,
     val thumbnailUrl: String?,
     val isSensitive: Boolean?,
     val folderId: String? = null
-): Serializable
+): Serializable {
+    val isRemoteFile: Boolean
+        get() = remoteFileId != null
+}
+
