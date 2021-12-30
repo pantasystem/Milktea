97d3ebfd11c70b246ffefda8193fed61fea38070
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-12-30 18:21:19 +0900

    [change] progressを表示するようにしました。

--- app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/auth/app/AppAuthViewModel.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/auth/app/AppAuthViewModel.kt
@@ -2,21 +2,28 @@ package jp.panta.misskeyandroidclient.viewmodel.auth.app
 
 import android.util.Log
 import androidx.lifecycle.*
+import androidx.lifecycle.map
 import jp.panta.misskeyandroidclient.api.MisskeyAPIServiceBuilder
 import jp.panta.misskeyandroidclient.api.app.CreateApp
 import jp.panta.misskeyandroidclient.api.throwIfHasError
 import jp.panta.misskeyandroidclient.model.api.Version
 import jp.panta.misskeyandroidclient.api.auth.AppSecret
+import jp.panta.misskeyandroidclient.api.auth.Session
 import jp.panta.misskeyandroidclient.model.auth.Authorization
 import jp.panta.misskeyandroidclient.model.auth.custom.*
 import jp.panta.misskeyandroidclient.model.instance.Meta
+import jp.panta.misskeyandroidclient.util.State
+import jp.panta.misskeyandroidclient.util.StateContent
 import jp.panta.misskeyandroidclient.viewmodel.MiCore
 import jp.panta.misskeyandroidclient.viewmodel.auth.Permissions
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
 import java.lang.IllegalStateException
 import java.util.*
 import java.util.regex.Pattern
+
 
 @Suppress("UNCHECKED_CAST")
 class AppAuthViewModel(
@@ -35,53 +42,66 @@ class AppAuthViewModel(
 
     private val urlPattern = Pattern.compile("""(https?)(://)([-_.!~*'()\[\]a-zA-Z0-9;/?:@&=+${'$'},%#]+)""")
 
-    val instanceDomain = MutableLiveData<String>()
-    val meta = MediatorLiveData<Meta?>()
+    val instanceDomain = MutableStateFlow<String>("")
+    private val _metaState = MutableStateFlow<State<Meta>>(State.Fixed(StateContent.NotExist()))
+    private val metaState: StateFlow<State<Meta>> = _metaState
+    private val _generatingTokenState = MutableStateFlow<State<Session>>(State.Fixed(StateContent.NotExist()))
+    private val generatingTokenState: StateFlow<State<Session>> = _generatingTokenState
 
-    val validatedInstanceDomain = MediatorLiveData<Boolean>()
+    private val isFetchingMeta = metaState.map {
+        it is State.Loading
+    }.stateIn(viewModelScope, SharingStarted.Lazily, false)
+
+    private val isGeneratingToken = generatingTokenState.map {
+        it is State.Loading
+    }.stateIn(viewModelScope, SharingStarted.Lazily, false)
+
+    val isProgress = combine(isFetchingMeta, isGeneratingToken) { a, b ->
+        a || b
+    }.stateIn(viewModelScope, SharingStarted.Lazily, false)
+
+    val validatedInstanceDomain = metaState.map {
+        it is State.Fixed && it.content is StateContent.Exist
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, false)
 
     val appName = MutableLiveData<String>()
-    val validatedAppName = Transformations.map(appName){
-        it.isNotBlank()
-    }
+
 
     val app = MutableLiveData<App?>()
     val waiting4UserAuthorization = MutableLiveData<Authorization.Waiting4UserAuthorization?>()
 
-    val generatingToken = MutableLiveData<Boolean>(false)
 
     private val metaStore = miCore.getMetaStore()
     private val misskeyAPIProvider = miCore.getMisskeyAPIProvider()
 
     init{
-        meta.addSource(instanceDomain){ base ->
+        instanceDomain.onEach { base ->
             val url = toEnableUrl(base)
             if(urlPattern.matcher(url).find()){
-
-                viewModelScope.launch(Dispatchers.IO) {
-                    runCatching {
-                        metaStore.get(url)
-                    }.onFailure {
-                        meta.postValue(null)
-                    }.onSuccess {
-                        meta.postValue(it)
-                    }
+                _metaState.value = State.Loading(_metaState.value.content)
+                runCatching {
+                    metaStore.get(url)
+                }.onFailure {
+                    _metaState.value = State.Error(_metaState.value.content, it)
+                }.onSuccess {
+                    _metaState.value = State.Fixed(
+                        if(it == null) StateContent.NotExist() else StateContent.Exist(it)
+                    )
                 }
             }
-        }
-        validatedInstanceDomain.addSource(meta){
-            validatedInstanceDomain.value = it != null
-        }
+        }.launchIn(viewModelScope + Dispatchers.IO)
+
     }
 
 
     fun auth(){
-        generatingToken.value = true
-        val url = this.instanceDomain.value?: return
+        val url = this.instanceDomain.value
         val instanceBase = toEnableUrl(url)
         val appName = this.appName.value?: return
-        val meta = this.meta.value?: return
+        val meta = (this.metaState.value.content as? StateContent.Exist)?.rawContent
+            ?: return
         viewModelScope.launch(Dispatchers.IO){
+            _generatingTokenState.value = State.Loading(generatingTokenState.value.content)
             runCatching {
                 val app = createApp(instanceBase, meta.getVersion(), appName)
                 this@AppAuthViewModel.app.postValue(app)
@@ -100,11 +120,11 @@ class AppAuthViewModel(
                 )
             }.onSuccess { w4a ->
                 this@AppAuthViewModel.waiting4UserAuthorization.postValue(w4a)
+                _generatingTokenState.value = State.Fixed(StateContent.Exist(w4a.session))
 
             }.onFailure {
                 Log.e("AppAuthViewModel", "認証開始処理失敗", it)
-            }.also {
-                generatingToken.postValue(false)
+                _generatingTokenState.value = State.Error(StateContent.NotExist(), it)
             }
 
         }
