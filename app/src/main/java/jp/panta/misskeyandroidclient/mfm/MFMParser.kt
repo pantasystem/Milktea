4610ce20c3b305e92ad8514cc4c7ca6223406630
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-02-03 16:42:01 +0900

    [fix] 修正

--- app/src/main/java/jp/panta/misskeyandroidclient/mfm/MFMParser.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/mfm/MFMParser.kt
@@ -273,7 +273,7 @@ object MFMParser{
 
         private val titlePattern = Pattern.compile("""\A[【\[]([^\n\[\]【】]+)[】\]](\n|\z)""")
 
-        private val searchPattern = Pattern.compile("""^(.+?) (\[(?i)Search]|検索|\[検索]|(?i)Search)$""", Pattern.MULTILINE)
+        private val searchPattern = Pattern.compile("""^(.+?)[ |　]((?i)Search|検索|\[検索]|\[Search])$""", Pattern.MULTILINE)
         private val linkPattern = Pattern.compile("""\??\[(.+?)]\((https?|ftp)(://[-_.!~*'()a-zA-Z0-9;/?:@&=+${'$'},%#]+)\)""")
 
         private fun parseTitle(): Node?{
