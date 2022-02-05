55ad38edd6d20d09f5a6716fad12524eb06097d7
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-02-05 17:59:41 +0900

    [change] Flowを使用するようにしました。

--- app/src/main/java/jp/panta/misskeyandroidclient/ui/users/viewmodel/search/SearchUserViewModel.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/users/viewmodel/search/SearchUserViewModel.kt
@@ -1,21 +1,30 @@
 package jp.panta.misskeyandroidclient.ui.users.viewmodel.search
 
 import androidx.lifecycle.*
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.subjects.PublishSubject
-import jp.panta.misskeyandroidclient.model.account.Account
-import jp.panta.misskeyandroidclient.api.users.RequestUser
-import jp.panta.misskeyandroidclient.api.users.SearchByUserAndHost
-import jp.panta.misskeyandroidclient.api.users.toUser
-import jp.panta.misskeyandroidclient.model.notes.NoteDataSourceAdder
-import jp.panta.misskeyandroidclient.viewmodel.MiCore
+import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.panta.misskeyandroidclient.ui.users.viewmodel.UserViewData
+import jp.panta.misskeyandroidclient.util.State
+import jp.panta.misskeyandroidclient.util.StateContent
+import jp.panta.misskeyandroidclient.util.asLoadingStateFlow
+import jp.panta.misskeyandroidclient.viewmodel.MiCore
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.launch
-import java.util.concurrent.TimeUnit
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.*
+import java.util.regex.Pattern
 
+
+data class SearchUser(
+    val word: String,
+    val host: String?
+) {
+    val isUserName: Boolean
+        get() = Pattern.compile("""^[a-zA-Z_\-0-9]+$""")
+            .matcher(word)
+            .find()
+
+}
 /**
  * SearchAndSelectUserViewModelを将来的にこのSearchUserViewModelと
  * SelectedUserViewModelに分離する予定
@@ -24,127 +33,91 @@ import java.util.concurrent.TimeUnit
 @ExperimentalCoroutinesApi
 class SearchUserViewModel(
     val miCore: MiCore,
-    private val hasDetail: Boolean?,
-    private val noteDataSourceAdder: NoteDataSourceAdder = NoteDataSourceAdder(miCore.getUserDataSource(), miCore.getNoteDataSource(), miCore.getFilePropertyDataSource())
 ) : ViewModel(){
 
     private val logger = miCore.loggerFactory.create("SearchUserViewModel")
 
     @Suppress("UNCHECKED_CAST")
-    class Factory(val miCore: MiCore, private val hasDetail: Boolean?) : ViewModelProvider.Factory{
+    class Factory(val miCore: MiCore) : ViewModelProvider.Factory{
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return SearchUserViewModel(miCore, hasDetail) as T
+            return SearchUserViewModel(miCore) as T
         }
     }
 
-    private val compositeDisposable = CompositeDisposable()
 
-    private val searchUserRequests = PublishSubject.create<RequestUser>()
+    private val searchUserRequests = MutableSharedFlow<SearchUser>(
+        replay = 0,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST,
+        extraBufferCapacity = 25
+    )
+
     val userName = MutableLiveData<String>()
     val host = MutableLiveData<String>()
 
-    val isLoading = MutableLiveData<Boolean>()
-
-    private val users = object : MediatorLiveData<List<UserViewData>>(){
-        override fun onActive() {
-            super.onActive()
-
-            addRequestSubscriber()
+    private val searchState = miCore.getCurrentAccount().filterNotNull()
+        .flatMapLatest { account ->
+            searchUserRequests.distinctUntilChanged()
+                .flatMapLatest {
+                    suspend {
+                        if (it.isUserName) {
+                            miCore.getUserRepository()
+                                .searchByUserName(
+                                    accountId = account.accountId,
+                                    userName = it.word,
+                                    host = it.host
+                                )
+                        } else {
+                            miCore.getUserRepository()
+                                .searchByName(
+                                    accountId = account.accountId,
+                                    name = it.word
+                                )
+                        }
+                    }.asLoadingStateFlow()
+                }
+        }.flowOn(Dispatchers.IO)
+        .onEach {
+            logger.debug("検索状態:$it")
         }
-
-        override fun onInactive() {
-            super.onInactive()
-            compositeDisposable.clear()
-
+        .catch { error ->
+            logger.info("ユーザー検索処理に失敗しました", e = error)
         }
-    }.apply{
-        addSource(userName){
+        .stateIn(viewModelScope, SharingStarted.Lazily, State.Fixed(StateContent.NotExist()))
+
+    val isLoading = searchState.map {
+        it is State.Loading
+    }.asLiveData()
+
+    val users = searchState.map {
+        (it.content as? StateContent.Exist)?.rawContent
+            ?: emptyList()
+    }.map {
+        it.map { u ->
+            UserViewData(u, miCore, viewModelScope)
+        }
+    }.asLiveData()
+
+    init {
+        userName.observeForever {
             search()
         }
-        addSource(host){
+
+        host.observeForever {
             search()
         }
-
-    }
-
-    private var mSearchByUserAndHost: SearchByUserAndHost? = null
-    private var mNowInstanceBase: String? = null
-
-
-    fun getUsers(): LiveData<List<UserViewData>> {
-        return users
     }
 
     fun search(){
         val userName = this.userName.value?: return
         val host = this.host.value
 
-        val request = RequestUser(
-            i = getAccount()?.getI(miCore.getEncryption())!!,
-            userName = userName,
-            userId = null,
+        val request = SearchUser(
             host = host,
-            detail = hasDetail
+            word = userName
         )
-        searchUserRequests.onNext(request)
-    }
-    fun search(request: RequestUser){
-
-        // TODO Service層などに切り出すなどをしてリファクタリングをする
-        viewModelScope.launch(Dispatchers.IO) {
-            val account = miCore.getAccountRepository().getCurrentAccount()
-            isLoading.postValue(true)
-            val resUsers = runCatching {
-                getSearchByUserAndHost()?.search(request)?.body()
-            }.onFailure {
-                logger.error("request api/users/searchエラー", it)
-            }.getOrNull()?.map {
-                val u = it.toUser(account, isDetail = hasDetail?:false).also{ u ->
-                    miCore.getUserDataSource().add(u)
-                }
-                it.pinnedNotes?.forEach { dto ->
-                    noteDataSourceAdder.addNoteDtoToDataSource(account, dto)
-                }
-                UserViewData(u, miCore, viewModelScope)
-            }?: emptyList()
-            users.postValue(resUsers)
-            isLoading.postValue(false)
-        }
-
+        searchUserRequests.tryEmit(request)
     }
 
-    private fun getSearchByUserAndHost(): SearchByUserAndHost?{
-        try{
-            val account = miCore.getCurrentAccount().value
 
-            if(account?.instanceDomain == null){
-                return null
-            }
-            if(mNowInstanceBase != account.instanceDomain){
-                miCore.getMisskeyAPIProvider().get(account).let{ api ->
-                    mNowInstanceBase = account.instanceDomain
-                    mSearchByUserAndHost = SearchByUserAndHost(api)
-                }
-            }
-
-            return mSearchByUserAndHost
-        }catch(e: Exception){
-            return null
-        }
-
-    }
-
-    private fun getAccount(): Account?{
-        return miCore.getCurrentAccount().value
-    }
-
-    private fun addRequestSubscriber(){
-        val disposable = searchUserRequests.distinctUntilChanged()
-            .debounce(500, TimeUnit.MILLISECONDS)
-            .subscribe {
-                search(it)
-            }
-        compositeDisposable.add(disposable)
-    }
 
 }
