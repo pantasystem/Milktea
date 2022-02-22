125625dcb26ced9f96ba30c3f2a9e3b7363fc56f
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-02-22 15:00:00 +0900

    [add] 単体テストを追加しました

---
+++ app/src/test/java/jp/panta/misskeyandroidclient/model/notes/poll/PollTest.kt
@@ -0,0 +1,185 @@
+package jp.panta.misskeyandroidclient.model.notes.poll
+
+import kotlinx.datetime.Clock
+import org.junit.Assert
+import org.junit.Test
+import kotlin.time.Duration.Companion.minutes
+
+class PollTest {
+
+
+    @Test
+    fun totalVoteCount() {
+        val poll = Poll(
+            choices = listOf(
+                Poll.Choice(
+                    index = 0,
+                    votes = 10,
+                    text = "aaa",
+                    isVoted = false
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 20,
+                    text = "aaa",
+                    isVoted = false
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 15,
+                    text = "aaa",
+                    isVoted = false
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 8,
+                    text = "aaa",
+                    isVoted = false
+                ),
+            ),
+            expiresAt = null,
+            multiple = false
+        )
+        Assert.assertEquals(53, poll.totalVoteCount)
+    }
+
+    @Test
+    fun canVoteMultiplePoll() {
+        val poll = Poll(
+            choices = listOf(
+                Poll.Choice(
+                    index = 0,
+                    votes = 10,
+                    text = "aaa",
+                    isVoted = true
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 20,
+                    text = "aaa",
+                    isVoted = true
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 15,
+                    text = "aaa",
+                    isVoted = false
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 8,
+                    text = "aaa",
+                    isVoted = false
+                ),
+            ),
+            expiresAt = null,
+            multiple = true
+        )
+        Assert.assertTrue(poll.canVote)
+    }
+
+    @Test
+    fun canVote() {
+        val poll = Poll(
+            choices = listOf(
+                Poll.Choice(
+                    index = 0,
+                    votes = 10,
+                    text = "aaa",
+                    isVoted = false
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 20,
+                    text = "aaa",
+                    isVoted = false
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 15,
+                    text = "aaa",
+                    isVoted = false
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 8,
+                    text = "aaa",
+                    isVoted = false
+                ),
+            ),
+            expiresAt = null,
+            multiple = false
+        )
+        Assert.assertTrue(poll.canVote)
+    }
+
+    @Test
+    fun canVoteWhenVoted() {
+        val poll = Poll(
+            choices = listOf(
+                Poll.Choice(
+                    index = 0,
+                    votes = 10,
+                    text = "aaa",
+                    isVoted = true
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 20,
+                    text = "aaa",
+                    isVoted = false
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 15,
+                    text = "aaa",
+                    isVoted = false
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 8,
+                    text = "aaa",
+                    isVoted = false
+                ),
+            ),
+            expiresAt = null,
+            multiple = false
+        )
+        Assert.assertFalse(poll.canVote)
+    }
+
+    @Test
+    fun canVoteExpired() {
+        val poll = Poll(
+            choices = listOf(
+                Poll.Choice(
+                    index = 0,
+                    votes = 10,
+                    text = "aaa",
+                    isVoted = true
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 20,
+                    text = "aaa",
+                    isVoted = true
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 15,
+                    text = "aaa",
+                    isVoted = true
+                ),
+                Poll.Choice(
+                    index = 0,
+                    votes = 8,
+                    text = "aaa",
+                    isVoted = true
+                ),
+            ),
+            expiresAt = Clock.System.now() + 10.minutes,
+            multiple = false
+        )
+        Assert.assertFalse(poll.canVote)
+    }
+}
