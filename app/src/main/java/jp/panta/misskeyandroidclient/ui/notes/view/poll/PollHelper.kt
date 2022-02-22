c01c920ee8c9c4b1b57193f594bba2797607222a
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-02-22 14:25:51 +0900

    [change] Helperに切り出しました

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/notes/view/poll/PollHelper.kt
@@ -0,0 +1,31 @@
+package jp.panta.misskeyandroidclient.ui.notes.view.poll
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import jp.panta.misskeyandroidclient.model.notes.Note
+import jp.panta.misskeyandroidclient.model.notes.poll.Poll
+import jp.panta.misskeyandroidclient.ui.notes.viewmodel.NotesViewModel
+
+object PollHelper {
+
+    @BindingAdapter("noteId", "poll", "notesViewModel")
+    @JvmStatic
+    fun RecyclerView.bindPollChoices(noteId: Note.Id?, poll: Poll?, notesViewModel: NotesViewModel?) {
+        if (noteId == null || poll == null || notesViewModel == null) {
+            this.visibility = View.GONE
+            return
+        } else {
+            this.visibility = View.VISIBLE
+        }
+
+        val adapter = PollListAdapter(noteId = noteId, poll, notesViewModel)
+        val layoutManager = this.layoutManager as? LinearLayoutManager
+            ?: LinearLayoutManager(this.context)
+
+        this.adapter = adapter
+        this.layoutManager = layoutManager
+        adapter.submitList(poll.choices)
+    }
+}
