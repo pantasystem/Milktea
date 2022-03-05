40ac11ce532332ea3e923f94a321591d4f32060a
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-03-06 04:21:54 +0900

    [change] 認証機構を修正

--- app/src/main/java/jp/panta/misskeyandroidclient/api/mastodon/MastodonAPI.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/api/mastodon/MastodonAPI.kt
@@ -1,9 +1,12 @@
 package jp.panta.misskeyandroidclient.api.mastodon
 
+import jp.panta.misskeyandroidclient.api.mastodon.apps.AccessToken
 import jp.panta.misskeyandroidclient.api.mastodon.apps.App
 import jp.panta.misskeyandroidclient.api.mastodon.apps.CreateApp
+import jp.panta.misskeyandroidclient.api.mastodon.apps.ObtainToken
 import jp.panta.misskeyandroidclient.api.mastodon.emojis.TootEmojiDTO
 import jp.panta.misskeyandroidclient.api.mastodon.instance.Instance
+import retrofit2.Response
 import retrofit2.http.Body
 
 import retrofit2.http.GET
@@ -15,9 +18,12 @@ interface MastodonAPI {
     suspend fun getInstance(): Instance
 
     @GET("api/v1/custom_emojis")
-    suspend fun getCustomEmojis(): List<TootEmojiDTO>
+    suspend fun getCustomEmojis(): Response<List<TootEmojiDTO>>
 
     @POST("api/v1/apps")
-    suspend fun createApp(@Body body: CreateApp): App
+    suspend fun createApp(@Body body: CreateApp): Response<App>
+
+    @POST("oauth/token")
+    suspend fun obtainToken(@Body body: ObtainToken): Response<AccessToken>
 
 }
--- app/src/main/java/jp/panta/misskeyandroidclient/api/mastodon/apps/App.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/api/mastodon/apps/App.kt
@@ -38,4 +38,40 @@ data class App(
         val encodedScope = URLEncoder.encode(scope, "utf-8")
         return "$baseURL/oauth/authorize?client_id=${encodedClientId}&redirect_uri=$encodedRedirectUri&response_type=$encodedResponseType&code=code&scope=$encodedScope"
     }
+
+    /**
+     * @param scope アプリ作成時に指定したscope
+     * @param code redirectUrl+codeで帰ってきたコード
+     */
+    fun createObtainToken(scope: String, code: String): ObtainToken {
+        return ObtainToken(
+            clientId = clientId,
+            clientSecret = clientSecret,
+            scope = scope,
+            redirectUri = redirectUri,
+            code = code
+        )
+    }
 }
+
+@Serializable
+data class ObtainToken(
+    val clientId: String,
+    val clientSecret: String,
+    val redirectUri: String,
+    val scope: String,
+    val code: String,
+    val grantType: String = "authorization_code",
+)
+
+@Serializable
+data class AccessToken(
+    @SerialName("access_token")
+    val accessToken: String,
+
+    @SerialName("token_type")
+    val tokenType: String,
+
+    val scope: String,
+    val createdAt: Long
+)
