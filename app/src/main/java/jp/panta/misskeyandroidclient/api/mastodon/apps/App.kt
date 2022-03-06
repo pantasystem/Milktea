2051c621f3204f5adf838a596ab10e800348fa6d
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-03-06 21:00:05 +0900

    [change] apiのDTOとModelで分けるようにしました

--- app/src/main/java/jp/panta/misskeyandroidclient/api/mastodon/apps/App.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/api/mastodon/apps/App.kt
@@ -1,4 +1,5 @@
 package jp.panta.misskeyandroidclient.api.mastodon.apps
+import jp.panta.misskeyandroidclient.model.auth.custom.AppType
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import java.net.URLEncoder
@@ -31,25 +32,15 @@ data class App(
     @SerialName("vapid_key")
     val vapidKey: String
 ) {
-    fun generateAuthUrl(baseURL: String, scope: String): String {
-        val encodedClientId = URLEncoder.encode(clientId, "utf-8")
-        val encodedRedirectUri = URLEncoder.encode(redirectUri, "utf-8")
-        val encodedResponseType = URLEncoder.encode("code", "utf-8")
-        val encodedScope = URLEncoder.encode(scope, "utf-8")
-        return "$baseURL/oauth/authorize?client_id=${encodedClientId}&redirect_uri=$encodedRedirectUri&response_type=$encodedResponseType&code=code&scope=$encodedScope"
-    }
-
-    /**
-     * @param scope アプリ作成時に指定したscope
-     * @param code redirectUrl+codeで帰ってきたコード
-     */
-    fun createObtainToken(scope: String, code: String): ObtainToken {
-        return ObtainToken(
-            clientId = clientId,
+    fun toModel(): AppType.Mastodon {
+        return AppType.Mastodon(
+            id = id,
+            name = name,
+            website = website,
             clientSecret = clientSecret,
-            scope = scope,
+            clientId = clientId,
             redirectUri = redirectUri,
-            code = code
+            vapidKey = vapidKey
         )
     }
 }
