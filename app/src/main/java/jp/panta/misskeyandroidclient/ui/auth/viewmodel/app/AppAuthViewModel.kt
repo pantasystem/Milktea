8f9aae178612a55f8639b72d1d92e016d5d163bb
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-03-06 21:11:29 +0900

    [chnage] HiltでViewModelをDIするようにしました。

--- app/src/main/java/jp/panta/misskeyandroidclient/ui/auth/viewmodel/app/AppAuthViewModel.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/auth/viewmodel/app/AppAuthViewModel.kt
@@ -2,10 +2,12 @@ package jp.panta.misskeyandroidclient.ui.auth.viewmodel.app
 
 import android.util.Log
 import androidx.lifecycle.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jp.panta.misskeyandroidclient.api.mastodon.MastodonAPIProvider
+import jp.panta.misskeyandroidclient.api.mastodon.instance.Instance
 import jp.panta.misskeyandroidclient.api.misskey.MisskeyAPIServiceBuilder
 import jp.panta.misskeyandroidclient.api.misskey.app.CreateApp
 import jp.panta.misskeyandroidclient.api.misskey.throwIfHasError
-import jp.panta.misskeyandroidclient.model.api.Version
 import jp.panta.misskeyandroidclient.api.misskey.auth.AppSecret
 import jp.panta.misskeyandroidclient.api.misskey.auth.Session
 import jp.panta.misskeyandroidclient.model.auth.Authorization
@@ -15,14 +17,13 @@ import jp.panta.misskeyandroidclient.util.State
 import jp.panta.misskeyandroidclient.util.StateContent
 import jp.panta.misskeyandroidclient.viewmodel.MiCore
 import jp.panta.misskeyandroidclient.ui.auth.viewmodel.Permissions
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
-import java.lang.IllegalStateException
+import java.lang.IllegalArgumentException
 import java.util.*
 import java.util.regex.Pattern
-
+import javax.inject.Inject
+import jp.panta.misskeyandroidclient.api.mastodon.apps.CreateApp as CreateTootApp
 
 sealed interface AuthErrors {
     val throwable: Throwable
@@ -35,21 +36,24 @@ sealed interface AuthErrors {
     ) : AuthErrors
 }
 
+sealed interface InstanceType {
+    data class Mastodon(val instance: Instance) : InstanceType
+    data class Misskey(val instance: Meta) : InstanceType
+}
+
+
 @ExperimentalCoroutinesApi
 @Suppress("UNCHECKED_CAST")
-class AppAuthViewModel(
+@HiltViewModel
+class AppAuthViewModel @Inject constructor(
     private val customAuthStore: CustomAuthStore,
+    private val mastodonAPIProvider: MastodonAPIProvider,
     miCore: MiCore
 ) : ViewModel(){
     companion object{
         const val CALL_BACK_URL = "misskey://app_auth_callback"
     }
 
-    class Factory(private val customAuthStore: CustomAuthStore, private val miCore: MiCore) : ViewModelProvider.Factory{
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return AppAuthViewModel(customAuthStore, miCore) as T
-        }
-    }
 
     private val urlPattern = Pattern.compile("""(https?)(://)([-_.!~*'()\[\]a-zA-Z0-9;/?:@&=+${'$'},%#]+)""")
 
@@ -89,7 +93,7 @@ class AppAuthViewModel(
     val appName = MutableLiveData<String>()
 
 
-    val app = MutableLiveData<App?>()
+    val app = MutableLiveData<AppType?>()
     val waiting4UserAuthorization = MutableLiveData<Authorization.Waiting4UserAuthorization?>()
 
 
@@ -110,14 +114,35 @@ class AppAuthViewModel(
         }
     }
 
-    private fun getMeta(instanceDomain: String): Flow<State<Meta>> {
-        return flow<State<Meta>> {
+    private fun getMeta(instanceDomain: String): Flow<State<InstanceType>> {
+        return flow<State<InstanceType>> {
             val url = toEnableUrl(instanceDomain)
             emit(State.Fixed(StateContent.NotExist()))
             if(urlPattern.matcher(url).find()){
                 emit(State.Loading(StateContent.NotExist()))
                 runCatching {
-                    metaStore.fetch(url)
+                    coroutineScope {
+                        val misskey = async {
+                            runCatching {
+                                metaStore.fetch(url)
+                            }.getOrNull()
+                        }.await()
+                        val mastodon = async {
+                            runCatching {
+                                mastodonAPIProvider.get(url)
+                                    .getInstance()
+                            }.getOrNull()
+
+                        }.await()
+                        if (misskey != null) {
+                            return@coroutineScope InstanceType.Misskey(misskey)
+                        }
+                        if (mastodon != null) {
+                            return@coroutineScope InstanceType.Mastodon(mastodon)
+                        }
+                        throw IllegalArgumentException()
+                    }
+
                 }.onFailure {
                     emit(State.Error(StateContent.NotExist(), it))
                 }.onSuccess {
@@ -141,24 +166,41 @@ class AppAuthViewModel(
         viewModelScope.launch(Dispatchers.IO){
             _generatingTokenState.value = State.Loading(generatingTokenState.value.content)
             runCatching {
-                val app = createApp(instanceBase, meta.getVersion(), appName)
+                val app = createApp(instanceBase, meta, appName)
                 this@AppAuthViewModel.app.postValue(app)
-                val secret = app.secret
-                val authApi = MisskeyAPIServiceBuilder.buildAuthAPI(instanceBase)
-                val session = authApi.generateSession(AppSecret(secret!!)).body()
-                    ?: throw IllegalStateException("セッションの作成に失敗しました。")
-                customAuthStore.setCustomAuthBridge(
-                    app.createAuth(instanceBase, session)
-                )
-                Authorization.Waiting4UserAuthorization(
-                    instanceBase,
-                    appSecret = app.secret,
-                    session = session,
-                    viaName = app.name
-                )
+                when(app) {
+                    is AppType.Mastodon -> {
+                        val authState = app.createAuth(instanceBase, "read write")
+                        customAuthStore.setCustomAuthBridge(authState)
+                        Authorization.Waiting4UserAuthorization.Mastodon(
+                            instanceBase,
+                            client = app,
+                            scope = "read write"
+                        )
+                    }
+                    is AppType.Misskey -> {
+                        val secret = app.secret
+                        val authApi = MisskeyAPIServiceBuilder.buildAuthAPI(instanceBase)
+                        val session = authApi.generateSession(AppSecret(secret!!)).body()
+                            ?: throw IllegalStateException("セッションの作成に失敗しました。")
+                        customAuthStore.setCustomAuthBridge(
+                            app.createAuth(instanceBase, session)
+                        )
+                        Authorization.Waiting4UserAuthorization.Misskey(
+                            instanceBase,
+                            appSecret = app.secret!!,
+                            session = session,
+                            viaName = app.name
+                        )
+                    }
+                }
+
+
             }.onSuccess { w4a ->
-                this@AppAuthViewModel.waiting4UserAuthorization.postValue(w4a)
-                _generatingTokenState.value = State.Fixed(StateContent.Exist(w4a.session))
+                if (w4a is Authorization.Waiting4UserAuthorization.Misskey) {
+                    this@AppAuthViewModel.waiting4UserAuthorization.postValue(w4a)
+                    _generatingTokenState.value = State.Fixed(StateContent.Exist(w4a.session))
+                }
 
             }.onFailure {
                 Log.e("AppAuthViewModel", "認証開始処理失敗", it)
@@ -171,18 +213,35 @@ class AppAuthViewModel(
     }
 
 
-    private suspend fun createApp(url: String, version: Version, appName: String): App {
-        val misskeyAPI = misskeyAPIProvider.get(url, version)
-        return misskeyAPI.createApp(
-            CreateApp(
-                null,
-                appName,
-                "misskey android application",
-                CALL_BACK_URL,
-                permission = Permissions.getPermission(version)
-            )
-        ).throwIfHasError().body()
-            ?: throw IllegalStateException("Appの作成に失敗しました。")
+    private suspend fun createApp(url: String, instanceType: InstanceType, appName: String): AppType {
+        when (instanceType) {
+            is InstanceType.Mastodon -> {
+                val app = mastodonAPIProvider.get(url)
+                    .createApp(CreateTootApp(
+                        clientName = appName,
+                        redirectUris = CALL_BACK_URL,
+                        scopes = "read write"
+                    )).throwIfHasError().body()
+                    ?: throw IllegalStateException("Appの作成に失敗しました。")
+                return AppType.fromDTO(app)
+            }
+            is InstanceType.Misskey -> {
+                val version = instanceType.instance.getVersion()
+                val misskeyAPI = misskeyAPIProvider.get(url, version)
+                val app = misskeyAPI.createApp(
+                    CreateApp(
+                        null,
+                        appName,
+                        "misskey android application",
+                        CALL_BACK_URL,
+                        permission = Permissions.getPermission(version)
+                    )
+                ).throwIfHasError().body()
+                    ?: throw IllegalStateException("Appの作成に失敗しました。")
+                return AppType.fromDTO(app)
+            }
+        }
+
     }
 
     private fun toEnableUrl(base: String) : String{
