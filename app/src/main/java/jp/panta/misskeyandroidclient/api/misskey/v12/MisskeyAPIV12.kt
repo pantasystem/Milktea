ba24594759f3d653f91440386de1847cd04bfe45
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-03-20 15:58:06 +0900

    [change] PageableにChannelを追加できるようにしました。

--- app/src/main/java/jp/panta/misskeyandroidclient/api/misskey/v12/MisskeyAPIV12.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/api/misskey/v12/MisskeyAPIV12.kt
@@ -9,6 +9,8 @@ import jp.panta.misskeyandroidclient.api.misskey.v11.MisskeyAPIV11Diff
 import jp.panta.misskeyandroidclient.api.misskey.v12.antenna.AntennaDTO
 import jp.panta.misskeyandroidclient.api.misskey.v12.antenna.AntennaQuery
 import jp.panta.misskeyandroidclient.api.misskey.v12.antenna.AntennaToAdd
+import jp.panta.misskeyandroidclient.api.misskey.v12.channel.*
+import jp.panta.misskeyandroidclient.model.I
 import retrofit2.Response
 
 open class MisskeyAPIV12(misskey: MisskeyAPI, private val misskeyAPIV12Diff: MisskeyAPIV12Diff, misskeyAPIV11Diff: MisskeyAPIV11Diff) : MisskeyAPIV11(misskey, misskeyAPIV11Diff),
@@ -32,4 +34,19 @@ open class MisskeyAPIV12(misskey: MisskeyAPI, private val misskeyAPIV12Diff: Mis
 
     override suspend fun searchByUserNameAndHost(requestUser: RequestUser) = misskeyAPIV12Diff.searchByUserNameAndHost(requestUser)
 
+    override suspend fun createChannel(dto: CreateChannelDTO): Response<ChannelDTO> = misskeyAPIV12Diff.createChannel(dto)
+    override suspend fun featuredChannels(i: I): Response<List<ChannelDTO>> = misskeyAPIV12Diff.featuredChannels(i)
+
+    override suspend fun followChannel(dto: FollowChannelDTO): Response<Unit> = misskeyAPIV12Diff.followChannel(dto)
+
+    override suspend fun unFollowChannel(dto: UnFollowChannelDTO): Response<Unit> = misskeyAPIV12Diff.unFollowChannel(dto)
+
+    override suspend fun ownedChannels(dto: FindPageable): Response<List<ChannelDTO>> = misskeyAPIV12Diff.ownedChannels(dto)
+    override suspend fun showChannel(dto: ShowChannelDTO): Response<ChannelDTO> = misskeyAPIV12Diff.showChannel(dto)
+
+    override suspend fun updateChannel(dto: UpdateChannelDTO): Response<ChannelDTO> = misskeyAPIV12Diff.updateChannel(dto)
+
+    override suspend fun channelTimeline(dto: NoteRequest): Response<List<NoteDTO>?> = misskeyAPIV12Diff.channelTimeline(dto)
+
+    override suspend fun followedChannels(dto: FindPageable): Response<List<ChannelDTO>> = misskeyAPIV12Diff.followedChannels(dto)
 }
