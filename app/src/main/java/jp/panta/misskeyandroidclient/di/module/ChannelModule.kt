f6b592dc5a2bf66ca12be9f7508dc9424197b03f
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-03-21 15:46:18 +0900

    [change] Moduleに追加

--- app/src/main/java/jp/panta/misskeyandroidclient/di/module/ChannelModule.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/di/module/ChannelModule.kt
@@ -4,8 +4,15 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import jp.panta.misskeyandroidclient.api.misskey.MisskeyAPIProvider
+import jp.panta.misskeyandroidclient.model.Encryption
+import jp.panta.misskeyandroidclient.model.account.AccountRepository
+import jp.panta.misskeyandroidclient.model.channel.ChannelAPIAdapter
+import jp.panta.misskeyandroidclient.model.channel.ChannelRepository
 import jp.panta.misskeyandroidclient.model.channel.ChannelStateModel
 import jp.panta.misskeyandroidclient.model.channel.ChannelStateModelOnMemory
+import jp.panta.misskeyandroidclient.model.channel.impl.ChannelAPIAdapterWebImpl
+import jp.panta.misskeyandroidclient.model.channel.impl.ChannelRepositoryImpl
 import javax.inject.Singleton
 
 @Module
@@ -16,5 +23,29 @@ object ChannelModule {
     @Singleton
     fun provideChannelStateModel(): ChannelStateModel {
         return ChannelStateModelOnMemory()
+    }
+
+    @Provides
+    @Singleton
+    fun provideChannelAPIAdapter(
+        accountRepository: AccountRepository,
+        encryption: Encryption,
+        misskeyAPIProvider: MisskeyAPIProvider
+    ): ChannelAPIAdapter {
+        return ChannelAPIAdapterWebImpl(accountRepository, misskeyAPIProvider, encryption)
+    }
+
+    @Provides
+    @Singleton
+    fun provideChannelRepository(
+        channelStateModel: ChannelStateModel,
+        accountRepository: AccountRepository,
+        channelAPIAdapter: ChannelAPIAdapter,
+        ): ChannelRepository {
+        return ChannelRepositoryImpl(
+            channelStateModel = channelStateModel,
+            accountRepository = accountRepository,
+            channelAPIAdapter = channelAPIAdapter
+        )
     }
 }
