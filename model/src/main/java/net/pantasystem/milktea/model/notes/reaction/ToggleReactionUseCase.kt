7be46cc1fca1be7536fbcc015e77001421c493e8
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-04-25 18:18:51 +0900

    [change] リアクション追加用のUseCaseを追加しました。

---
+++ model/src/main/java/net/pantasystem/milktea/model/notes/reaction/ToggleReactionUseCase.kt
@@ -0,0 +1,47 @@
+package net.pantasystem.milktea.model.notes.reaction
+
+import com.vdurmont.emoji.EmojiManager
+import net.pantasystem.milktea.model.UseCase
+import net.pantasystem.milktea.model.account.AccountRepository
+import net.pantasystem.milktea.model.instance.FetchMeta
+import net.pantasystem.milktea.model.notes.Note
+import net.pantasystem.milktea.model.notes.NoteRepository
+import net.pantasystem.milktea.model.notes.reaction.history.ReactionHistory
+import net.pantasystem.milktea.model.notes.reaction.history.ReactionHistoryDao
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ToggleReactionUseCase @Inject constructor(
+    private val noteRepository: NoteRepository,
+    private val reactionHistoryDao: ReactionHistoryDao,
+    private val accountRepository: AccountRepository,
+    private val fetchMeta: FetchMeta,
+) : UseCase {
+
+    suspend operator fun invoke(noteId: Note.Id, reaction: String): Result<Unit> {
+        return runCatching {
+            val account = accountRepository.get(noteId.accountId)
+            val reactionObj = Reaction(reaction)
+            val sendReaction =
+                if (EmojiManager.isEmoji(reaction) || fetchMeta.fetch(account.instanceDomain)
+                        .isOwnEmojiBy(reactionObj)
+                ) {
+                    reaction
+                } else {
+                    "👍"
+                }
+            val note = noteRepository.find(noteId)
+            if (note.myReaction.isNullOrBlank()) {
+                if (noteRepository.reaction(CreateReaction(noteId, sendReaction))) {
+                    reactionHistoryDao.insert(ReactionHistory(sendReaction, account.instanceDomain))
+                }
+            } else if (note.myReaction != sendReaction) {
+                noteRepository.unreaction(noteId)
+                noteRepository.reaction(CreateReaction(noteId, sendReaction))
+            } else {
+                noteRepository.unreaction(noteId)
+            }
+        }
+    }
+}
