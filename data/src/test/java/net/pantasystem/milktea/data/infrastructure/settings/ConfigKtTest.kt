f648cd6269ca264ffb2ae25bd30226ed8776a528
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-05-16 22:15:29 +0900

    [change] テストを追加しました。

---
+++ data/src/test/java/net/pantasystem/milktea/data/infrastructure/settings/ConfigKtTest.kt
@@ -0,0 +1,55 @@
+package net.pantasystem.milktea.data.infrastructure.settings
+
+import net.pantasystem.milktea.model.setting.DefaultConfig
+import net.pantasystem.milktea.model.setting.UrlPreviewConfig
+import org.junit.Assert
+import org.junit.Test
+
+class ConfigKtTest {
+
+
+    @Test
+    fun prefs() {
+        val config = DefaultConfig.config
+        config.prefs().forEach { (k, u) ->
+            when (k) {
+                Keys.BackgroundImage -> Assert.assertEquals(
+                    config.backgroundImagePath,
+                    (u as PrefType.StrPref).value
+                )
+                Keys.ClassicUI -> Assert.assertEquals(
+                    config.isClassicUI,
+                    (u as PrefType.BoolPref).value
+                )
+                Keys.IsPostButtonToBottom -> Assert.assertEquals(
+                    config.isPostButtonAtTheBottom,
+                    (u as PrefType.BoolPref).value
+                )
+                Keys.IsSimpleEditorEnabled -> Assert.assertEquals(
+                    config.isSimpleEditorEnabled,
+                    (u as PrefType.BoolPref).value
+                )
+                Keys.IsUserNameDefault -> Assert.assertEquals(
+                    config.isUserNameDefault,
+                    (u as PrefType.BoolPref).value
+                )
+                Keys.NoteLimitHeight -> Assert.assertEquals(
+                    config.noteExpandedHeightSize,
+                    (u as PrefType.IntPref).value
+                )
+                Keys.ReactionPickerType -> Assert.assertEquals(
+                    config.reactionPickerType.ordinal,
+                    (u as PrefType.IntPref).value
+                )
+                Keys.SummalyServerUrl -> Assert.assertEquals(
+                    (config.urlPreviewConfig.type as UrlPreviewConfig.Type.SummalyServer?)?.url,
+                    (u as PrefType.StrPref).value
+                )
+                Keys.ThemeType -> Assert.assertEquals(
+                    (config.theme.toInt()), (u as PrefType.IntPref).value
+                )
+                Keys.UrlPreviewSourceType -> Assert.assertEquals(config.urlPreviewConfig.type.toInt(), (u as PrefType.IntPref).value)
+            }
+        }
+    }
+}
