4c4e92f6614c6857f24f86be1d694d65cec48199
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-05-18 16:16:12 +0900

    [change] 設定用のForm部品を作成しました。

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/settings/compose/RadioTile.kt
@@ -0,0 +1,48 @@
+package jp.panta.misskeyandroidclient.ui.settings.compose
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.RadioButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RadioTile(selected: Boolean, onClick: () -> Unit, title: @Composable RowScope.() -> Unit) {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = selected, onClick = onClick)
+
+        Box(modifier = Modifier.weight(1f)) {
+            title.invoke(this@Row)
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun PreviewRadioTile() {
+    Column {
+        RadioTile(selected = true, onClick = { /*TODO*/ }) {
+            Text("はるのん")
+        }
+        RadioTile(selected = false, onClick = { /*TODO*/ }) {
+            Text("ぬるきゃ")
+        }
+        RadioTile(selected = false, onClick = { /*TODO*/ }) {
+            Text("あずきゃ")
+        }
+        RadioTile(selected = false, onClick = { /*TODO*/ }) {
+            Text("村上さん")
+        }
+    }
+
+}
