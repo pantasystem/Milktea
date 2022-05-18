fceff439a55490f514a2c2d66126243c9c3d3f47
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-05-19 00:38:38 +0900

    [change] 直接SharedPreferenceに依存しないようにしました。

--- app/src/main/java/jp/panta/misskeyandroidclient/ThemeUtil.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/ThemeUtil.kt
@@ -5,18 +5,13 @@ import android.util.TypedValue
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import net.pantasystem.milktea.common.getPreferenceName
-import net.pantasystem.milktea.data.infrastructure.settings.Keys
-import net.pantasystem.milktea.data.infrastructure.settings.from
-import net.pantasystem.milktea.data.infrastructure.settings.str
-import net.pantasystem.milktea.data.infrastructure.settings.toInt
-import net.pantasystem.milktea.model.setting.DefaultConfig
+import jp.panta.misskeyandroidclient.viewmodel.MiCore
 import net.pantasystem.milktea.model.setting.Theme
 import net.pantasystem.milktea.model.setting.isNightTheme
 
 fun AppCompatActivity.setTheme(){
-    val preference = this.getSharedPreferences(getPreferenceName(), Context.MODE_PRIVATE)
-    val theme = Theme.from(preference.getInt(Keys.ThemeType.str(), DefaultConfig.config.theme.toInt()))
+    val miCore = this.applicationContext as MiCore
+    val theme = miCore.getSettingStore().configState.value.theme
     if(theme.isNightTheme()){
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }else{
