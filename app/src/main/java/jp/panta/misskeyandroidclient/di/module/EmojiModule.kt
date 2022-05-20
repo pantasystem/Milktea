734923803e8cb5fbbd0b19d635eef42c69621637
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-05-21 01:10:48 +0900

    [change] 絵文字をロードしてくるRepositoryを作成しました

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/di/module/EmojiModule.kt
@@ -0,0 +1,38 @@
+package jp.panta.misskeyandroidclient.di.module
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jp.panta.misskeyandroidclient.impl.CheckEmojiAndroidImpl
+import kotlinx.coroutines.CoroutineScope
+import net.pantasystem.milktea.common.Logger
+import net.pantasystem.milktea.data.infrastructure.emoji.Utf8EmojiRepositoryImpl
+import net.pantasystem.milktea.model.emoji.UtfEmojiRepository
+import net.pantasystem.milktea.model.notes.reaction.CheckEmoji
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object EmojiModule {
+
+    @Singleton
+    @Provides
+    fun provideCheckEmoji(
+        utfEmojiRepository: UtfEmojiRepository
+    ): CheckEmoji {
+        return CheckEmojiAndroidImpl(utfEmojiRepository)
+    }
+
+    @Singleton
+    @Provides
+    fun provideUtf8EmojiRepository(
+        coroutineScope: CoroutineScope,
+        loggerFactory: Logger.Factory
+    ): UtfEmojiRepository {
+        return Utf8EmojiRepositoryImpl(
+            coroutineScope = coroutineScope,
+            loggerFactory = loggerFactory
+        )
+    }
+}
---
+++ model/src/main/java/net/pantasystem/milktea/model/emoji/UtfEmojiRepository.kt
@@ -0,0 +1,6 @@
+package net.pantasystem.milktea.model.emoji
+
+interface UtfEmojiRepository {
+    suspend fun findAll(): List<Utf8Emoji>
+    suspend fun exists(emoji: CharSequence): Boolean
+}
