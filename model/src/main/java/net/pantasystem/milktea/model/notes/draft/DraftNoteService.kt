32d9159e03964ea2bfbefadfc3809b8505270130
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-06-17 05:13:47 +0900

    [change] 下書きノートのファイルを保存できるようにしました。

--- model/src/main/java/net/pantasystem/milktea/model/notes/draft/DraftNoteService.kt
+++ model/src/main/java/net/pantasystem/milktea/model/notes/draft/DraftNoteService.kt
@@ -6,4 +6,5 @@ import net.pantasystem.milktea.model.notes.CreateNote
 interface DraftNoteService {
 
     suspend fun save(createNote: CreateNote): Result<DraftNote>
+    suspend fun save(draftNoteFile: DraftNoteFile): Result<DraftNoteFile>
 }
