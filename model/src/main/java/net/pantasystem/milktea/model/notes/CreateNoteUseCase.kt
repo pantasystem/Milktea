c293d6905158132cfb44581659bb77e6ec69ec2f
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-06-16 18:02:40 +0900

    [feat] ノートを作成するユースケースを作成しました。

---
+++ model/src/main/java/net/pantasystem/milktea/model/notes/CreateNoteUseCase.kt
@@ -0,0 +1,56 @@
+package net.pantasystem.milktea.model.notes
+
+import net.pantasystem.milktea.model.UseCase
+import net.pantasystem.milktea.model.drive.DriveFileRepository
+import net.pantasystem.milktea.model.notes.draft.DraftNoteRepository
+import net.pantasystem.milktea.model.notes.draft.DraftNoteService
+import net.pantasystem.milktea.model.setting.LocalConfigRepository
+import net.pantasystem.milktea.model.setting.RememberVisibility
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CreateNoteUseCase @Inject constructor(
+    private val noteRepository: NoteRepository,
+    private val draftNoteRepository: DraftNoteRepository,
+    private val driveFileRepository: DriveFileRepository,
+    private val settingRepository: LocalConfigRepository,
+    private val draftNoteService: DraftNoteService,
+) : UseCase {
+
+    suspend operator fun invoke(createNote: CreateNote): Result<Note> {
+        return runCatching {
+            val createNoteResult = runCatching {
+                noteRepository.create(createNote)
+            }
+            if (createNoteResult.isFailure) {
+                draftNoteService.save(createNote)
+            }
+            val result = createNoteResult.getOrThrow()
+            if (createNoteResult.isSuccess && createNote.draftNoteId != null) {
+                draftNoteRepository.delete(createNote.draftNoteId)
+            }
+            setNoteVisibility(createNote)
+
+            return@runCatching result
+        }
+    }
+
+    suspend internal fun setNoteVisibility(createNote: CreateNote) {
+        if (!(createNote.channelId == null && createNote.renoteId == null && createNote.replyId == null)) {
+            return
+        }
+        val nowConfig =
+            (settingRepository.getRememberVisibility(createNote.author.accountId).getOrThrow())
+
+        when (nowConfig) {
+            is RememberVisibility.None -> return
+            is RememberVisibility.Remember -> settingRepository.save(
+                nowConfig.copy(visibility = createNote.visibility)
+            )
+        }
+
+    }
+
+
+}
