e390b2445c3c381200442055f27c36a7a596fd1b
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-06-16 18:50:15 +0900

    [change] 取得用のメソッドを実装しました。

--- model/src/main/java/net/pantasystem/milktea/model/notes/draft/DraftNoteRepository.kt
+++ model/src/main/java/net/pantasystem/milktea/model/notes/draft/DraftNoteRepository.kt
@@ -4,4 +4,5 @@ interface DraftNoteRepository {
 
     suspend fun save(draftNote: DraftNote): Result<DraftNote>
     suspend fun delete(draftNoteId: Long): Result<Unit>
+    suspend fun findOne(draftNoteId: Long): Result<DraftNote>
 }
