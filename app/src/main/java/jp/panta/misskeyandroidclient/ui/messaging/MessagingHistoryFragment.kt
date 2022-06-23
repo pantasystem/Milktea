bcc533ce904cdb19c23dca44516ea6fbe5c2de91
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-06-23 11:48:34 +0900

    [change] Composeで描画するようにしました。

--- app/src/main/java/jp/panta/misskeyandroidclient/ui/messaging/MessagingHistoryFragment.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/messaging/MessagingHistoryFragment.kt
@@ -2,92 +2,63 @@ package jp.panta.misskeyandroidclient.ui.messaging
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.wada811.databinding.dataBinding
+import com.google.android.material.composethemeadapter.MdcTheme
 import dagger.hilt.android.AndroidEntryPoint
 import jp.panta.misskeyandroidclient.MessageActivity
-import jp.panta.misskeyandroidclient.R
-import jp.panta.misskeyandroidclient.databinding.FragmentMessagingHistoryBinding
-import jp.panta.misskeyandroidclient.ui.messaging.viewmodel.HistoryViewData
+import jp.panta.misskeyandroidclient.UserDetailActivity
 import jp.panta.misskeyandroidclient.ui.messaging.viewmodel.MessageHistoryViewModel
-import net.pantasystem.milktea.common.StateContent
+import net.pantasystem.milktea.model.messaging.messagingId
 
 
 @AndroidEntryPoint
-class MessagingHistoryFragment : Fragment(R.layout.fragment_messaging_history) {
-
-    private var mLinearLayoutManager: LinearLayoutManager? = null
-
-    private val binding: FragmentMessagingHistoryBinding by dataBinding()
+class MessagingHistoryFragment : Fragment() {
 
     private val historyViewModel: MessageHistoryViewModel by viewModels()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val layoutManager = LinearLayoutManager(context)
-        binding.historyListView.layoutManager = layoutManager
-        mLinearLayoutManager = layoutManager
-
-        val adapter =
-            HistoryListAdapter(diffUtilItemCallback, historyViewModel, viewLifecycleOwner)
-        binding.historyListView.adapter = adapter
-        lifecycleScope.launchWhenResumed {
-            historyViewModel.loadGroupAndUser()
-            historyViewModel.histories.collect { resultState ->
-                Log.d("MsgHistoryFragment", "msg:$resultState")
-                when(val content = resultState.content) {
-                    is StateContent.Exist -> {
-                        adapter.submitList(content.rawContent)
-                    }
-                    is StateContent.NotExist -> {
-                        adapter.submitList(emptyList())
-                    }
-                }
-
-            }
-        }
-
-        historyViewModel.isRefreshing.observe(viewLifecycleOwner) {
-            binding.refresh.isRefreshing = it
-        }
-
-
-        binding.refresh.setOnRefreshListener {
-            historyViewModel.loadGroupAndUser()
-        }
-
-        historyViewModel.messageHistorySelected.observe(viewLifecycleOwner) { hvd ->
-            Handler(Looper.getMainLooper()).post {
-                val intent = Intent(activity, MessageActivity::class.java)
-                intent.putExtra(MessageActivity.EXTRA_MESSAGING_ID, hvd.messagingId)
-                startActivity(intent)
-            }
-        }
-
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        historyViewModel.loadGroupAndUser()
     }
 
-
-    private val diffUtilItemCallback = object : DiffUtil.ItemCallback<HistoryViewData>() {
-        override fun areContentsTheSame(
-            oldItem: HistoryViewData,
-            newItem: HistoryViewData
-        ): Boolean {
-            return oldItem == newItem
-        }
-
-        override fun areItemsTheSame(oldItem: HistoryViewData, newItem: HistoryViewData): Boolean {
-            return oldItem.messagingId == newItem.messagingId
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MdcTheme {
+                    MessageHistoryScreen(
+                        historyViewModel = historyViewModel,
+                        onAction = { action ->
+                            when (action) {
+                                is Action.OnAvatarIconClick -> {
+                                    val intent = UserDetailActivity.newInstance(
+                                        requireActivity(),
+                                        action.user.id
+                                    )
+                                    startActivity(intent)
+                                }
+                                is Action.OnClick -> {
+                                    val intent = Intent(activity, MessageActivity::class.java)
+                                    intent.putExtra(
+                                        MessageActivity.EXTRA_MESSAGING_ID,
+                                        action.history.messagingId
+                                    )
+                                    startActivity(intent)
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        }.rootView
     }
 
 
