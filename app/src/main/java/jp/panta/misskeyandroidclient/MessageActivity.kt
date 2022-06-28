cf2071ee201704b1ac994b8da3dd52f9b0bc17c2
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2022-06-28 14:59:32 +0900

    feat: activityにあった実装をFragmentに移しました。

--- app/src/main/java/jp/panta/misskeyandroidclient/MessageActivity.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/MessageActivity.kt
@@ -1,28 +1,14 @@
 package jp.panta.misskeyandroidclient
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import jp.panta.misskeyandroidclient.databinding.ActivityMessageBinding
 import jp.panta.misskeyandroidclient.ui.TitleSettable
 import jp.panta.misskeyandroidclient.ui.messaging.MessageFragment
-import jp.panta.misskeyandroidclient.ui.messaging.viewmodel.MessageActionViewModel
-import jp.panta.misskeyandroidclient.ui.text.CustomEmojiCompleteAdapter
-import jp.panta.misskeyandroidclient.ui.text.CustomEmojiTokenizer
-import jp.panta.misskeyandroidclient.viewmodel.MiCore
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.mapNotNull
-import kotlinx.coroutines.flow.onEach
-import net.pantasystem.milktea.drive.DriveActivity
-import net.pantasystem.milktea.model.drive.FileProperty
 import net.pantasystem.milktea.model.messaging.MessagingId
 
 @AndroidEntryPoint
@@ -32,7 +18,6 @@ class MessageActivity : AppCompatActivity(), TitleSettable {
         const val EXTRA_MESSAGING_ID = "jp.panta.misskeyandroidclient.MessageActivity.EXTRA_MESSAGING_ID"
     }
 
-    private lateinit var mViewModel: MessageActionViewModel
     private lateinit var mBinding: ActivityMessageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,17 +30,10 @@ class MessageActivity : AppCompatActivity(), TitleSettable {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val messagingId = intent?.getSerializableExtra(EXTRA_MESSAGING_ID) as MessagingId?
-        val account = (applicationContext as MiApplication).getAccountStore().currentAccount
 
 
         if(messagingId == null){
             Log.e("MessageActivity", "EXTRA_MESSAGE_HISTORY must not null")
-            finish()
-            return
-        }
-
-        if(account == null){
-            Log.d("MessageActivity", "ac not found")
             finish()
             return
         }
@@ -67,29 +45,10 @@ class MessageActivity : AppCompatActivity(), TitleSettable {
             ft.commit()
         }
 
-        val factory = MessageActionViewModel.Factory(messagingId, application as MiApplication)
-        val messageActionViewModel = ViewModelProvider(this, factory)[MessageActionViewModel::class.java]
-        mViewModel = messageActionViewModel
-        mBinding.actionViewModel = messageActionViewModel
 
-        mBinding.openDrive.setOnClickListener {
-            openDriveActivity()
-        }
 
-        val miCore = application as MiCore
 
-        miCore.getMetaRepository().observe(account.instanceDomain)
-            .mapNotNull { it?.emojis }
-            .distinctUntilChanged()
-            .onEach { emojis ->
-                mBinding.inputMessage.setTokenizer(CustomEmojiTokenizer())
-                mBinding.inputMessage.setAdapter(
-                    CustomEmojiCompleteAdapter(
-                        emojis,
-                        this
-                    )
-                )
-            }.launchIn(lifecycleScope)
+
     }
 
     override fun setTitle(text: String) {
@@ -103,20 +62,6 @@ class MessageActivity : AppCompatActivity(), TitleSettable {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun openDriveActivity(){
-        val intent = Intent(this, DriveActivity::class.java)
-        intent.putExtra(DriveActivity.EXTRA_INT_SELECTABLE_FILE_MAX_SIZE, 1)
-        intent.action = Intent.ACTION_OPEN_DOCUMENT
-        intent.action = Intent.ACTION_OPEN_DOCUMENT
-        openDriveActivityForPickFileResult.launch(intent)
-    }
 
-    private val openDriveActivityForPickFileResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        val ids = (result.data?.getSerializableExtra(DriveActivity.EXTRA_SELECTED_FILE_PROPERTY_IDS) as? List<*>)?.map {
-            it as FileProperty.Id
-        }
-        ids?.firstOrNull()?.let {
-            mViewModel.setFilePropertyFromId(it)
-        }
-    }
+
 }
--- app/src/main/java/jp/panta/misskeyandroidclient/ui/messaging/MessageFragment.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/messaging/MessageFragment.kt
@@ -1,28 +1,39 @@
 package jp.panta.misskeyandroidclient.ui.messaging
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.ui.platform.ComposeView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.composethemeadapter.MdcTheme
 import com.wada811.databinding.dataBinding
 import dagger.hilt.android.AndroidEntryPoint
-import jp.panta.misskeyandroidclient.BuildConfig
+import jp.panta.misskeyandroidclient.MiApplication
 import jp.panta.misskeyandroidclient.R
 import jp.panta.misskeyandroidclient.databinding.FragmentMessageBinding
 import jp.panta.misskeyandroidclient.ui.TitleSettable
+import jp.panta.misskeyandroidclient.ui.messaging.viewmodel.MessageActionViewModel
 import jp.panta.misskeyandroidclient.ui.messaging.viewmodel.MessageViewModel
+import jp.panta.misskeyandroidclient.ui.text.CustomEmojiCompleteAdapter
+import jp.panta.misskeyandroidclient.ui.text.CustomEmojiTokenizer
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import net.pantasystem.milktea.common.StateContent
+import net.pantasystem.milktea.drive.DriveActivity
+import net.pantasystem.milktea.model.account.AccountStore
+import net.pantasystem.milktea.model.drive.FileProperty
+import net.pantasystem.milktea.model.instance.MetaRepository
 import net.pantasystem.milktea.model.messaging.MessageRelation
 import net.pantasystem.milktea.model.messaging.MessagingId
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MessageFragment : Fragment(R.layout.fragment_message) {
@@ -46,42 +57,58 @@ class MessageFragment : Fragment(R.layout.fragment_message) {
 
     val messageViewModel by viewModels<MessageViewModel>()
 
+    private val messagingId: MessagingId by lazy {
+        arguments?.getSerializable(EXTRA_MESSAGING_ID) as MessagingId
+    }
+
+    lateinit var messageActionViewModel: MessageActionViewModel
+
+    @Inject
+    lateinit var accountStore: AccountStore
+
+    @Inject
+    lateinit var metaRepository: MetaRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val factory = MessageActionViewModel.Factory(messagingId, requireContext().applicationContext as MiApplication)
+        messageActionViewModel = ViewModelProvider(this, factory)[MessageActionViewModel::class.java]
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        if (!BuildConfig.DEBUG) {
+//        if (!BuildConfig.DEBUG) {
             return super.onCreateView(inflater, container, savedInstanceState)
-        }
+//        }
 
-        return ComposeView(requireContext()).apply {
-            setContent {
-                MdcTheme {
-
-                }
-            }
-        }.rootView
+//        return ComposeView(requireContext()).apply {
+//            setContent {
+//                MdcTheme {
+//
+//                }
+//            }
+//        }.rootView
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val messagingId = arguments?.getSerializable(EXTRA_MESSAGING_ID) as? MessagingId
 
-        require(messagingId != null) {
-            "messageHistory must not null"
-        }
-
-        if (BuildConfig.DEBUG) {
-            return
-        }
+//        if (BuildConfig.DEBUG) {
+//            return
+//        }
 
         val lm = LinearLayoutManager(context).apply {
             stackFromEnd = true
         }
 
+        mBinding.actionViewModel = messageActionViewModel
         mBinding.messagesView.layoutManager = lm
+
         mLayoutManager = lm
 
         messageViewModel.setMessagingId(messagingId)
@@ -121,6 +148,23 @@ class MessageFragment : Fragment(R.layout.fragment_message) {
                 }
             }
         })
+
+        mBinding.openDrive.setOnClickListener {
+            openDriveActivity()
+        }
+
+        accountStore.observeCurrentAccount.filterNotNull().flatMapLatest {
+            metaRepository.observe(it.instanceDomain)
+        }.distinctUntilChanged().filterNotNull().onEach {
+            mBinding.inputMessage.setTokenizer(CustomEmojiTokenizer())
+            mBinding.inputMessage.setAdapter(
+                CustomEmojiCompleteAdapter(
+                    it.emojis ?: emptyList(),
+                    requireContext()
+                )
+            )
+        }.launchIn(lifecycleScope)
+
     }
 
 
@@ -149,6 +193,24 @@ class MessageFragment : Fragment(R.layout.fragment_message) {
 
         override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
 
+        }
+    }
+
+    private fun openDriveActivity(){
+        val intent = Intent(requireActivity(), DriveActivity::class.java)
+        intent.putExtra(DriveActivity.EXTRA_INT_SELECTABLE_FILE_MAX_SIZE, 1)
+        intent.action = Intent.ACTION_OPEN_DOCUMENT
+        intent.action = Intent.ACTION_OPEN_DOCUMENT
+        openDriveActivityForPickFileResult.launch(intent)
+    }
+
+    private val openDriveActivityForPickFileResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result ->
+        val ids = (result.data?.getSerializableExtra(DriveActivity.EXTRA_SELECTED_FILE_PROPERTY_IDS) as? List<*>)?.map {
+            it as FileProperty.Id
+        }
+        ids?.firstOrNull()?.let {
+            messageActionViewModel.setFilePropertyFromId(it)
         }
     }
 }
