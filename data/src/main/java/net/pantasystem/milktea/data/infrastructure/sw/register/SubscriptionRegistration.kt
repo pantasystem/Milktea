b566855de73e986ac1e115482a4aeb5cf3264f4f
Panta <kinoshita062323@gmail.com>
2022-07-04 20:14:13 +0900

    feat: Factoryで生成するようにしました。

--- data/src/main/java/net/pantasystem/milktea/data/infrastructure/sw/register/SubscriptionRegistration.kt
+++ data/src/main/java/net/pantasystem/milktea/data/infrastructure/sw/register/SubscriptionRegistration.kt
@@ -1,14 +1,14 @@
 package net.pantasystem.milktea.data.infrastructure.sw.register
 
-import net.pantasystem.milktea.data.api.misskey.MisskeyAPIProvider
-import net.pantasystem.milktea.common.Encryption
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import net.pantasystem.milktea.api.misskey.register.Subscription
 import net.pantasystem.milktea.api.misskey.register.SubscriptionState
+import net.pantasystem.milktea.common.Encryption
 import net.pantasystem.milktea.common.Logger
 import net.pantasystem.milktea.common.throwIfHasError
+import net.pantasystem.milktea.data.api.misskey.MisskeyAPIProvider
 import net.pantasystem.milktea.model.account.AccountRepository
 
 class SubscriptionRegistration(
