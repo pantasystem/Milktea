d809e370e592a637f3a3e81e7493c461630ba5b3
Panta <kinoshita062323@gmail.com>
2022-07-04 19:45:35 +0900

    feat: Subscription周りの機能をHiltでDIするためのModuleを作成しました。

---
+++ app/src/main/java/jp/panta/misskeyandroidclient/di/module/PushSubscriptionModule.kt
@@ -0,0 +1,53 @@
+package jp.panta.misskeyandroidclient.di.module
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jp.panta.misskeyandroidclient.BuildConfig
+import net.pantasystem.milktea.common.Encryption
+import net.pantasystem.milktea.common.Logger
+import net.pantasystem.milktea.data.api.misskey.MisskeyAPIProvider
+import net.pantasystem.milktea.data.infrastructure.sw.register.SubscriptionRegistration
+import net.pantasystem.milktea.data.infrastructure.sw.register.SubscriptionUnRegistration
+import net.pantasystem.milktea.model.account.AccountRepository
+import java.util.*
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PushSubscriptionModule {
+
+    fun provideSubscriptionRegistration(
+        accountRepository: AccountRepository,
+        encryption: Encryption,
+        misskeyAPIProvider: MisskeyAPIProvider,
+        loggerFactory: Logger.Factory
+    ): SubscriptionRegistration {
+        return SubscriptionRegistration(
+            accountRepository,
+            encryption,
+            misskeyAPIProvider,
+            lang = Locale.getDefault().language,
+            loggerFactory,
+            auth = BuildConfig.PUSH_TO_FCM_AUTH,
+            publicKey = BuildConfig.PUSH_TO_FCM_PUBLIC_KEY,
+            endpointBase = BuildConfig.PUSH_TO_FCM_SERVER_BASE_URL,
+        )
+    }
+
+    fun provideUnSubscriptionRegistration(
+        accountRepository: AccountRepository,
+        encryption: Encryption,
+        misskeyAPIProvider: MisskeyAPIProvider,
+        loggerFactory: Logger.Factory
+    ): SubscriptionUnRegistration {
+        return SubscriptionUnRegistration(
+            accountRepository,
+            encryption,
+            lang = Locale.getDefault().language,
+            misskeyAPIProvider = misskeyAPIProvider,
+            endpointBase = BuildConfig.PUSH_TO_FCM_SERVER_BASE_URL,
+            auth = BuildConfig.PUSH_TO_FCM_AUTH,
+            publicKey = BuildConfig.PUSH_TO_FCM_PUBLIC_KEY,
+        )
+    }
+}
