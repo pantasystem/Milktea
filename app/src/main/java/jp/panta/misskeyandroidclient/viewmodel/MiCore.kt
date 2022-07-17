2daa321071a884ffa02d37d84d1d866834069e8d
Panta <kinoshita062323@gmail.com>
2022-07-18 01:14:19 +0900

    rm: 不要になったメソッドを削除

--- app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/MiCore.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/viewmodel/MiCore.kt
@@ -5,12 +5,10 @@ import net.pantasystem.milktea.data.infrastructure.sw.register.SubscriptionRegis
 import net.pantasystem.milktea.data.infrastructure.url.UrlPreviewStore
 import net.pantasystem.milktea.model.account.Account
 import net.pantasystem.milktea.model.instance.Meta
-import net.pantasystem.milktea.model.notes.reaction.ReactionHistoryDataSource
 
 interface MiCore {
     fun getUrlPreviewStore(account: Account): UrlPreviewStore?
     fun getSubscriptionRegistration(): SubscriptionRegistration
     fun getCurrentInstanceMeta(): Meta?
     fun getSettingStore(): SettingStore
-    fun getReactionHistoryDataSource(): ReactionHistoryDataSource
 }
