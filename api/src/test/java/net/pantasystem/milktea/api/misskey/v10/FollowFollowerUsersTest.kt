33aaec1876ea77bd97f00243c4c154a589804b4f
Panta <kinoshita062323@gmail.com>
2022-07-20 16:02:55 +0900

    feat: フォロワー一覧のデコードテストを行いました。

---
+++ api/src/test/java/net/pantasystem/milktea/api/misskey/v10/FollowFollowerUsersTest.kt
@@ -0,0 +1,24 @@
+package net.pantasystem.milktea.api.misskey.v10
+
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.junit.Test
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+
+class FollowFollowerUsersTest {
+
+    @Test
+    fun decodeJsonTest() {
+        val file = File(javaClass.classLoader!!.getResource("v10_followers_case1.json").file)
+        val reader = BufferedReader(InputStreamReader(BufferedInputStream(file.inputStream())))
+        val textJson = reader.readLines().reduce { acc, s -> acc + s }.trimIndent()
+        val decoder = Json {
+            ignoreUnknownKeys = true
+        }
+        val dto = decoder.decodeFromString<FollowFollowerUsers>(textJson)
+        println(dto)
+    }
+}
