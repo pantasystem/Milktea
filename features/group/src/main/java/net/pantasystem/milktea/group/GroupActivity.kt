9ab042e1c9f996e51afa8aeacca17e5fab2aa152
Panta <kinoshita062323@gmail.com>
2022-08-07 19:13:59 +0900

    feat: グループを一覧表示するためのViewModelを追加しました

---
+++ features/group/src/main/java/net/pantasystem/milktea/group/GroupActivity.kt
@@ -0,0 +1,11 @@
+package net.pantasystem.milktea.group
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group)
+    }
+}
