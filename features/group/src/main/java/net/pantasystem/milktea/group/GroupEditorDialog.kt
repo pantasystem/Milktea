835e0f52029da25e0420f9b5b3c73746c973bc88
Panta <kinoshita062323@gmail.com>
2022-08-14 15:31:26 +0900

    feat: グループ作成時用のダイアログを作成

---
+++ features/group/src/main/java/net/pantasystem/milktea/group/GroupEditorDialog.kt
@@ -0,0 +1,64 @@
+package net.pantasystem.milktea.group
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+@Stable
+fun GroupEditorDialog(
+    isNew: Boolean,
+    name: String,
+    onAction: (GroupEditorDialogAction) -> Unit,
+) {
+
+    Dialog(onDismissRequest = { onAction(GroupEditorDialogAction.OnDismiss) }) {
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            color = MaterialTheme.colors.surface,
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(if(isNew) "グループ作成" else "編集", fontSize = 24.sp)
+                Spacer(modifier = Modifier.height(8.dp))
+                TextField(
+                    value = name,
+                    placeholder = {
+                        Text("グループ名")
+                    },
+                    onValueChange = { text ->
+                        onAction(GroupEditorDialogAction.OnNameChanged(text))
+                    }
+                )
+                Row(
+                    Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    TextButton(onClick = {onAction(GroupEditorDialogAction.OnDismiss)}) {
+                        Text("Cancel")
+                    }
+                    TextButton(
+                        enabled = name.isNotBlank(),
+                        onClick = {
+                        onAction(GroupEditorDialogAction.OnSave)
+                        }
+                    ) {
+                        Text("Save")
+                    }
+                }
+            }
+        }
+    }
+}
+
+interface GroupEditorDialogAction {
+    object OnDismiss : GroupEditorDialogAction
+    data class OnNameChanged(val text: String) : GroupEditorDialogAction
+    object OnSave : GroupEditorDialogAction
+}
