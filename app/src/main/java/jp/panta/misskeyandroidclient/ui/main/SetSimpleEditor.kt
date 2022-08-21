eb61c6b2f1b75553c4daf753c4e6ff16f665958e
Panta <kinoshita062323@gmail.com>
2022-08-22 00:18:57 +0900

    SimpleEditorFragmentを動くように修正

--- app/src/main/java/jp/panta/misskeyandroidclient/ui/main/SetSimpleEditor.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/main/SetSimpleEditor.kt
@@ -1,8 +1,11 @@
 package jp.panta.misskeyandroidclient.ui.main
 
+import android.view.View
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import jp.panta.misskeyandroidclient.R
 import net.pantasystem.milktea.app_store.setting.SettingStore
+import net.pantasystem.milktea.note.view.editor.SimpleEditorFragment
 
 
 internal class SetSimpleEditor(
@@ -11,23 +14,23 @@ internal class SetSimpleEditor(
     private val fab: FloatingActionButton,
 ) {
     operator fun invoke() {
-//        val ft = fragmentManager.beginTransaction()
-//
-//        val editor = fragmentManager.findFragmentByTag("simpleEditor")
-//
-//        if (settingStore.isSimpleEditorEnabled) {
-//            fab.visibility = View.GONE
-//            if (editor == null) {
-//                ft.replace(R.id.simpleEditorBase, SimpleEditorFragment(), "simpleEditor")
-//            }
-//        } else {
-//            fab.visibility = View.VISIBLE
-//
-//            editor?.let {
-//                ft.remove(it)
-//            }
-//
-//        }
-//        ft.commit()
+        val ft = fragmentManager.beginTransaction()
+
+        val editor = fragmentManager.findFragmentByTag("simpleEditor")
+
+        if (settingStore.isSimpleEditorEnabled) {
+            fab.visibility = View.GONE
+            if (editor == null) {
+                ft.replace(R.id.simpleEditorBase, SimpleEditorFragment(), "simpleEditor")
+            }
+        } else {
+            fab.visibility = View.VISIBLE
+
+            editor?.let {
+                ft.remove(it)
+            }
+
+        }
+        ft.commit()
     }
 }
