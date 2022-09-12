68efac3de265db32657f6307c5c57fdfeb7b166f
Panta <kinoshita062323@gmail.com>
2022-09-12 13:44:26 +0900

    fix: clientIdの生成保存処理に問題があることが発覚したため修正

--- modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/account/ClientIdRepositoryImpl.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/account/ClientIdRepositoryImpl.kt
@@ -6,17 +6,18 @@ import net.pantasystem.milktea.model.account.ClientId
 import net.pantasystem.milktea.model.account.ClientIdRepository
 import javax.inject.Inject
 
+private const val CLIENT_ID = "milktea.CLIENT_ID"
 class ClientIdRepositoryImpl @Inject constructor(
     private val sharedPreferences: SharedPreferences
 ): ClientIdRepository {
     override fun getOrCreate(): ClientId {
-        val clientId = sharedPreferences.getString("milktea.CLIENT_ID", null)?.let {
+        val clientId = sharedPreferences.getString(CLIENT_ID, null)?.let {
             ClientId(it)
         }
         val newClientId = ClientId.createOrNothing(clientId)
         if(clientId == null) {
             sharedPreferences.edit {
-                putString(newClientId.clientId, "milktea.CLIENT_ID")
+                putString(CLIENT_ID, newClientId.clientId)
             }
         }
         return newClientId
