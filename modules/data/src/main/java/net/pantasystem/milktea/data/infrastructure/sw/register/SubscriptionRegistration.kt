fe42cf7a1b24c67b4ee0a22ba44050a4d835e452
Panta <kinoshita062323@gmail.com>
2022-09-13 20:04:57 +0900

    feat: ログイン時にregisterを呼び出すようにしました

--- modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/sw/register/SubscriptionRegistration.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/sw/register/SubscriptionRegistration.kt
@@ -27,7 +27,7 @@ class SubscriptionRegistration(
     /**
      * 特定のアカウントをsw/registerに登録します。
      */
-    private suspend fun register(accountId: Long) : SubscriptionState?{
+    suspend fun register(accountId: Long) : Result<SubscriptionState?> = runCatching {
         val token = FirebaseMessaging.getInstance().token.asSuspend()
         logger.debug("call register(accountId:$accountId)")
         logger.debug("auth:$auth, publicKey:$publicKey")
@@ -53,7 +53,7 @@ class SubscriptionRegistration(
         )
         res.throwIfHasError()
         logger.debug("res code:${res.code()}, body:${res.body()}")
-        return res.body()
+        res.body()
     }
 
     /**
@@ -65,9 +65,7 @@ class SubscriptionRegistration(
         return coroutineScope {
             accounts.map {
                 async {
-                    runCatching {
-                        register(it.accountId)
-                    }.onFailure {
+                    register(it.accountId).onFailure {
                         logger.error("sw/registerに失敗しました", it)
                     }.onSuccess { result ->
                         logger.debug("subscription result:${result}")
