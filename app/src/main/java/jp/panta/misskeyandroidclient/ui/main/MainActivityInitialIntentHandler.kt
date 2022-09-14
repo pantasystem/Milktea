3b49d31c9c8b5c6a19e9d1aabc495cc8ed78e2e0
Panta <kinoshita062323@gmail.com>
2022-09-15 00:54:59 +0900

    feat: 通知と対応するアカウントに切り替えて通知を開くようにしました。

--- app/src/main/java/jp/panta/misskeyandroidclient/ui/main/MainActivityInitialIntentHandler.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/main/MainActivityInitialIntentHandler.kt
@@ -2,11 +2,16 @@ package jp.panta.misskeyandroidclient.ui.main
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import jp.panta.misskeyandroidclient.R
+import kotlinx.coroutines.launch
 import net.pantasystem.milktea.common_navigation.UserDetailNavigation
 import net.pantasystem.milktea.common_navigation.UserDetailNavigationArgs
+import net.pantasystem.milktea.model.account.AccountRepository
 import net.pantasystem.milktea.model.notes.Note
+import net.pantasystem.milktea.model.notification.PushNotification
 import net.pantasystem.milktea.model.notification.toPushNotification
 import net.pantasystem.milktea.model.user.User
 import net.pantasystem.milktea.note.NoteDetailActivity
@@ -19,36 +24,49 @@ import net.pantasystem.milktea.note.NoteDetailActivity
 class MainActivityInitialIntentHandler(
     private val bottomNavigationView: BottomNavigationView,
     private val activity: AppCompatActivity,
-    private val userDetailNavigation: UserDetailNavigation
+    private val userDetailNavigation: UserDetailNavigation,
+    private val accountRepository: AccountRepository,
 ) {
 
     operator fun invoke(intent: Intent) {
         // NOTE: バックグラウンドから通知を開いた場合、通知のデータがintentに入っている
         val pushNotification = intent.extras?.toPushNotification()?.getOrNull()
         if (pushNotification != null) {
-            bottomNavigationView.selectedItemId = R.id.navigation_notification
-            when {
-                pushNotification.isNearUserNotification() -> {
-                    activity.startActivity(
-                        userDetailNavigation.newIntent(
-                            UserDetailNavigationArgs.UserId(
+            activity.lifecycleScope.launch {
+                accountRepository.get(pushNotification.accountId).mapCatching {
+                    accountRepository.setCurrentAccount(it).getOrThrow()
+                }.onSuccess {
+                    navigateBy(pushNotification)
+                }.onFailure {
+                    FirebaseCrashlytics.getInstance().recordException(it)
+                }
+            }
+            return
+        }
+    }
+
+    private fun navigateBy(pushNotification: PushNotification) {
+        bottomNavigationView.selectedItemId = R.id.navigation_notification
+        when {
+            pushNotification.isNearUserNotification() -> {
+                activity.startActivity(
+                    userDetailNavigation.newIntent(
+                        UserDetailNavigationArgs.UserId(
                             userId = User.Id(
                                 pushNotification.accountId,
                                 pushNotification.userId!!
                             )
                         ))
-                    )
-                }
-                pushNotification.isNearNoteNotification() -> {
-                    activity.startActivity(
-                        NoteDetailActivity.newIntent(activity, Note.Id(
-                            pushNotification.accountId,
-                            pushNotification.noteId!!,
-                        ))
-                    )
-                }
+                )
             }
-            return
+            pushNotification.isNearNoteNotification() -> {
+                activity.startActivity(
+                    NoteDetailActivity.newIntent(activity, Note.Id(
+                        pushNotification.accountId,
+                        pushNotification.noteId!!,
+                    ))
+                )
+            }
         }
     }
 }
