b7b33add6e83bb112780eedf732e5cbdc3433b94
Panta <kinoshita062323@gmail.com>
2022-09-26 16:39:52 +0900

    fix: 条件を修正

--- modules/model/src/main/java/net/pantasystem/milktea/model/notes/muteword/WordFilterConfig.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/notes/muteword/WordFilterConfig.kt
@@ -4,4 +4,22 @@ data class WordFilterConfig(
     val conditions: List<FilterConditionType>
 ) {
     companion object
+
+    fun checkMatchText(text: String?): Boolean {
+        if (text == null) {
+            return false
+        }
+        return conditions.any { type ->
+            when(type) {
+                is FilterConditionType.Normal -> {
+                    type.words.all { word ->
+                        text.contains(word)
+                    }
+                }
+                is FilterConditionType.Regex -> {
+                    type.regex.matches(text)
+                }
+            }
+        }
+    }
 }
