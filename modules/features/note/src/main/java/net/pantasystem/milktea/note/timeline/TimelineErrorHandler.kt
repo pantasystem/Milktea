16d24e6e1ee260b93a6c190de5637e5b943907b4
Panta <kinoshita062323@gmail.com>
2022-10-15 15:49:57 +0900

    feat:認証エラーを表示するようにしました

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/timeline/TimelineErrorHandler.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/timeline/TimelineErrorHandler.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import android.widget.Toast
 import net.pantasystem.milktea.common.APIError
+import net.pantasystem.milktea.model.account.UnauthorizedException
 import net.pantasystem.milktea.note.R
 import java.io.IOException
 import java.net.SocketTimeoutException
@@ -15,13 +16,12 @@ class TimelineErrorHandler(
     operator fun invoke(error: Throwable) {
         Log.e("TimelineErrorHandler", "error", error)
         when (error) {
-            is IOException -> {
-                Toast.makeText(context, R.string.network_error, Toast.LENGTH_LONG)
-                    .show()
-
-            }
             is SocketTimeoutException -> {
                 Toast.makeText(context, R.string.timeout_error, Toast.LENGTH_LONG)
+                    .show()
+            }
+            is IOException -> {
+                Toast.makeText(context, R.string.network_error, Toast.LENGTH_LONG)
                     .show()
 
             }
@@ -41,6 +41,13 @@ class TimelineErrorHandler(
                 Toast.makeText(
                     context,
                     R.string.parameter_error,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            is UnauthorizedException -> {
+                Toast.makeText(
+                    context,
+                    R.string.timeline_unauthorized_error,
                     Toast.LENGTH_LONG
                 ).show()
             }
