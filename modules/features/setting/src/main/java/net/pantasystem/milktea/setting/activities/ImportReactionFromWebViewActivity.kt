d068da4ab5c07d0922049169d8c1036b526502d4
Panta <kinoshita062323@gmail.com>
2022-10-17 16:01:55 +0900

    feat: レイアウトを更新、Activityを追加

---
+++ modules/features/setting/src/main/java/net/pantasystem/milktea/setting/activities/ImportReactionFromWebViewActivity.kt
@@ -0,0 +1,12 @@
+package net.pantasystem.milktea.setting.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.pantasystem.milktea.setting.R
+
+class ImportReactionFromWebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_import_reaction_from_web_view)
+    }
+}
