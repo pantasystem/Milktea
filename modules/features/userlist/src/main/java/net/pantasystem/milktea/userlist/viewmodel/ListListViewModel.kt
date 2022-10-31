9606020cea2062045c76a05e1c0c7bc9fb62d80a
Panta <kinoshita062323@gmail.com>
2022-10-31 16:01:50 +0900

    fix

--- modules/features/userlist/src/main/java/net/pantasystem/milktea/userlist/viewmodel/ListListViewModel.kt
+++ modules/features/userlist/src/main/java/net/pantasystem/milktea/userlist/viewmodel/ListListViewModel.kt
@@ -8,7 +8,10 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import net.pantasystem.milktea.app_store.account.AccountStore
-import net.pantasystem.milktea.common.*
+import net.pantasystem.milktea.common.Logger
+import net.pantasystem.milktea.common.ResultState
+import net.pantasystem.milktea.common.StateContent
+import net.pantasystem.milktea.common.asLoadingStateFlow
 import net.pantasystem.milktea.model.account.AccountRepository
 import net.pantasystem.milktea.model.account.page.Page
 import net.pantasystem.milktea.model.account.page.Pageable
