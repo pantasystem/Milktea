13c9e8c90e805f1e1cd7f2b4c006e7dbcadf2474
Panta <kinoshita062323@gmail.com>
2022-11-11 00:16:44 +0900

    UIStateパターンにリファクタリングしました

--- modules/features/user/src/main/java/net/pantasystem/milktea/user/viewmodel/FollowFollowerViewModel.kt
+++ modules/features/user/src/main/java/net/pantasystem/milktea/user/viewmodel/FollowFollowerViewModel.kt
@@ -2,56 +2,118 @@ package net.pantasystem.milktea.user.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import net.pantasystem.milktea.app_store.user.FollowFollowerPagingStore
 import net.pantasystem.milktea.app_store.user.RequestType
+import net.pantasystem.milktea.common.PageableState
+import net.pantasystem.milktea.model.user.User
 
 class FollowFollowerViewModel @AssistedInject constructor(
     followFollowerPagingStoreFactory: FollowFollowerPagingStore.Factory,
-    @Assisted val type: RequestType
+    @Assisted val userId: User.Id
 ) : ViewModel() {
 
     companion object
 
     @AssistedFactory
     interface ViewModelAssistedFactory {
-        fun create(type: RequestType): FollowFollowerViewModel
+        fun create(userId: User.Id): FollowFollowerViewModel
     }
 
-    private val followFollowerPagingStore = followFollowerPagingStoreFactory.create(type)
+    private val followingPagingStore =
+        followFollowerPagingStoreFactory.create(RequestType.Following(userId))
+    private val followerPagingStore =
+        followFollowerPagingStoreFactory.create(RequestType.Follower(userId))
 
-
-    val users = followFollowerPagingStore.users.asLiveData()
-    val state = followFollowerPagingStore.state
-
-
+    private val followUsers = followingPagingStore.users.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        emptyList()
+    )
+    private val followerUsers = followerPagingStore.users.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        emptyList()
+    )
+    private val followUsersState = followingPagingStore.state.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        PageableState.Loading.Init()
+    )
+    private val followerUsersState = followerPagingStore.state.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        PageableState.Loading.Init()
+    )
+    val uiState = combine(
+        followUsers,
+        followerUsers,
+        followerUsersState,
+        followUsersState
+    ) { follows, followers, followerState, followState ->
+        FollowFollowerUiState(
+            followerUsers = followers,
+            followUsers = follows,
+            followerUsersState = followerState,
+            followUsersState = followState,
+        )
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        FollowFollowerUiState()
+    )
 
     fun loadInit() = viewModelScope.launch(Dispatchers.IO) {
-        followFollowerPagingStore.clear()
-        followFollowerPagingStore.loadPrevious()
+        launch {
+            followerPagingStore.clear()
+            followerPagingStore.loadPrevious()
+        }
+        launch {
+            followingPagingStore.clear()
+            followingPagingStore.loadPrevious()
+        }
+
     }
 
 
-    fun loadOld() = viewModelScope.launch(Dispatchers.IO) {
-        followFollowerPagingStore.loadPrevious()
+    fun loadOld(loadType: LoadType) = viewModelScope.launch(Dispatchers.IO) {
+        when(loadType) {
+            LoadType.Follow -> {
+                followingPagingStore.loadPrevious()
+            }
+            LoadType.Follower -> {
+                followerPagingStore.loadPrevious()
+            }
+        }
     }
-
 
 
 }
 
+enum class LoadType {
+    Follow, Follower
+}
+data class FollowFollowerUiState(
+    val followUsers: List<User.Detail> = emptyList(),
+    val followerUsers: List<User.Detail> = emptyList(),
+    val followerUsersState: PageableState<List<User.Id>> = PageableState.Loading.Init(),
+    val followUsersState: PageableState<List<User.Id>> = PageableState.Loading.Init()
+)
+
 fun FollowFollowerViewModel.Companion.provideFactory(
     factory: FollowFollowerViewModel.ViewModelAssistedFactory,
-    type: RequestType,
+    userId: User.Id,
 ) = object : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         @Suppress("UNCHECKED_CAST")
-        return factory.create(type) as T
+        return factory.create(userId) as T
     }
 }
