23332806a2437feb39b09cf8bc1f80191fd19b65
Panta <kinoshita062323@gmail.com>
2022-11-12 14:46:14 +0900

    wip

--- server/api/pkg/handler/push_to_fcm.go
+++ server/api/pkg/handler/push_to_fcm.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -15,6 +16,38 @@ type PushToFCMHandler struct {
 	Decrypter domain.Decrypter
 }
 
+type NotifyMsg struct {
+	Type  string
+	Title string
+	Body  string
+}
+
+type RemoteUserDTO struct {
+	Name     *string `json:"name"`
+	UserName string  `json:"username"`
+}
+
+type RemoteNoteDTO struct {
+	Text  *string          `json:"text"`
+	CW    *string          `json:"cw"`
+	Files *[]RemoteFileDTO `json:"files"`
+	Poll  *RemotePollDTO   `json:"poll"`
+}
+type RemoteNotifyDTO struct {
+	Type string         `json:"type"`
+	Id   string         `json:"id"`
+	User *RemoteUserDTO `json:"user"`
+	Note *RemoteNoteDTO `json:"note"`
+}
+
+type RemoteFileDTO struct {
+	Id string `json:"id"`
+}
+
+type RemotePollDTO struct {
+	Choices *[]interface{} `json:"choices"`
+}
+
 func (r PushToFCMHandler) Setup(e *gin.Engine) {
 	e.POST("/webpushcallback", func(ctx *gin.Context) {
 		deviceToken, dtErr := ctx.GetQuery("deviceToken")
@@ -27,14 +60,34 @@ func (r PushToFCMHandler) Setup(e *gin.Engine) {
 		fmt.Printf("deviceToken:%s, accountId:%s", deviceToken, accountId)
 		body, err := ioutil.ReadAll(ctx.Request.Body)
 		if err != nil {
-			ctx.Status(500)
+			ctx.Status(400)
 		}
 
 		decryptedBody, err := r.Decrypter.Decrypt(string(body))
 		if err != nil {
 			fmt.Printf("decrypt error:%s", err.Error())
-			ctx.Status(500)
+			ctx.Status(400)
+			return
 		}
+		var jsonObj map[string]interface{}
+		if json.Unmarshal([]byte(*decryptedBody), &jsonObj); err != nil {
+			fmt.Printf("JSONデコードに失敗したためキャンセル:%s", *decryptedBody)
+			ctx.Status(400)
+			return
+		}
+
+		t := jsonObj["type"]
+		if t != "notification" {
+			fmt.Printf("通知ではないためキャンセル:%s", *decryptedBody)
+			ctx.Status(400)
+			return
+		}
+
+		// r.GenerateNotifyMsg(jsonObj)
 		ctx.JSON(http.StatusOK, decryptedBody)
 	})
 }
+
+// func (r PushToFCMHandler) GenerateNotifyMsg(jsonObj map[string]interface{}) (*NotifyMsg, error) {
+
+// }
