462251566384f10bf24137ce44877f1c1e79d0c7
Panta <kinoshita062323@gmail.com>
2022-11-14 17:57:57 +0900

    feat: drag and dropの実験に成功

--- modules/features/setting/src/main/java/net/pantasystem/milktea/setting/activities/PageSettingActivity.kt
+++ modules/features/setting/src/main/java/net/pantasystem/milktea/setting/activities/PageSettingActivity.kt
@@ -1,25 +1,34 @@
 package net.pantasystem.milktea.setting.activities
 
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
+import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.composethemeadapter.MdcTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import net.pantasystem.milktea.common.ui.ApplyTheme
 import net.pantasystem.milktea.common_navigation.*
 import net.pantasystem.milktea.common_navigation.SearchAndSelectUserNavigation.Companion.EXTRA_SELECTED_USER_CHANGED_DIFF
-import net.pantasystem.milktea.model.account.page.PageType
-import net.pantasystem.milktea.setting.*
-import net.pantasystem.milktea.setting.R
-import net.pantasystem.milktea.setting.databinding.ActivityPageSettingBinding
 import net.pantasystem.milktea.setting.viewmodel.page.PageSettingViewModel
 import javax.inject.Inject
 
@@ -52,64 +61,87 @@ class PageSettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         applyTheme()
-        val binding = DataBindingUtil.setContentView<ActivityPageSettingBinding>(this,
-            R.layout.activity_page_setting
-        )
-        binding.lifecycleOwner = this
-        setSupportActionBar(binding.pageSettingToolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        val binding = DataBindingUtil.setContentView<ActivityPageSettingBinding>(this,
+//            R.layout.activity_page_setting
+//        )
+//        binding.lifecycleOwner = this
+//        setSupportActionBar(binding.pageSettingToolbar)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//
+//
+//
+//        val touchHelper = ItemTouchHelper(ItemTouchCallback())
+//        touchHelper.attachToRecyclerView(binding.pagesView)
+//        binding.pagesView.addItemDecoration(touchHelper)
+//
+//        val pagesAdapter = PagesAdapter(mPageSettingViewModel)
+//        binding.pagesView.adapter = pagesAdapter
+//        binding.pagesView.layoutManager = LinearLayoutManager(this)
+//
+//        mPageSettingViewModel.selectedPages.observe(this) {
+//            Log.d("PageSettingActivity", "選択済みページが更新された")
+//            pagesAdapter.submitList(it)
+//        }
+//
+//        binding.addPageButton.setOnClickListener {
+//            SelectPageToAddDialog().show(supportFragmentManager, "Activity")
+//        }
+//
+//        mPageSettingViewModel.pageOnActionEvent.observe(this) {
+//            PageSettingActionDialog().show(supportFragmentManager, "PSA")
+//        }
+//
+//        mPageSettingViewModel.pageOnUpdateEvent.observe(this) {
+//            EditTabNameDialog().show(supportFragmentManager, "ETD")
+//        }
+//
+//        mPageSettingViewModel.pageAddedEvent.observe(this) { pt ->
+//            when (pt) {
+//                PageType.SEARCH, PageType.SEARCH_HASH -> startActivity(
+//                    searchNavigation.newIntent(SearchNavType.SearchScreen())
+//                )
+//                PageType.USER -> {
+//                    val intent =
+//                        searchAndSelectUserNavigation.newIntent(SearchAndSelectUserNavigationArgs( selectableMaximumSize = 1))
+//                    launchSearchAndSelectUserForAddUserTimelineTab.launch(intent)
+//                }
+//                PageType.USER_LIST -> startActivity(userListNavigation.newIntent(UserListArgs()))
+//                PageType.DETAIL -> startActivity(searchNavigation.newIntent(SearchNavType.SearchScreen()))
+//                PageType.ANTENNA -> startActivity(antennaNavigation.newIntent(Unit))
+//                PageType.USERS_GALLERY_POSTS -> {
+//                    val intent =
+//                        searchAndSelectUserNavigation.newIntent(SearchAndSelectUserNavigationArgs( selectableMaximumSize = 1))
+//                    launchSearchAndSelectUserForAddGalleryTab.launch(intent)
+//                }
+//                PageType.CHANNEL_TIMELINE -> {
+//                    val intent = channelNavigation.newIntent(Unit)
+//                    startActivity(intent)
+//                }
+//                else -> {
+//                    // auto add
+//                }
+//            }
+//        }
 
 
-
-        val touchHelper = ItemTouchHelper(ItemTouchCallback())
-        touchHelper.attachToRecyclerView(binding.pagesView)
-        binding.pagesView.addItemDecoration(touchHelper)
-
-        val pagesAdapter = PagesAdapter(mPageSettingViewModel)
-        binding.pagesView.adapter = pagesAdapter
-        binding.pagesView.layoutManager = LinearLayoutManager(this)
-
-        mPageSettingViewModel.selectedPages.observe(this) {
-            Log.d("PageSettingActivity", "選択済みページが更新された")
-            pagesAdapter.submitList(it)
-        }
-
-        binding.addPageButton.setOnClickListener {
-            SelectPageToAddDialog().show(supportFragmentManager, "Activity")
-        }
-
-        mPageSettingViewModel.pageOnActionEvent.observe(this) {
-            PageSettingActionDialog().show(supportFragmentManager, "PSA")
-        }
-
-        mPageSettingViewModel.pageOnUpdateEvent.observe(this) {
-            EditTabNameDialog().show(supportFragmentManager, "ETD")
-        }
-
-        mPageSettingViewModel.pageAddedEvent.observe(this) { pt ->
-            when (pt) {
-                PageType.SEARCH, PageType.SEARCH_HASH -> startActivity(
-                    searchNavigation.newIntent(SearchNavType.SearchScreen())
-                )
-                PageType.USER -> {
-                    val intent =
-                        searchAndSelectUserNavigation.newIntent(SearchAndSelectUserNavigationArgs( selectableMaximumSize = 1))
-                    launchSearchAndSelectUserForAddUserTimelineTab.launch(intent)
+        setContent {
+            MdcTheme {
+                var list by remember {
+                    mutableStateOf((0 until 100).map { it.toString() })
                 }
-                PageType.USER_LIST -> startActivity(userListNavigation.newIntent(UserListArgs()))
-                PageType.DETAIL -> startActivity(searchNavigation.newIntent(SearchNavType.SearchScreen()))
-                PageType.ANTENNA -> startActivity(antennaNavigation.newIntent(Unit))
-                PageType.USERS_GALLERY_POSTS -> {
-                    val intent =
-                        searchAndSelectUserNavigation.newIntent(SearchAndSelectUserNavigationArgs( selectableMaximumSize = 1))
-                    launchSearchAndSelectUserForAddGalleryTab.launch(intent)
-                }
-                PageType.CHANNEL_TIMELINE -> {
-                    val intent = channelNavigation.newIntent(Unit)
-                    startActivity(intent)
-                }
-                else -> {
-                    // auto add
+                Scaffold() {
+                    ReorderList(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it),
+                        list = list,
+                        onMove = { from, to ->
+                            val tmp = list[to]
+                            val mutable = list.toMutableList()
+                            mutable[to] = mutable[from]
+                            mutable[from] = tmp
+                            list = mutable
+                        }
+                    )
                 }
             }
         }
@@ -122,13 +154,16 @@ class PageSettingActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             android.R.id.home -> finish()
         }
         return super.onOptionsItemSelected(item)
     }
 
-    inner class ItemTouchCallback : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN, ItemTouchHelper.ACTION_STATE_IDLE){
+    inner class ItemTouchCallback : ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+        ItemTouchHelper.ACTION_STATE_IDLE
+    ) {
         override fun onMove(
             recyclerView: RecyclerView,
             viewHolder: RecyclerView.ViewHolder,
@@ -136,7 +171,7 @@ class PageSettingActivity : AppCompatActivity() {
         ): Boolean {
             val from = viewHolder.absoluteAdapterPosition
             val to = target.absoluteAdapterPosition
-            val exList = ArrayList(mPageSettingViewModel.selectedPages.value?: emptyList())
+            val exList = ArrayList(mPageSettingViewModel.selectedPages.value ?: emptyList())
             val d = exList.removeAt(from)
             exList.add(to, d)
             mPageSettingViewModel.setList(exList)
@@ -147,20 +182,151 @@ class PageSettingActivity : AppCompatActivity() {
     }
 
 
-
-    private val launchSearchAndSelectUserForAddGalleryTab = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode == RESULT_OK && it.data != null) {
-            val changeDiff = it.data!!.getSerializableExtra(EXTRA_SELECTED_USER_CHANGED_DIFF) as ChangedDiffResult
-            mPageSettingViewModel.addUsersGalleryByIds(changeDiff.selected)
+    private val launchSearchAndSelectUserForAddGalleryTab =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK && it.data != null) {
+                val changeDiff =
+                    it.data!!.getSerializableExtra(EXTRA_SELECTED_USER_CHANGED_DIFF) as ChangedDiffResult
+                mPageSettingViewModel.addUsersGalleryByIds(changeDiff.selected)
+            }
         }
+
+    private val launchSearchAndSelectUserForAddUserTimelineTab =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK && it.data != null) {
+                val changeDiff =
+                    it.data!!.getSerializableExtra(EXTRA_SELECTED_USER_CHANGED_DIFF) as ChangedDiffResult
+                mPageSettingViewModel.addUserPageByIds(changeDiff.selected)
+            }
+        }
+
+
+}
+
+
+@Composable
+fun ReorderList(
+    modifier: Modifier,
+    list: List<String>,
+    onMove: (fromIndex: Int, toIndex: Int) -> Unit
+) {
+    val listState = rememberLazyListState()
+
+    var initiallyDraggedElement by remember { mutableStateOf<LazyListItemInfo?>(null) }
+
+    var currentIndexOfDraggedItem by remember { mutableStateOf<Int?>(null) }
+
+    var draggedDistance by remember {
+        mutableStateOf<Float>(0f)
     }
 
-    private val launchSearchAndSelectUserForAddUserTimelineTab = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode == RESULT_OK && it.data != null) {
-            val changeDiff = it.data!!.getSerializableExtra(EXTRA_SELECTED_USER_CHANGED_DIFF) as ChangedDiffResult
-            mPageSettingViewModel.addUserPageByIds(changeDiff.selected)
-        }
+    var overscrollJob by remember { mutableStateOf<Job?>(null) }
+
+
+    val scope = rememberCoroutineScope()
+
+
+    fun checkForOverScroll(): Float {
+        return initiallyDraggedElement?.let {
+            val startOffset = it.offset + draggedDistance
+            val endOffset = (it.offset + it.size) + draggedDistance
+            val viewPortStart = listState.layoutInfo.viewportStartOffset
+            val viewPortEnd = listState.layoutInfo.viewportEndOffset
+
+            when {
+                draggedDistance > 0 -> (endOffset - viewPortEnd).takeIf { diff -> diff > 0 }
+                draggedDistance < 0 -> (startOffset - viewPortStart).takeIf { diff -> diff < 0 }
+                else -> null
+            }
+        } ?: 0f
     }
 
 
+    LazyColumn(modifier = modifier.pointerInput(Unit) {
+        detectDragGesturesAfterLongPress(
+            onDrag = { change, offset ->
+                change.consume()
+                draggedDistance += offset.y
+
+                val currentElementItemInfo = currentIndexOfDraggedItem?.let {
+                    listState.getVisibleItemInfoFor(it)
+                }
+
+
+                initiallyDraggedElement?.let {
+                    val startOffset = it.offset + draggedDistance
+                    val endOffset = it.offset + it.size + draggedDistance
+
+                    currentElementItemInfo?.let { hovered ->
+                        listState.layoutInfo.visibleItemsInfo.filterNot { item ->
+                            (item.offset + item.size) < startOffset || item.offset > endOffset
+                        }.firstOrNull { item ->
+                            val delta = startOffset - hovered.offset
+                            when {
+                                delta > 0 -> (endOffset > (item.offset + item.size))
+                                else -> (startOffset < item.offset)
+                            }
+                        }?.also { item ->
+                            currentIndexOfDraggedItem?.let { current ->
+                                onMove(current, item.index)
+                            }
+                            currentIndexOfDraggedItem = item.index
+                        }
+                    }
+                }
+                if (overscrollJob?.isActive == true) {
+                    return@detectDragGesturesAfterLongPress
+                }
+                checkForOverScroll().takeIf { o -> o != 0f }
+                    ?.let {
+                        overscrollJob = scope.launch {
+                            listState.scrollBy(it)
+                        }
+                    } ?: run { overscrollJob?.cancel() }
+            },
+            onDragStart = { offset ->
+                listState.layoutInfo.visibleItemsInfo
+                    .firstOrNull { item ->
+                        offset.y.toInt() in item.offset..(item.offset + item.size)
+                    }?.also {
+                        currentIndexOfDraggedItem = it.index
+                        initiallyDraggedElement = it
+                    }
+            },
+            onDragEnd = {
+                draggedDistance = 0f
+                currentIndexOfDraggedItem = null
+                initiallyDraggedElement = null
+                overscrollJob?.cancel()
+            },
+            onDragCancel = {
+                draggedDistance = 0f
+                currentIndexOfDraggedItem = null
+                initiallyDraggedElement = null
+                overscrollJob?.cancel()
+            }
+        )
+    }, state = listState) {
+        itemsIndexed(list) { index, item ->
+            Box(modifier = Modifier.graphicsLayer {
+                translationY = currentIndexOfDraggedItem
+                    ?.let {
+                        listState.getVisibleItemInfoFor(it)
+                    }
+                    ?.let { item -> (initiallyDraggedElement?.offset ?: 0f).toFloat() + draggedDistance - item.offset }
+                    .takeIf {
+                        index == currentIndexOfDraggedItem
+                    }?: 0f
+            }) {
+                Text(item)
+            }
+        }
+    }
+}
+
+fun LazyListState.getVisibleItemInfoFor(absoluteIndex: Int): LazyListItemInfo? {
+    return this
+        .layoutInfo
+        .visibleItemsInfo
+        .getOrNull(absoluteIndex - this.layoutInfo.visibleItemsInfo.first().index)
 }
