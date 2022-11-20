9b22dc3fc0ac921c5af312777c00faff51035f28
Panta <kinoshita062323@gmail.com>
2022-11-20 19:55:05 +0900

    rm: 不要になった処理を削除

--- modules/data/src/main/java/net/pantasystem/milktea/data/di/module/MetaModule.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/di/module/MetaModule.kt
@@ -5,16 +5,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import net.pantasystem.milktea.common.Logger
-import net.pantasystem.milktea.data.api.misskey.MisskeyAPIProvider
 import net.pantasystem.milktea.data.infrastructure.DataBase
 import net.pantasystem.milktea.data.infrastructure.instance.MetaRepositoryImpl
 import net.pantasystem.milktea.data.infrastructure.instance.db.InMemoryMetaDataSource
 import net.pantasystem.milktea.data.infrastructure.instance.db.MediatorMetaDataSource
 import net.pantasystem.milktea.data.infrastructure.instance.db.RoomMetaDataSource
-import net.pantasystem.milktea.data.infrastructure.instance.remote.RemoteFetchMeta
-import net.pantasystem.milktea.model.instance.FetchMeta
-import net.pantasystem.milktea.model.instance.MediatorFetchMeta
 import net.pantasystem.milktea.model.instance.MetaDataSource
 import net.pantasystem.milktea.model.instance.MetaRepository
 import javax.inject.Singleton
@@ -25,7 +20,7 @@ object MetaModule {
 
     @Provides
     @Singleton
-    fun metaRepository(db: DataBase): MetaDataSource {
+    fun provideMetaDataSource(db: DataBase): MetaDataSource {
         return MediatorMetaDataSource(
             RoomMetaDataSource(
                 db.metaDAO(),
@@ -36,19 +31,6 @@ object MetaModule {
         )
     }
 
-    @Provides
-    @Singleton
-    fun metaStore(
-        metaRepository: MetaDataSource,
-        loggerFactory: Logger.Factory,
-        misskeyAPIProvider: MisskeyAPIProvider,
-    ): FetchMeta {
-        return MediatorFetchMeta(
-            metaRepository,
-            RemoteFetchMeta(misskeyAPIProvider),
-            loggerFactory
-        )
-    }
 }
 
 @Module
