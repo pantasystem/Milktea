5001a203a5da60f24447a8d7c61290dab461b590
Panta <kinoshita062323@gmail.com>
2022-11-21 17:38:29 +0900

    feat: Jetpack Navigationを使って画面を管理するようにしました。

--- modules/features/channel/src/main/java/net/pantasystem/milktea/channel/ChannelActivity.kt
+++ modules/features/channel/src/main/java/net/pantasystem/milktea/channel/ChannelActivity.kt
@@ -6,11 +6,21 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.google.android.material.composethemeadapter.MdcTheme
 import dagger.hilt.android.AndroidEntryPoint
 import net.pantasystem.milktea.app_store.account.AccountStore
 import net.pantasystem.milktea.common.ui.ApplyTheme
+import net.pantasystem.milktea.common_android_ui.PageableFragmentFactory
 import net.pantasystem.milktea.common_navigation.ChannelNavigation
+import net.pantasystem.milktea.model.account.page.Pageable
 import javax.inject.Inject
 
 
@@ -24,19 +34,55 @@ class ChannelActivity : AppCompatActivity() {
     @Inject
     lateinit var setTheme: ApplyTheme
 
+    @Inject
+    lateinit var pageableFragmentFactory: PageableFragmentFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme.invoke()
 
         setContent {
+            val navController = rememberNavController()
             MdcTheme {
-                ChannelScreen(
-                    onNavigateUp = {
-                        finish()
-                    },
-                    accountStore = accountStore,
-                    channelViewModel = channelViewModel
-                )
+                NavHost(navController = navController, startDestination = "/channels") {
+                    composable("/channels") {
+                        ChannelScreen(
+                            onNavigateUp = {
+                                finish()
+                            },
+                            accountStore = accountStore,
+                            channelViewModel = channelViewModel,
+                            onNavigateChannelDetail = {
+                                navController.navigate("/accounts/${it.accountId}/channels/${it.channelId}")
+                            }
+                        )
+                    }
+                    composable(
+                        "/accounts/{${ChannelDetailArgs.accountId}}/channels/{${ChannelDetailArgs.channelId}}",
+                        arguments = listOf(
+                            navArgument(ChannelDetailArgs.accountId) {
+                                type = NavType.LongType
+                            },
+                            navArgument(ChannelDetailArgs.channelId) {
+                                type = NavType.StringType
+                            }
+                        )
+                    ) {
+                        val viewModel: ChannelDetailViewModel = hiltViewModel()
+                        val channel by viewModel.channel.collectAsState()
+                        ChannelDetailScreen(
+                            onNavigateUp = { navController.popBackStack() },
+                            channelId = viewModel.channelId,
+                            channel = channel,
+                            onUpdateFragment = { id, layout, channelId ->
+                                val fragment = pageableFragmentFactory.create(Pageable.ChannelTimeline(channelId.channelId))
+                                val ft = supportFragmentManager.beginTransaction()
+                                ft.replace(id, fragment)
+                                ft.commit()
+                            }
+                        )
+                    }
+                }
             }
         }
     }
