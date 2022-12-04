b3a24f6431583c3e7812439791c242ae8f61a148
Panta <kinoshita062323@gmail.com>
2022-12-04 17:37:01 +0900

    feat: 実装

--- modules/common_android_ui/src/main/java/net/pantasystem/milktea/common_android_ui/account/AccountSwitchingDialog.kt
+++ modules/common_android_ui/src/main/java/net/pantasystem/milktea/common_android_ui/account/AccountSwitchingDialog.kt
@@ -2,22 +2,13 @@ package net.pantasystem.milktea.common_android_ui.account
 
 import android.app.Dialog
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.activityViewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.composethemeadapter.MdcTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.launch
-import net.pantasystem.milktea.common_android_ui.R
-import net.pantasystem.milktea.common_android_ui.account.viewmodel.AccountViewData
 import net.pantasystem.milktea.common_android_ui.account.viewmodel.AccountViewModel
 import net.pantasystem.milktea.common_navigation.AuthorizationArgs
 import net.pantasystem.milktea.common_navigation.AuthorizationNavigation
@@ -29,59 +20,42 @@ class AccountSwitchingDialog : BottomSheetDialogFragment() {
     @Inject
     lateinit var authorizationNavigation: AuthorizationNavigation
 
-    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+    val viewModel: AccountViewModel by activityViewModels()
+
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dialog = super.onCreateDialog(savedInstanceState)
-        val view = View.inflate(context, R.layout.dialog_switch_account, null)
-        dialog.setContentView(view)
-
-
-        val activity = activity
-        if (activity == null) {
-            dismiss()
-            return dialog
-        }
-
-        view.findViewById<Button>(R.id.add_account).setOnClickListener {
-            startActivity(authorizationNavigation.newIntent(AuthorizationArgs.New))
-            dismiss()
-        }
-        val accountsView = view.findViewById<RecyclerView>(R.id.accounts_view)
-
-        accountsView.layoutManager = LinearLayoutManager(view.context)
-
-        val accountViewModel = ViewModelProvider(activity)[AccountViewModel::class.java]
-
-        val adapter = AccountListAdapter(diff, accountViewModel, activity)
-        lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                accountViewModel.accounts.collect {
-                    adapter.submitList(it)
+        return super.onCreateDialog(savedInstanceState).apply {
+            val view = ComposeView(requireContext()).apply {
+                setContent {
+                    MdcTheme {
+                        val uiState by viewModel.uiState.collectAsState()
+                        AccountSwitchingDialogLayout(
+                            uiState = uiState,
+                            onSettingButtonClicked = {
+                                // TODO: アカウント設定画面を作成してそこに遷移するようにする
+                                dismiss()
+                            },
+                            onAvatarIconClicked = {
+                                viewModel.showProfile(it.account)
+                                dismiss()
+                            },
+                            onAccountClicked = {
+                                viewModel.setSwitchTargetConnectionInstance(it.account)
+                                dismiss()
+                            },
+                            onAddAccountButtonClicked = {
+                                requireActivity().startActivity(authorizationNavigation.newIntent(AuthorizationArgs.New))
+                                dismiss()
+                            }
+                        )
+                    }
                 }
             }
-        }
-
-        accountsView.adapter = adapter
-        accountViewModel.switchTargetConnectionInstanceEvent.observe(activity) {
-            dismiss()
-        }
-        return dialog
-    }
-
-
-    @FlowPreview
-    @ExperimentalCoroutinesApi
-    private val diff = object : DiffUtil.ItemCallback<AccountViewData>() {
-        override fun areContentsTheSame(
-            oldItem: AccountViewData,
-            newItem: AccountViewData
-        ): Boolean {
-            return oldItem.account.accountId == newItem.account.accountId
-        }
-
-        override fun areItemsTheSame(oldItem: AccountViewData, newItem: AccountViewData): Boolean {
-            return oldItem.user == newItem.user
-                    && oldItem.account == newItem.account
+            setContentView(view)
         }
     }
+
+
+
 }
+
