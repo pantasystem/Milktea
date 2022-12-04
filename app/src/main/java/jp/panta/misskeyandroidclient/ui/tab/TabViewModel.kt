88d39aafcc4220b15187d07f0553fd984fdcca07
Panta <kinoshita062323@gmail.com>
2022-12-04 20:33:05 +0900

    feat: Home画面のタイトルの右側に現在のユーザーのアイコンを表示するようにしました

--- app/src/main/java/jp/panta/misskeyandroidclient/ui/tab/TabViewModel.kt
+++ app/src/main/java/jp/panta/misskeyandroidclient/ui/tab/TabViewModel.kt
@@ -3,19 +3,26 @@ package jp.panta.misskeyandroidclient.ui.tab
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
 import net.pantasystem.milktea.app_store.account.AccountStore
+import net.pantasystem.milktea.model.user.User
+import net.pantasystem.milktea.model.user.UserDataSource
 import javax.inject.Inject
 
 @HiltViewModel
 class TabViewModel @Inject constructor(
-    val accountStore: AccountStore
+    val accountStore: AccountStore,
+    private val userDataSource: UserDataSource,
 ): ViewModel() {
+    
+    val pages = accountStore.observeCurrentAccount.map {
+        it?.pages ?: emptyList()
+    }.stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
 
-    val currentAccount = accountStore.observeCurrentAccount.distinctUntilChanged().filterNotNull()
-        .stateIn(viewModelScope, SharingStarted.Lazily, null)
-
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val currentUser = accountStore.observeCurrentAccount.filterNotNull().flatMapLatest {
+        userDataSource.observe(User.Id(it.accountId, it.remoteId))
+    }.flowOn(Dispatchers.IO).stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), null)
 }
