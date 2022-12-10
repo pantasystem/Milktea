74c85c06e5fec6b722dd9e82647a72911a426bdc
Panta <kinoshita062323@gmail.com>
2022-12-11 00:47:27 +0900

    refactor: Compose部分を別ファイルとして切り出し

---
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/option/NoteOptionDialogLayout.kt
@@ -0,0 +1,110 @@
+package net.pantasystem.milktea.note.option
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import net.pantasystem.milktea.model.notes.Note
+import net.pantasystem.milktea.model.notes.NoteRelation
+import net.pantasystem.milktea.note.R
+import net.pantasystem.milktea.note.view.NormalBottomSheetDialogSelectionLayout
+
+@Composable
+fun NoteOptionDialogLayout(
+    uiState: NoteOptionUiState,
+    onShowDetailButtonClicked: (Note.Id) -> Unit,
+    onCopyTextButtonClicked: (text: Note?) -> Unit,
+    onShareButtonClicked: (note: Note?) -> Unit,
+    onTranslateButtonClicked: (noteId: Note.Id) -> Unit,
+    onDeleteFavoriteButtonClicked: (noteId: Note.Id) -> Unit,
+    onCreateFavoriteButtonClicked: (noteId: Note.Id) -> Unit,
+    onDeleteAndEditButtonClicked: (NoteRelation?) -> Unit,
+    onDeleteButtonClicked: (NoteRelation?) -> Unit,
+    onReportButtonClicked: (NoteRelation?) -> Unit,
+) {
+    Surface(Modifier.fillMaxWidth()) {
+        Column(Modifier.fillMaxWidth()) {
+            NormalBottomSheetDialogSelectionLayout(
+                onClick = {
+                    onShowDetailButtonClicked(requireNotNull(uiState.note?.id))
+                },
+                icon = Icons.Default.Info,
+                text = stringResource(id = R.string.show_detail)
+            )
+            NormalBottomSheetDialogSelectionLayout(
+                onClick = {
+                    onCopyTextButtonClicked(uiState.note)
+                },
+                icon = Icons.Default.ContentCopy,
+                text = stringResource(id = R.string.copy_content)
+            )
+            NormalBottomSheetDialogSelectionLayout(
+                onClick = {
+                    onShareButtonClicked(uiState.note)
+                },
+                icon = Icons.Default.Share,
+                text = stringResource(id = R.string.share)
+            )
+            Divider()
+            NormalBottomSheetDialogSelectionLayout(
+                onClick = {
+                    onTranslateButtonClicked(requireNotNull(uiState.noteId))
+                },
+                icon = Icons.Default.Translate,
+                text = stringResource(id = R.string.translate)
+            )
+            Divider()
+            if (uiState.noteState?.isFavorited == true) {
+                NormalBottomSheetDialogSelectionLayout(
+                    onClick = {
+                        onDeleteFavoriteButtonClicked(requireNotNull(uiState.noteId))
+                    },
+                    icon = Icons.Filled.Star,
+                    text = stringResource(
+                        id = R.string.remove_favorite
+                    )
+                )
+            } else {
+                NormalBottomSheetDialogSelectionLayout(
+                    onClick = {
+                        onCreateFavoriteButtonClicked(requireNotNull(uiState.noteId))
+                    },
+                    icon = Icons.Outlined.Star,
+                    text = stringResource(id = R.string.favorite)
+                )
+            }
+
+            if (uiState.isMyNote) {
+                Divider()
+                NormalBottomSheetDialogSelectionLayout(
+                    onClick = {
+                        onDeleteAndEditButtonClicked(uiState.noteRelation)
+                    },
+                    icon = Icons.Default.Edit,
+                    text = stringResource(id = R.string.delete_and_edit)
+                )
+                NormalBottomSheetDialogSelectionLayout(
+                    onClick = {
+                        onDeleteButtonClicked(uiState.noteRelation)
+                    },
+                    icon = Icons.Default.Delete,
+                    text = stringResource(id = R.string.remove_note)
+                )
+            }
+            Divider()
+            NormalBottomSheetDialogSelectionLayout(
+                onClick = {
+                    onReportButtonClicked(uiState.noteRelation)
+                },
+                icon = Icons.Default.Report,
+                text = stringResource(id = R.string.report)
+            )
+        }
+    }
+}
