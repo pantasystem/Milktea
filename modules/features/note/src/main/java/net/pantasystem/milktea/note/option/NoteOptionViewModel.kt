feee1966796ccc42f7a24e6c721d3e8f9b0a12b7
Panta <kinoshita062323@gmail.com>
2022-12-20 17:25:33 +0900

    refactor: NoteRepository経由でNoteStateを取得するようにしました

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/option/NoteOptionViewModel.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/option/NoteOptionViewModel.kt
@@ -8,11 +8,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import net.pantasystem.milktea.api.misskey.notes.NoteRequest
-import net.pantasystem.milktea.api.misskey.notes.NoteStateResponse
 import net.pantasystem.milktea.common.Logger
-import net.pantasystem.milktea.common.throwIfHasError
 import net.pantasystem.milktea.data.api.misskey.MisskeyAPIProvider
 import net.pantasystem.milktea.data.gettters.NoteRelationGetter
 import net.pantasystem.milktea.model.account.Account
@@ -20,6 +16,7 @@ import net.pantasystem.milktea.model.account.AccountRepository
 import net.pantasystem.milktea.model.notes.Note
 import net.pantasystem.milktea.model.notes.NoteRelation
 import net.pantasystem.milktea.model.notes.NoteRepository
+import net.pantasystem.milktea.model.notes.NoteState
 import javax.inject.Inject
 
 @HiltViewModel
@@ -42,7 +39,7 @@ class NoteOptionViewModel @Inject constructor(
     private val noteIdFlow = savedStateHandle.getStateFlow<Note.Id?>(NOTE_ID, null)
 
     private val noteState = noteIdFlow.filterNotNull().map { noteId ->
-        loadNoteState(noteId).onFailure {
+        noteRepository.findNoteState(noteId).onFailure {
             logger.error("noteState load error", it)
         }.getOrNull()
     }.stateIn(
@@ -105,23 +102,11 @@ class NoteOptionViewModel @Inject constructor(
         }
     }
 
-    private suspend fun loadNoteState(id: Note.Id): Result<NoteStateResponse> = runCatching {
-        withContext(Dispatchers.IO) {
-            val account = accountRepository.get(id.accountId).getOrThrow()
-            misskeyAPIProvider.get(account.normalizedInstanceDomain).noteState(
-                NoteRequest(
-                    i = account.token,
-                    noteId = id.noteId
-                )
-            ).throwIfHasError().body()!!
-        }
-    }
-
 }
 
 data class NoteOptionUiState(
     val noteId: Note.Id? = null,
-    val noteState: NoteStateResponse? = null,
+    val noteState: NoteState? = null,
     val note: Note? = null,
     val noteRelation: NoteRelation? = null,
     val isMyNote: Boolean = false,
