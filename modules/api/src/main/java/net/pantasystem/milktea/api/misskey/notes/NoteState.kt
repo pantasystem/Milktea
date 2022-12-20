8f855a72132d0506496d5cb6da3ff7d660395243
Panta <kinoshita062323@gmail.com>
2022-12-20 16:56:08 +0900

    feat: スレッドミュートの状態を取得できるように変更

--- modules/api/src/main/java/net/pantasystem/milktea/api/misskey/notes/NoteState.kt
+++ modules/api/src/main/java/net/pantasystem/milktea/api/misskey/notes/NoteState.kt
@@ -2,4 +2,9 @@ package net.pantasystem.milktea.api.misskey.notes
 
 import kotlinx.serialization.Serializable
 
-@Serializable data class NoteState(val isFavorited: Boolean, val isWatching: Boolean)
+@Serializable
+data class NoteState(
+    val isFavorited: Boolean,
+    val isWatching: Boolean,
+    val isMutedThread: Boolean = false
+)
