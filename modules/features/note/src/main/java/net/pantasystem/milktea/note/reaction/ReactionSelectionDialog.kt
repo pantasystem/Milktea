713d5050a8320c65d65defe7b7c88bc0138873fd
Panta <kinoshita062323@gmail.com>
2022-12-22 16:51:31 +0900

    feat: レイアウト制御部分を別クラスとして切り出し

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/reaction/ReactionSelectionDialog.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/reaction/ReactionSelectionDialog.kt
@@ -1,20 +1,28 @@
 
 package net.pantasystem.milktea.note.reaction
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
 import com.google.android.flexbox.AlignItems
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.tabs.TabLayout
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import net.pantasystem.milktea.app_store.account.AccountStore
 import net.pantasystem.milktea.model.notes.Note
@@ -43,12 +51,10 @@ class ReactionSelectionDialog : BottomSheetDialogFragment(),
         }
     }
 
-    private var mNoteViewModel: NotesViewModel? = null
-    val notesViewModel by activityViewModels<NotesViewModel>()
-
-
     @Inject
     lateinit var accountStore: AccountStore
+
+    val notesViewModel by activityViewModels<NotesViewModel>()
 
     val viewModel: ReactionSelectionDialogViewModel by viewModels()
 
@@ -59,11 +65,6 @@ class ReactionSelectionDialog : BottomSheetDialogFragment(),
         )
     }
 
-    private val flexBoxLayoutManager: FlexboxLayoutManager by lazy {
-        val flexBoxLayoutManager = FlexboxLayoutManager(requireContext())
-        flexBoxLayoutManager.alignItems = AlignItems.STRETCH
-        flexBoxLayoutManager
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -80,56 +81,93 @@ class ReactionSelectionDialog : BottomSheetDialogFragment(),
         binding.reactionSelectionViewModel = viewModel
         binding.lifecycleOwner = this
 
-
-        val searchedReactionAdapter = EmojiChoicesAdapter {
-            notesViewModel.toggleReaction(noteId, it.toTextReaction())
-            dismiss()
-        }
-
-        binding.searchSuggestionsView.adapter = searchedReactionAdapter
-        binding.searchSuggestionsView.layoutManager = flexBoxLayoutManager
-
-        mNoteViewModel = notesViewModel
-        binding.reactionChoicesTab.setupWithViewPager(binding.reactionChoicesViewPager)
-
-        val adapter = ReactionChoicesPagerAdapter(childFragmentManager, requireContext())
-        binding.reactionChoicesViewPager.adapter = adapter
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                viewModel.categories.collect { categories ->
-                    adapter.setList(categories.toList())
-                }
-            }
-
-        }
-
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                viewModel.filteredEmojis.collect { list ->
-                    searchedReactionAdapter.submitList(list)
-                }
-            }
-        }
-
-        binding.searchReactionEditText.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_SEND) {
+        val binder = ReactionSelectionDialogBinder(
+            context = requireContext(),
+            scope = lifecycleScope,
+            fragmentManager = childFragmentManager,
+            lifecycleOwner = viewLifecycleOwner,
+            searchSuggestionListView = binding.searchSuggestionsView,
+            tabLayout = binding.reactionChoicesTab,
+            viewPager = binding.reactionChoicesViewPager,
+            searchWordTextField = binding.searchReactionEditText,
+            viewModel = viewModel,
+            onReactionSelected = {
                 notesViewModel.toggleReaction(noteId, viewModel.searchWord.value)
                 dismiss()
-                return@setOnEditorActionListener true
+            },
+            onSearchEmojiTextFieldEntered = {
+                notesViewModel.toggleReaction(noteId, viewModel.searchWord.value)
+                dismiss()
             }
-            return@setOnEditorActionListener false
-        }
+        )
+        binder.bind()
 
     }
 
     override fun selectReaction(reaction: String) {
-        mNoteViewModel?.toggleReaction(noteId, reaction)
+        notesViewModel.toggleReaction(noteId, reaction)
         dismiss()
     }
 
 
 
+}
+
+class ReactionSelectionDialogBinder(
+    val context: Context,
+    val scope: CoroutineScope,
+    val fragmentManager: FragmentManager,
+    val lifecycleOwner: LifecycleOwner,
+    val searchSuggestionListView: RecyclerView,
+    val tabLayout: TabLayout,
+    val viewPager: ViewPager,
+    val searchWordTextField: EditText,
+    val viewModel: ReactionSelectionDialogViewModel,
+    val onReactionSelected: (String) -> Unit,
+    val onSearchEmojiTextFieldEntered: (String) -> Unit,
+) {
+
+    private val flexBoxLayoutManager: FlexboxLayoutManager by lazy {
+        val flexBoxLayoutManager = FlexboxLayoutManager(context)
+        flexBoxLayoutManager.alignItems = AlignItems.STRETCH
+        flexBoxLayoutManager
+    }
+
+    fun bind() {
+        val searchedReactionAdapter = EmojiChoicesAdapter {
+            onReactionSelected(it.toTextReaction())
+        }
+        searchSuggestionListView.adapter = searchedReactionAdapter
+        searchSuggestionListView.layoutManager = flexBoxLayoutManager
+
+        tabLayout.setupWithViewPager(viewPager)
+        val adapter = ReactionChoicesPagerAdapter(fragmentManager, context)
+        viewPager.adapter = adapter
+
+        scope.launch {
+            lifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.filteredEmojis.collect {
+                    searchedReactionAdapter.submitList(it)
+                }
+            }
+        }
+
+        scope.launch {
+            lifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.categories.collect { categories ->
+                    adapter.setList(categories.toList())
+                }
+            }
+        }
+
+
+        searchWordTextField.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                onSearchEmojiTextFieldEntered(viewModel.searchWord.value)
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
+    }
 }
 
