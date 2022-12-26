bb999b17f1a560992141673f54602b7de0330e3e
Panta <kinoshita062323@gmail.com>
2022-12-26 20:11:10 +0900

    feat: 特定の例外を握りつぶさないrunCatchingの代用関数を作成

---
+++ modules/common/src/main/java/net/pantasystem/milktea/common/ResultHelper.kt
@@ -0,0 +1,24 @@
+package net.pantasystem.milktea.common
+
+import kotlinx.coroutines.CancellationException
+
+inline fun <T, R> T.runCancellableCatching(block: T.() -> R): Result<R> {
+    runCatching {}
+    return try {
+        Result.success(block())
+    } catch (e: CancellationException) {
+        throw e
+    } catch (e: Throwable) {
+        Result.failure(e)
+    }
+}
+
+inline fun <R> runCancellableCatching(block: () -> R): Result<R> {
+    return try {
+        Result.success(block())
+    } catch (e: CancellationException) {
+        throw e
+    } catch (e: Throwable) {
+        Result.failure(e)
+    }
+}
