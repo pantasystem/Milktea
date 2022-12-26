c6208e0b3788d92ae93c71044293dc9243313a3b
Panta <kinoshita062323@gmail.com>
2022-12-26 23:00:33 +0900

    feat: main-safeな実装に

--- modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/drive/MediatorFilePropertyDataSource.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/drive/MediatorFilePropertyDataSource.kt
@@ -2,6 +2,7 @@ package net.pantasystem.milktea.data.infrastructure.drive
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.withContext
 import net.pantasystem.milktea.common.Logger
 import net.pantasystem.milktea.common.runCancellableCatching
 import net.pantasystem.milktea.model.AddResult
@@ -26,98 +27,107 @@ class MediatorFilePropertyDataSource @Inject constructor(
         get() = inMemoryFilePropertyDataSource.state
 
     override suspend fun add(fileProperty: FileProperty): Result<AddResult> = runCancellableCatching {
-        val result = inMemoryFilePropertyDataSource.add(fileProperty)
-        val record = runCancellableCatching {
-            driveFileRecordDao.findOne(fileProperty.id.accountId, fileProperty.id.fileId)
-        }.getOrNull()
-        try {
-            if (record == null) {
-                driveFileRecordDao.insert(DriveFileRecord.from(fileProperty))
-                return@runCancellableCatching AddResult.Created
-            } else if (record.toFileProperty() != fileProperty) {
-                driveFileRecordDao.update(DriveFileRecord.from(fileProperty).copy(id = record.id))
-                return@runCancellableCatching AddResult.Updated
+        withContext(Dispatchers.IO) {
+            val result = inMemoryFilePropertyDataSource.add(fileProperty)
+            val record = runCancellableCatching {
+                driveFileRecordDao.findOne(fileProperty.id.accountId, fileProperty.id.fileId)
+            }.getOrNull()
+            try {
+                if (record == null) {
+                    driveFileRecordDao.insert(DriveFileRecord.from(fileProperty))
+                    return@withContext AddResult.Created
+                } else if (record.toFileProperty() != fileProperty) {
+                    driveFileRecordDao.update(
+                        DriveFileRecord.from(fileProperty).copy(id = record.id)
+                    )
+                    return@withContext AddResult.Updated
+                }
+            } catch (e: Exception) {
+                return@withContext AddResult.Canceled
             }
-        } catch (e: Exception) {
-            return@runCancellableCatching AddResult.Canceled
-        }
 
-        return result
+            return@withContext result.getOrThrow()
+        }
     }
 
     override suspend fun addAll(list: List<FileProperty>): Result<List<AddResult>> = runCancellableCatching {
-        if (list.isEmpty()) {
-            return@runCancellableCatching emptyList()
+        withContext(Dispatchers.IO) {
+            if (list.isEmpty()) {
+                return@withContext emptyList()
+            }
+            val results = inMemoryFilePropertyDataSource.addAll(list)
+            val insertResults = driveFileRecordDao.insertAll(list.map {
+                DriveFileRecord.from(it)
+            })
+
+            // NOTE: 既に作成されいているためInsertがキャンセルされたファイル
+            val ignored = list.filterIndexed { index, _ ->
+                insertResults[index] == -1L
+            }
+
+            logger.debug("insert結果:$insertResults")
+            if (ignored.isEmpty()) {
+                return@withContext results.getOrThrow()
+            }
+
+            // NOTE: DB上の同一のエンティティを取得する
+            val records = findRecords(ignored.map { it.id })
+            val idAndFileMap = ignored.associateBy { it.id }
+
+            // NOTE: 内容に変更があったファイルをフィルタする
+            val needUpdates = records.filter { record ->
+                val property = idAndFileMap.getValue(record.toFilePropertyId())
+                record.equalFileProperty(property)
+            }
+
+            if (needUpdates.isEmpty()) {
+                return@withContext results.getOrThrow()
+            }
+
+            logger.debug("必要更新件数:${needUpdates.size}")
+            // NOTE: 内容に変更があった場合更新をする
+            needUpdates.map { record ->
+                driveFileRecordDao.update(record.update(idAndFileMap.getValue(record.toFilePropertyId())))
+            }
+
+            return@withContext results.getOrThrow()
         }
-        val results = inMemoryFilePropertyDataSource.addAll(list)
-        val insertResults = driveFileRecordDao.insertAll(list.map {
-            DriveFileRecord.from(it)
-        })
-
-        // NOTE: 既に作成されいているためInsertがキャンセルされたファイル
-        val ignored = list.filterIndexed { index, _ ->
-            insertResults[index] == -1L
-        }
-
-        logger.debug("insert結果:$insertResults")
-        if (ignored.isEmpty()) {
-            return results
-        }
-
-        // NOTE: DB上の同一のエンティティを取得する
-        val records = findRecords(ignored.map { it.id })
-        val idAndFileMap = ignored.associateBy { it.id }
-
-        // NOTE: 内容に変更があったファイルをフィルタする
-        val needUpdates = records.filter { record ->
-            val property = idAndFileMap.getValue(record.toFilePropertyId())
-            record.equalFileProperty(property)
-        }
-
-        if (needUpdates.isEmpty()) {
-            return results
-        }
-
-        logger.debug("必要更新件数:${needUpdates.size}")
-        // NOTE: 内容に変更があった場合更新をする
-        needUpdates.map { record ->
-            driveFileRecordDao.update(record.update(idAndFileMap.getValue(record.toFilePropertyId())))
-        }
-
-        return results
     }
 
     override suspend fun find(filePropertyId: FileProperty.Id): Result<FileProperty> = runCancellableCatching {
-        var result = inMemoryFilePropertyDataSource.find(filePropertyId).getOrNull()
-        if (result != null) {
-            return@runCancellableCatching result
-        }
-        result = runCancellableCatching {
-            driveFileRecordDao.findOne(filePropertyId.accountId, filePropertyId.fileId)
-                ?.toFileProperty()
-        }.getOrNull()
+        withContext(Dispatchers.IO) {
+            var result = inMemoryFilePropertyDataSource.find(filePropertyId).getOrNull()
+            if (result != null) {
+                return@withContext result
+            }
+            result = runCancellableCatching {
+                driveFileRecordDao.findOne(filePropertyId.accountId, filePropertyId.fileId)
+                    ?.toFileProperty()
+            }.getOrNull()
 
-        result ?: throw FilePropertyNotFoundException(filePropertyId)
+            result ?: throw FilePropertyNotFoundException(filePropertyId)
+        }
     }
 
     override suspend fun remove(fileProperty: FileProperty): Result<Boolean> = runCancellableCatching {
-        try {
-            driveFileRecordDao.delete(fileProperty.id.accountId, fileProperty.id.fileId)
-            true
-        } catch (e: Exception) {
-            false
-        } && inMemoryFilePropertyDataSource.remove(fileProperty).getOrThrow()
+        withContext(Dispatchers.IO) {
+            try {
+                driveFileRecordDao.delete(fileProperty.id.accountId, fileProperty.id.fileId)
+                true
+            } catch (e: Exception) {
+                false
+            } && inMemoryFilePropertyDataSource.remove(fileProperty).getOrThrow()
+        }
     }
 
 
     override fun observe(id: FileProperty.Id): Flow<FileProperty?> {
         return driveFileRecordDao.observe(id.accountId, id.fileId).distinctUntilChanged().map {
             it?.toFileProperty()
-        }
+        }.flowOn(Dispatchers.IO)
     }
 
     override fun observeIn(ids: List<FileProperty.Id>): Flow<List<FileProperty>> {
-
         val accountIds = ids.map { it.accountId }.distinct()
         val flows = accountIds.map { accountId ->
             driveFileRecordDao.observeIn(
@@ -147,23 +157,25 @@ class MediatorFilePropertyDataSource @Inject constructor(
     }
 
     override suspend fun findIn(ids: List<FileProperty.Id>): Result<List<FileProperty>> = runCancellableCatching {
-        val inMemories = inMemoryFilePropertyDataSource.findIn(ids).getOrThrow()
-        if (inMemories.size == ids.size) {
-            return@runCancellableCatching inMemories
-        }
-        val sets = inMemories.map { it.id }.toSet()
-        val notExistsIds = ids.filterNot {
-            sets.contains(it)
-        }
+        withContext(Dispatchers.IO) {
+            val inMemories = inMemoryFilePropertyDataSource.findIn(ids).getOrThrow()
+            if (inMemories.size == ids.size) {
+                return@withContext inMemories
+            }
+            val sets = inMemories.map { it.id }.toSet()
+            val notExistsIds = ids.filterNot {
+                sets.contains(it)
+            }
 
-        val onDb = findRecords(notExistsIds).map {
-            it.toFileProperty()
-        }
-        val map = (inMemories + onDb).associateBy {
-            it.id
-        }
-        return@runCancellableCatching ids.mapNotNull {
-            map[it]
+            val onDb = findRecords(notExistsIds).map {
+                it.toFileProperty()
+            }
+            val map = (inMemories + onDb).associateBy {
+                it.id
+            }
+            return@withContext ids.mapNotNull {
+                map[it]
+            }
         }
     }
 
