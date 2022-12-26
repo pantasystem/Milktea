37c09db4ba71e79ad8778702466ddb17cade1f2d
Panta <kinoshita062323@gmail.com>
2022-12-26 20:17:28 +0900

    feat: CancellableなmapCatchingの代用関数を作成

--- modules/common/src/main/java/net/pantasystem/milktea/common/ResultHelper.kt
+++ modules/common/src/main/java/net/pantasystem/milktea/common/ResultHelper.kt
@@ -14,11 +14,22 @@ inline fun <T, R> T.runCancellableCatching(block: T.() -> R): Result<R> {
 }
 
 inline fun <R> runCancellableCatching(block: () -> R): Result<R> {
+    runCatching(block).mapCatching {
+
+    }
     return try {
         Result.success(block())
     } catch (e: CancellationException) {
         throw e
     } catch (e: Throwable) {
         Result.failure(e)
+    }
+}
+
+public inline fun <R, T> Result<T>.mapCancellableCatching(transform: (value: T) -> R): Result<R> {
+    val successResult = getOrNull()
+    return when {
+        successResult != null -> runCancellableCatching { transform(successResult) }
+        else -> Result.failure(exceptionOrNull() ?: error("Unreachable state"))
     }
 }
