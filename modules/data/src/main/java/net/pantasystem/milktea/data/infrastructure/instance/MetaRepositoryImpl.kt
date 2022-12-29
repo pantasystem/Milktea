b635af4c5545453dbbc4b548477b62cdd213a28a
Panta <kinoshita062323@gmail.com>
2022-12-30 00:02:07 +0900

    feat: CoroutineDispatcherをDIするように

--- modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/instance/MetaRepositoryImpl.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/instance/MetaRepositoryImpl.kt
@@ -1,11 +1,12 @@
 package net.pantasystem.milktea.data.infrastructure.instance
 
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.withContext
 import net.pantasystem.milktea.common.runCancellableCatching
 import net.pantasystem.milktea.common.throwIfHasError
+import net.pantasystem.milktea.common_android.hilt.IODispatcher
 import net.pantasystem.milktea.data.api.misskey.MisskeyAPIProvider
 import net.pantasystem.milktea.model.instance.Meta
 import net.pantasystem.milktea.model.instance.MetaDataSource
@@ -17,10 +18,11 @@ class MetaRepositoryImpl @Inject constructor(
     private val metaDataSource: MetaDataSource,
     private val metaCache: MetaCache,
     private val misskeyAPIProvider: MisskeyAPIProvider,
+    @IODispatcher private val ioDispatcher: CoroutineDispatcher,
 ): MetaRepository {
 
     override suspend fun sync(instanceDomain: String): Result<Unit> = runCancellableCatching {
-        withContext(Dispatchers.IO) {
+        withContext(ioDispatcher) {
             val meta = fetch(instanceDomain)
             metaDataSource.add(meta)
             metaCache.put(meta.uri, meta)
@@ -29,7 +31,7 @@ class MetaRepositoryImpl @Inject constructor(
     }
 
     override suspend fun find(instanceDomain: String): Result<Meta> = runCancellableCatching {
-        withContext(Dispatchers.IO) {
+        withContext(ioDispatcher) {
             val cacheMeta = metaCache.get(instanceDomain)
             if (cacheMeta != null) {
                 return@withContext cacheMeta
