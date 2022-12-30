d40bf0549298c7b783e5a1de9e21a0ea5b25769f
Panta <kinoshita062323@gmail.com>
2022-12-30 11:15:49 +0900

    feat: junit5に書き換え

--- modules/model/src/test/java/net/pantasystem/milktea/model/user/AcctTest.kt
+++ modules/model/src/test/java/net/pantasystem/milktea/model/user/AcctTest.kt
@@ -1,7 +1,8 @@
 package net.pantasystem.milktea.model.user
 
-import org.junit.Assert
-import org.junit.Test
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 
 class AcctTest {
 
@@ -9,27 +10,27 @@ class AcctTest {
     fun userName_giveUserNameOnly() {
         val textAcct = "@Panta"
         val acct = Acct(textAcct)
-        Assert.assertEquals("Panta", acct.userName)
+        Assertions.assertEquals("Panta", acct.userName)
     }
 
     @Test
     fun host_giveUserNameOnlyReturnsNull() {
         val textAcct = "@Panta"
         val acct = Acct(textAcct)
-        Assert.assertNull(acct.host)
+        Assertions.assertNull(acct.host)
     }
 
     @Test
     fun userName_giveUserNameAndHost() {
         val textAcct = "@Panta@misskey.io"
         val acct = Acct(textAcct)
-        Assert.assertEquals("Panta", acct.userName)
+        Assertions.assertEquals("Panta", acct.userName)
     }
 
     @Test
     fun host_giveUserNameAndHost() {
         val textAcct = "@Panta@misskey.io"
         val acct = Acct(textAcct)
-        Assert.assertEquals("misskey.io", acct.host)
+        Assertions.assertEquals("misskey.io", acct.host)
     }
 }
