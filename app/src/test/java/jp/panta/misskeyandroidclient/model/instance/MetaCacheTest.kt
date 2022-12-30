50d9179ee8aea2abb4a2441424d2dc725bff0edf
Panta <kinoshita062323@gmail.com>
2022-12-30 11:16:53 +0900

    feat: junit5に書き換え

--- app/src/test/java/jp/panta/misskeyandroidclient/model/instance/MetaCacheTest.kt
+++ app/src/test/java/jp/panta/misskeyandroidclient/model/instance/MetaCacheTest.kt
@@ -3,16 +3,17 @@ package jp.panta.misskeyandroidclient.model.instance
 import kotlinx.coroutines.runBlocking
 import net.pantasystem.milktea.data.infrastructure.instance.MetaCache
 import net.pantasystem.milktea.model.instance.Meta
-import org.junit.Assert.assertNotNull
-import org.junit.Assert.assertNull
-import org.junit.Before
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
 
 class MetaCacheTest {
 
     lateinit var metaCache: MetaCache
 
-    @Before
+    @BeforeEach
     fun setup() {
         metaCache = MetaCache()
     }
