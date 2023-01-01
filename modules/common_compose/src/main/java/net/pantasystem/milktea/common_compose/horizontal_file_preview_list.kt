ec50c4350a9375ece04255690eef7f8ec3525e1c
Panta <kinoshita062323@gmail.com>
2023-01-01 17:16:23 +0900

    feat: 色を指定

--- modules/common_compose/src/main/java/net/pantasystem/milktea/common_compose/horizontal_file_preview_list.kt
+++ modules/common_compose/src/main/java/net/pantasystem/milktea/common_compose/horizontal_file_preview_list.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -151,7 +152,12 @@ fun LocalFilePreview(
 
             if (allowMaxFileSize != null && file.fileSize != null) {
                 if (allowMaxFileSize < (file.fileSize ?: 0)) {
-                    Icon(Icons.Default.Warning, contentDescription = null)
+                    Icon(
+                        Icons.Default.Warning,
+                        contentDescription = null,
+                        tint = Color.Yellow,
+                        modifier = Modifier.padding(2.dp)
+                    )
                 }
             }
 
