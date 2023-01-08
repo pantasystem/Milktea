44da766237345faf6e5a4c126083de61c4636ec2
Panta <kinoshita062323@gmail.com>
2023-01-08 21:37:40 +0900

    feat: 最近使ったリアクションを取得できるようにしました

--- modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/notes/reaction/impl/history/ReactionHistoryDao.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/notes/reaction/impl/history/ReactionHistoryDao.kt
@@ -17,6 +17,9 @@ interface ReactionHistoryDao{
 
     @Query("select reaction, count(reaction) as reaction_count from reaction_history where instance_domain=:instanceDomain group by reaction order by reaction_count desc limit :limit")
     fun observeSumReactions(instanceDomain: String, limit: Int) : Flow<List<ReactionHistoryCountRecord>>
+    
+    @Query("select * from reaction_history where instance_domain=:instanceDomain order by id desc limit :limit")
+    fun observeRecentlyUsed(instanceDomain: String, limit: Int): Flow<List<ReactionHistoryRecord>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(reactionHistory: ReactionHistoryRecord)
--- modules/model/src/main/java/net/pantasystem/milktea/model/notes/reaction/history/ReactionHistoryRepository.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/notes/reaction/history/ReactionHistoryRepository.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.Flow
 interface ReactionHistoryRepository {
     suspend fun create(reactionHistory: ReactionHistory): Result<Unit>
     fun observeSumReactions(instanceDomain: String, limit: Int = 50): Flow<List<ReactionHistoryCount>>
+    fun observeRecentlyUsedBy(instanceDomain: String, limit: Int = 20): Flow<List<ReactionHistory>>
     suspend fun sumReactions(instanceDomain: String, limit: Int = 50): List<ReactionHistoryCount>
     suspend fun findAll(): List<ReactionHistory>
 }
