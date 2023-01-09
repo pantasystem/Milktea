abd7ea7db616e507e09918aed94745c7666dac4b
Panta <kinoshita062323@gmail.com>
2023-01-10 00:06:51 +0900

    feat: 新しいパーサーに移行

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/view/StatusMessageHelper.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/view/StatusMessageHelper.kt
@@ -4,8 +4,8 @@ import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import dagger.hilt.android.EntryPointAccessors
-import net.pantasystem.milktea.common_android_ui.BindingProvider
 import net.pantasystem.milktea.common_android.ui.text.CustomEmojiDecorator
+import net.pantasystem.milktea.common_android_ui.BindingProvider
 import net.pantasystem.milktea.model.notes.NoteRelation
 import net.pantasystem.milktea.note.R
 import net.pantasystem.milktea.note.viewmodel.PlaneNoteViewData
@@ -15,22 +15,25 @@ object StatusMessageHelper {
 
     @JvmStatic
     @BindingAdapter("statusMessageTargetViewNote")
-    fun TextView.setStatusMessage(statusMessageTargetViewNote: PlaneNoteViewData){
-        val settingStore = EntryPointAccessors.fromApplication(context.applicationContext, BindingProvider::class.java)
+    fun TextView.setStatusMessage(statusMessageTargetViewNote: PlaneNoteViewData) {
+        val settingStore = EntryPointAccessors.fromApplication(
+            context.applicationContext,
+            BindingProvider::class.java
+        )
             .settingStore()
         val isUserNameDefault = settingStore.isUserNameDefault
         val note = statusMessageTargetViewNote.note
-        val name = if(isUserNameDefault){
+        val name = if (isUserNameDefault) {
             note.user.displayUserName
-        }else{
+        } else {
             note.user.displayName
         }
         val context = this.context
-        val message = when{
-            note.reply != null ->{
+        val message = when {
+            note.reply != null -> {
                 context.getString(R.string.replied_by, name)
             }
-            note.note.isRenote() && !note.note.hasContent() ->{
+            note.note.isRenote() && !note.note.hasContent() -> {
                 context.getString(R.string.renoted_by, name)
             }
             note is NoteRelation.Featured -> {
@@ -42,15 +45,21 @@ object StatusMessageHelper {
 
             else -> null
         }
-        if(message == null) {
+        if (message == null) {
             this.visibility = View.GONE
             return
         }
         this.visibility = View.VISIBLE
-        if(isUserNameDefault){
+        if (isUserNameDefault) {
             this.text = message
-        }else{
-            this.text = CustomEmojiDecorator().decorate(note.user.emojis, message, this)
+        } else {
+            this.text = CustomEmojiDecorator().decorate(
+                statusMessageTargetViewNote.account.getHost(),
+                note.user.host,
+                note.user.emojis,
+                message,
+                this
+            )
         }
     }
 }
