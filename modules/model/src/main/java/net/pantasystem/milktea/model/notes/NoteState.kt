c731b61ec3802a3023a4f9904169926ddac017d6
Panta <kinoshita062323@gmail.com>
2023-01-12 00:17:14 +0900

    feat: watchingの状態を3値で表現できるようにしました。

--- modules/model/src/main/java/net/pantasystem/milktea/model/notes/NoteState.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/notes/NoteState.kt
@@ -1,7 +1,16 @@
 package net.pantasystem.milktea.model.notes
 
+/**
+ * お気に入りやスレッドのミュートの状態を表す。
+ * v13でノートのウォッチ機能が削除されたため、isWatchingのフィールドも無効(None)になる。
+ */
 data class NoteState(
     val isFavorited: Boolean,
-    val isWatching: Boolean,
+    val isWatching: Watching,
     val isMutedThread: Boolean?
-)
+) {
+    sealed interface Watching {
+        object None : Watching
+        data class Some(val isWatching: Boolean) : Watching
+    }
+}
