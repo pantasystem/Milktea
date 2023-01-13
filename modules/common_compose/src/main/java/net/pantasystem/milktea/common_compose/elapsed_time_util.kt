9b52e67b02dc0f90e28291b67b2f33c0eb3da096
Panta <kinoshita062323@gmail.com>
2023-01-13 16:10:13 +0900

    feat: 変更に合わせ修正

--- modules/common_compose/src/main/java/net/pantasystem/milktea/common_compose/elapsed_time_util.kt
+++ modules/common_compose/src/main/java/net/pantasystem/milktea/common_compose/elapsed_time_util.kt
@@ -5,30 +5,25 @@ import androidx.compose.runtime.Stable
 import androidx.compose.ui.res.stringResource
 import kotlinx.datetime.Instant
 import net.pantasystem.milktea.common.ui.SimpleElapsedTime
+import net.pantasystem.milktea.common.ui.TimeUnit
 
 @Composable
 @Stable
 fun getSimpleElapsedTime(time: Instant): String {
-
-    val map = SimpleElapsedTime.TimeUnit.values().associateWith {
-        getStr(unit = it)
-    }
-    return SimpleElapsedTime {
-        map[it]!!
-    }.invoke(time)
+    return getStr(SimpleElapsedTime.invoke(time))
 }
 
 @Composable
 @Stable
-private fun getStr(unit: SimpleElapsedTime.TimeUnit): String {
+private fun getStr(unit: TimeUnit): String {
     return when(unit){
-        SimpleElapsedTime.TimeUnit.YEAR -> stringResource(R.string.year_ago)
-        SimpleElapsedTime.TimeUnit.MONTH -> stringResource(R.string.month_ago)
-        SimpleElapsedTime.TimeUnit.DATE -> stringResource(R.string.date_ago)
-        SimpleElapsedTime.TimeUnit.HOUR -> stringResource(R.string.hour_ago)
-        SimpleElapsedTime.TimeUnit.MINUTE -> stringResource(R.string.minute_ago)
-        SimpleElapsedTime.TimeUnit.SECOND -> stringResource(R.string.second_ago)
-        SimpleElapsedTime.TimeUnit.NOW -> stringResource(R.string.now)
-        SimpleElapsedTime.TimeUnit.FUTURE -> stringResource(R.string.future)
+        is TimeUnit.Day -> stringResource(R.string.time_with_date_ago, unit.value.toString())
+        TimeUnit.Future -> stringResource(R.string.future)
+        is TimeUnit.Hour -> stringResource(R.string.time_with_hour_ago, unit.value.toString())
+        is TimeUnit.Minute -> stringResource(R.string.time_with_minute_ago, unit.value.toString())
+        is TimeUnit.Month -> stringResource(R.string.time_with_month_ago, unit.value.toString())
+        TimeUnit.Now -> stringResource(R.string.now)
+        is TimeUnit.Second -> stringResource(R.string.time_with_second_ago, unit.value.toString())
+        is TimeUnit.Year -> stringResource(R.string.time_with_year_ago, unit.value.toString())
     }
 }
