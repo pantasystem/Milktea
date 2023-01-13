339a11ef9449c890e4594e04a66611a97027c15f
Panta <kinoshita062323@gmail.com>
2023-01-13 15:18:14 +0900

    feat: renote, replyの場合はアイコンを表示するようにした

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/view/StatusMessageHelper.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/view/StatusMessageHelper.kt
@@ -1,11 +1,17 @@
 package net.pantasystem.milktea.note.view
 
+import android.text.SpannableStringBuilder
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import dagger.hilt.android.EntryPointAccessors
 import net.pantasystem.milktea.common_android.ui.text.CustomEmojiDecorator
+import net.pantasystem.milktea.common_android.ui.text.DrawableEmojiSpan
+import net.pantasystem.milktea.common_android.ui.text.EmojiAdapter
 import net.pantasystem.milktea.common_android_ui.BindingProvider
+import net.pantasystem.milktea.note.R
 import net.pantasystem.milktea.note.viewmodel.NoteStatusMessageTextGenerator
 import net.pantasystem.milktea.note.viewmodel.PlaneNoteViewData
 
@@ -31,16 +37,38 @@ object StatusMessageHelper {
             return
         }
         this.visibility = View.VISIBLE
-        if (isUserNameDefault) {
-            this.text = message.getString(context)
+        val text = if (isUserNameDefault) {
+            message.getString(context)
         } else {
-            this.text = CustomEmojiDecorator().decorate(
+            CustomEmojiDecorator().decorate(
                 statusMessageTargetViewNote.account.getHost(),
                 note.user.host,
                 note.user.emojis,
                 message.getString(context),
                 this,
             )
+        }
+
+        val icon = when {
+            note.reply != null -> R.drawable.ic_reply_black_24dp
+            note.renote != null -> R.drawable.ic_re_note
+            else -> null
+        }
+
+        this.text = if (icon != null) {
+            SpannableStringBuilder(text).apply {
+                insert(0, "icon")
+                val span = DrawableEmojiSpan(EmojiAdapter(this@setStatusMessage))
+                val drawable = ContextCompat.getDrawable(context, icon)
+                drawable?.setTint(currentTextColor)
+                Glide.with(context)
+                    .load(drawable)
+                    .into(span.target)
+
+                setSpan(span, 0, "icon".length, 0)
+            }
+        } else {
+            text
         }
     }
 }
