38ab7aa02e1b4916f1ee630f9ddf263440ededbe
Panta <kinoshita062323@gmail.com>
2023-01-22 20:19:38 +0900

    feat: mastodonのためのパラメータを追加

--- modules/model/src/main/java/net/pantasystem/milktea/model/user/mute/CreateMute.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/user/mute/CreateMute.kt
@@ -3,7 +3,13 @@ package net.pantasystem.milktea.model.user.mute
 import kotlinx.datetime.Instant
 import net.pantasystem.milktea.model.user.User
 
+/**
+ * @param userId ミュートする対象のUserのId
+ * @param expiresAt ミュートの期限
+ * @param notifications Mastodonのためのパラメータで、ミュートしたユーザからの通知の有無を指定できる
+ */
 data class CreateMute(
     val userId: User.Id,
     val expiresAt: Instant? = null,
+    val notifications: Boolean? = null
 )
