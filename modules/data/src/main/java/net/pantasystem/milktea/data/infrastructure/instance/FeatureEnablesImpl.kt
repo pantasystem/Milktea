04de7347d2368645f95bb1ab420182a49a92df2a
Panta <kinoshita062323@gmail.com>
2023-01-24 01:15:44 +0900

    feat: 判別時にソフトウェアの種別も確認するようにした

--- modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/instance/FeatureEnablesImpl.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/instance/FeatureEnablesImpl.kt
@@ -10,6 +10,7 @@ import net.pantasystem.milktea.model.instance.MetaRepository
 import net.pantasystem.milktea.model.instance.Version
 import net.pantasystem.milktea.model.nodeinfo.NodeInfo
 import net.pantasystem.milktea.model.nodeinfo.NodeInfoRepository
+import net.pantasystem.milktea.model.nodeinfo.getVersion
 import java.net.URL
 import javax.inject.Inject
 
@@ -20,34 +21,29 @@ class FeatureEnablesImpl @Inject constructor(
     @IODispatcher private val ioDispatcher: CoroutineDispatcher
 ): FeatureEnables {
     override suspend fun isEnable(instanceDomain: String, type: FeatureType, default: Boolean): Boolean {
-        return withContext(ioDispatcher) {
-            val meta = metaRepository.find(instanceDomain).getOrNull()?: return@withContext default
-            val nodeInfo = nodeInfoRepository.find(URL(instanceDomain).host).getOrNull() ?: return@withContext default
-            when(type) {
-                FeatureType.Gallery -> meta.getVersion() >= Version("12.75.0")
-                FeatureType.Channel -> meta.getVersion() >= Version("12")
-                FeatureType.Group -> meta.getVersion() >= Version("11")
-                FeatureType.Antenna -> meta.getVersion() >= Version("12.75.0")
-                FeatureType.UserReactionHistory -> meta.getVersion() >= Version("12")
-                FeatureType.Drive -> nodeInfo.type is NodeInfo.SoftwareType.Misskey
-                FeatureType.Bookmark -> nodeInfo.type is NodeInfo.SoftwareType.Mastodon
-            }
+        val features = enableFeatures(instanceDomain)
+        return if (features.isEmpty()) {
+            default
+        } else {
+            features.contains(type)
         }
     }
 
     override suspend fun enableFeatures(instanceDomain: String): Set<FeatureType> {
         return withContext(ioDispatcher) {
-            val meta = metaRepository.find(instanceDomain).getOrNull()?: return@withContext emptySet()
-            val nodeInfo = nodeInfoRepository.find(URL(instanceDomain).host).getOrNull()
-
+            val meta = metaRepository.find(instanceDomain).getOrNull()
+            val nodeInfo = nodeInfoRepository.find(URL(instanceDomain).host).getOrNull() ?: return@withContext emptySet()
+            val version = meta?.getVersion() ?: nodeInfo.type.getVersion()
+            val isMisskey = meta != null || nodeInfo.type is NodeInfo.SoftwareType.Misskey
+            val isMastodon = nodeInfo.type is NodeInfo.SoftwareType.Mastodon
             setOfNotNull(
-                if (meta.getVersion() >= Version("12.75.0")) FeatureType.Gallery else null,
-                if (meta.getVersion() >= Version("12")) FeatureType.Channel else null,
-                if (meta.getVersion() >= Version("11")) FeatureType.Group else null,
-                if (meta.getVersion() >= Version("12.75.0")) FeatureType.Antenna else null,
-                if (meta.getVersion() >= Version("12")) FeatureType.UserReactionHistory else null,
-                if (nodeInfo?.type is NodeInfo.SoftwareType.Misskey) FeatureType.Drive else null,
-                if (nodeInfo?.type is NodeInfo.SoftwareType.Mastodon) FeatureType.Bookmark else null,
+                if (isMisskey && version >= Version("12.75.0")) FeatureType.Gallery else null,
+                if (isMisskey && version >= Version("12")) FeatureType.Channel else null,
+                if (isMisskey && version >= Version("11")) FeatureType.Group else null,
+                if (isMisskey && version >= Version("12.75.0")) FeatureType.Antenna else null,
+                if (isMisskey && version >= Version("12")) FeatureType.UserReactionHistory else null,
+                if (isMisskey) FeatureType.Drive else null,
+                if (isMastodon) FeatureType.Bookmark else null,
             )
         }
     }
