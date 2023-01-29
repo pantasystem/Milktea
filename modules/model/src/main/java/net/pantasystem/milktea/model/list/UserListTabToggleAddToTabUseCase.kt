984a2fb6228df5cc66ec1a72e4a15390b8736ce3
Panta <kinoshita062323@gmail.com>
2023-01-29 18:06:26 +0900

    feat: ユーザリストをタブに追加するためのUseCaseを追加

---
+++ modules/model/src/main/java/net/pantasystem/milktea/model/list/UserListTabToggleAddToTabUseCase.kt
@@ -0,0 +1,50 @@
+package net.pantasystem.milktea.model.list
+
+import net.pantasystem.milktea.common.runCancellableCatching
+import net.pantasystem.milktea.model.UseCase
+import net.pantasystem.milktea.model.account.Account
+import net.pantasystem.milktea.model.account.AccountRepository
+import net.pantasystem.milktea.model.account.page.Page
+import net.pantasystem.milktea.model.account.page.Pageable
+import net.pantasystem.milktea.model.antenna.AccountService
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserListTabToggleAddToTabUseCase @Inject constructor(
+    private val userListRepository: UserListRepository,
+    private val accountRepository: AccountRepository,
+    private val accountService: AccountService,
+): UseCase {
+
+    suspend operator fun invoke(listId: UserList.Id) = runCancellableCatching<Unit> {
+        val account = accountRepository.get(listId.accountId)
+            .getOrThrow()
+        val page = account.pages.firstOrNull {
+            it.pageParams.listId == listId.userListId
+        }
+
+        if (page == null) {
+            val userList = userListRepository.findOne(listId)
+            accountService.add(
+                Page(
+                    account.accountId,
+                    userList.name,
+                    weight = -1,
+                    pageable = when(account.instanceType) {
+                        Account.InstanceType.MISSKEY -> Pageable.UserListTimeline(
+                            listId.userListId
+                        )
+                        Account.InstanceType.MASTODON -> Pageable.Mastodon.ListTimeline(
+                            listId.userListId
+                        )
+                    }
+                )
+            )
+
+        } else {
+            accountService.remove(page)
+        }
+
+    }
+}
