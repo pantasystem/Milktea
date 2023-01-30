bd144352910beff8ff893995012d56dba391a160
Panta <kinoshita062323@gmail.com>
2023-01-30 14:26:45 +0900

    fix: データ構造に誤りがあったので修正

--- modules/api/src/main/java/net/pantasystem/milktea/api/mastodon/status/TootStatusDTO.kt
+++ modules/api/src/main/java/net/pantasystem/milktea/api/mastodon/status/TootStatusDTO.kt
@@ -4,6 +4,7 @@ import kotlinx.datetime.Instant
 import kotlinx.serialization.SerialName
 import net.pantasystem.milktea.api.mastodon.accounts.MastodonAccountDTO
 import net.pantasystem.milktea.api.mastodon.emojis.TootEmojiDTO
+import net.pantasystem.milktea.api.mastodon.filter.FilterResultDTO
 import net.pantasystem.milktea.api.mastodon.media.TootMediaAttachment
 import net.pantasystem.milktea.api.mastodon.poll.TootPollDTO
 import net.pantasystem.milktea.model.emoji.Emoji
@@ -40,7 +41,7 @@ data class TootStatusDTO(
     val muted: Boolean? = null,
     val bookmarked: Boolean? = null,
     val pinned: Boolean? = null,
-    val filtered: Boolean? = null,
+    val filtered: List<FilterResultDTO>? = null,
     @SerialName("emoji_reactions") val emojiReactions: List<EmojiReactionCount>? = null,
     @SerialName("quote") val quote: TootStatusDTO? = null,
     @SerialName("circle_id") val circleId: String? = null,
