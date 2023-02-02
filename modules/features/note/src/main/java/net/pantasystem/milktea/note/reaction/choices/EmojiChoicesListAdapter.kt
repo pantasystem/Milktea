d22581aa299910bc02cdbcc2bbb2c1f32a81980e
Panta <kinoshita062323@gmail.com>
2023-02-03 03:46:11 +0900

    feat: GridLayoutManagerを利用するようにした

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/reaction/choices/EmojiChoicesListAdapter.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/reaction/choices/EmojiChoicesListAdapter.kt
@@ -2,14 +2,16 @@ package net.pantasystem.milktea.note.reaction.choices
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.flexbox.AlignItems
-import com.google.android.flexbox.FlexboxLayoutManager
+import net.pantasystem.milktea.common_android.resource.convertDp2Px
 import net.pantasystem.milktea.note.EmojiType
 import net.pantasystem.milktea.note.SegmentType
 import net.pantasystem.milktea.note.databinding.ItemCategoryWithListBinding
+
 
 class EmojiChoicesListAdapter(
     val onEmojiSelected: (EmojiType) -> Unit,
@@ -41,17 +43,38 @@ class SegmentViewHolder(
     val binding: ItemCategoryWithListBinding,
     onEmojiSelected: (EmojiType) -> Unit,
 ) : RecyclerView.ViewHolder(binding.root) {
+
+    var isSatLayoutManager = false
+
     val adapter = EmojiChoicesAdapter(onEmojiSelected)
     fun onBind(segmentType: SegmentType) {
         val label = segmentType.label.getString(binding.root.context)
         binding.categoryName.text = label
 
-        val flexBoxLayoutManager = FlexboxLayoutManager(binding.root.context)
-        flexBoxLayoutManager.alignItems = AlignItems.STRETCH
-        binding.emojisView.layoutManager = flexBoxLayoutManager
+        if (!isSatLayoutManager) {
+            val listener = object : ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    val layoutManager = GridLayoutManager(binding.root.context, calculateSpanCount())
+
+                    binding.emojisView.layoutManager = layoutManager
+                    binding.emojisView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    isSatLayoutManager = true
+                }
+            }
+            binding.emojisView.viewTreeObserver.addOnGlobalLayoutListener(listener)
+        }
+
+
         adapter.submitList(segmentType.emojis)
         binding.emojisView.isNestedScrollingEnabled = false
 
         binding.emojisView.adapter = adapter
     }
+
+    private fun calculateSpanCount(): Int {
+        val viewWidth = binding.emojisView.measuredWidth
+        val itemWidth =  binding.root.context.convertDp2Px(54f).toInt()
+        return viewWidth / itemWidth
+    }
+
 }
