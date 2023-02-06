00dacb9f3ad7130cf6e0e702ddf94af96cd25c8e
Panta <kinoshita062323@gmail.com>
2023-02-06 22:19:24 +0900

    fix: 絵文字が入れ替わる問題を修正

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/reaction/choices/EmojiChoicesListAdapter.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/reaction/choices/EmojiChoicesListAdapter.kt
@@ -18,11 +18,24 @@ class EmojiChoicesListAdapter(
 ) : ListAdapter<SegmentType, SegmentViewHolder>(
     object : DiffUtil.ItemCallback<SegmentType>() {
         override fun areContentsTheSame(oldItem: SegmentType, newItem: SegmentType): Boolean {
-            return oldItem == newItem
+            return when (oldItem) {
+                is SegmentType.Category -> oldItem.name == (newItem as? SegmentType.Category)?.name
+                        && oldItem.emojis == newItem.emojis
+                is SegmentType.OftenUse -> oldItem.emojis == newItem.emojis
+                is SegmentType.OtherCategory -> oldItem.emojis == newItem.emojis
+                is SegmentType.RecentlyUsed -> oldItem.emojis == newItem.emojis
+                is SegmentType.UserCustom -> oldItem.emojis == newItem.emojis
+            }
         }
 
         override fun areItemsTheSame(oldItem: SegmentType, newItem: SegmentType): Boolean {
-            return oldItem.label == newItem.label && oldItem.javaClass == newItem.javaClass
+            return when (oldItem) {
+                is SegmentType.Category -> oldItem.name == (newItem as? SegmentType.Category)?.name
+                is SegmentType.OftenUse -> oldItem == newItem
+                is SegmentType.OtherCategory -> oldItem == newItem
+                is SegmentType.RecentlyUsed -> oldItem == newItem
+                is SegmentType.UserCustom -> oldItem == newItem
+            }
         }
     }
 ) {
@@ -41,20 +54,21 @@ class EmojiChoicesListAdapter(
 
 class SegmentViewHolder(
     val binding: ItemCategoryWithListBinding,
-    onEmojiSelected: (EmojiType) -> Unit,
+    private val onEmojiSelected: (EmojiType) -> Unit,
 ) : RecyclerView.ViewHolder(binding.root) {
 
     var isSatLayoutManager = false
 
-    val adapter = EmojiChoicesAdapter(onEmojiSelected)
     fun onBind(segmentType: SegmentType) {
+        val adapter = EmojiChoicesAdapter(onEmojiSelected)
         val label = segmentType.label.getString(binding.root.context)
         binding.categoryName.text = label
 
         if (!isSatLayoutManager) {
             val listener = object : ViewTreeObserver.OnGlobalLayoutListener {
                 override fun onGlobalLayout() {
-                    val layoutManager = GridLayoutManager(binding.root.context, calculateSpanCount())
+                    val layoutManager =
+                        GridLayoutManager(binding.root.context, calculateSpanCount())
 
                     binding.emojisView.layoutManager = layoutManager
                     binding.emojisView.viewTreeObserver.removeOnGlobalLayoutListener(this)
@@ -73,7 +87,7 @@ class SegmentViewHolder(
 
     private fun calculateSpanCount(): Int {
         val viewWidth = binding.emojisView.measuredWidth
-        val itemWidth =  binding.root.context.convertDp2Px(54f).toInt()
+        val itemWidth = binding.root.context.convertDp2Px(54f).toInt()
         return viewWidth / itemWidth
     }
 
