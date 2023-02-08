bd195cd79c5cfce4c22b359e3116152e456d2232
Panta <kinoshita062323@gmail.com>
2023-02-09 00:49:29 +0900

    feat: 画面遷移関連の実装とモジュールを追加

---
+++ modules/features/clip/src/main/java/net/pantasystem/milktea/clip/NavigationModule.kt
@@ -0,0 +1,16 @@
+package net.pantasystem.milktea.clip
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import net.pantasystem.milktea.common_navigation.ClipListNavigation
+
+@Module
+@InstallIn(ActivityComponent::class)
+abstract class NavigationModule {
+
+    @Binds
+    abstract fun bindClipListNavigation(impl: ClipListNavigationImpl): ClipListNavigation
+
+}
