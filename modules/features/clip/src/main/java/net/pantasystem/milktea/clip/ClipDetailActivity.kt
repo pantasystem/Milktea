00f2bb6989b81ca43546035987608e00edb8391d
Panta <kinoshita062323@gmail.com>
2023-02-08 23:53:16 +0900

    feat: 必要になりそうなActivityなどを作成

---
+++ modules/features/clip/src/main/java/net/pantasystem/milktea/clip/ClipDetailActivity.kt
@@ -0,0 +1,10 @@
+package net.pantasystem.milktea.clip
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ClipDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ modules/features/clip/src/main/java/net/pantasystem/milktea/clip/ClipListActivity.kt
@@ -0,0 +1,10 @@
+package net.pantasystem.milktea.clip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClipListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
