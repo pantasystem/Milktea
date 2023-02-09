f1b697928f266d7650f6f23e3275b9a45df5cfb1
Panta <kinoshita062323@gmail.com>
2023-02-09 17:38:12 +0900

    feat: 16件よりリアクション種別がある時は折り畳むようにした。また展開できるようにもした。

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/viewmodel/PlaneNoteViewData.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/viewmodel/PlaneNoteViewData.kt
@@ -23,6 +23,7 @@ import net.pantasystem.milktea.model.url.UrlPreview
 import net.pantasystem.milktea.model.url.UrlPreviewLoadTask
 import net.pantasystem.milktea.model.user.User
 import net.pantasystem.milktea.note.media.viewmodel.MediaViewData
+import kotlin.math.min
 
 open class PlaneNoteViewData(
     val note: NoteRelation,
@@ -139,8 +140,17 @@ open class PlaneNoteViewData(
         it.canRenote(User.Id(accountId = account.accountId, id = account.remoteId))
     }
 
-    val reactionCounts: LiveData<List<ReactionCount>> = Transformations.map(currentNote) {
-        it?.reactionCounts ?: emptyList()
+    val reactionCountsExpanded = MutableLiveData(toShowNote.note.reactionCounts.size <= 16)
+
+    val reactionCounts: LiveData<List<ReactionCount>> = currentNote.switchMap { note ->
+        reactionCountsExpanded.map {
+            if (it == true) {
+                note.reactionCounts
+            } else {
+                note.reactionCounts.subList(0, min(note.reactionCounts.size, 16))
+            }
+
+        }
     }
 
     val reactionCount = Transformations.map(reactionCounts) {
@@ -244,6 +254,10 @@ open class PlaneNoteViewData(
     fun expand() {
         Log.d("PlaneNoteViewData", "expand")
         expanded.value = true
+    }
+
+    fun expandReactions() {
+        reactionCountsExpanded.value = true
     }
 
 }
