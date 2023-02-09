5056b9ee3c43c5ce3aa874e2302346e5625b7ea9
Panta <kinoshita062323@gmail.com>
2023-02-09 14:17:27 +0900

    feat: 必要になりそうなViewHolderを作成実装

--- modules/features/notification/src/main/java/net/pantasystem/milktea/notification/NotificationListAdapter.kt
+++ modules/features/notification/src/main/java/net/pantasystem/milktea/notification/NotificationListAdapter.kt
@@ -3,6 +3,7 @@ package net.pantasystem.milktea.notification
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
@@ -11,11 +12,14 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.flexbox.*
 import net.pantasystem.milktea.model.notes.reaction.ReactionCount
+import net.pantasystem.milktea.note.databinding.ItemTimelineEmptyBinding
 import net.pantasystem.milktea.note.reaction.ReactionCountAdapter
 import net.pantasystem.milktea.note.view.NoteCardAction
 import net.pantasystem.milktea.note.view.NoteCardActionListenerAdapter
 import net.pantasystem.milktea.note.viewmodel.PlaneNoteViewData
 import net.pantasystem.milktea.notification.databinding.ItemNotificationBinding
+import net.pantasystem.milktea.notification.databinding.ItemNotificationErrorBinding
+import net.pantasystem.milktea.notification.viewmodel.NotificationListItem
 import net.pantasystem.milktea.notification.viewmodel.NotificationViewData
 import net.pantasystem.milktea.notification.viewmodel.NotificationViewModel
 
@@ -25,10 +29,11 @@ class NotificationListAdapter constructor(
     val notificationViewModel: NotificationViewModel,
     private val lifecycleOwner: LifecycleOwner,
     onNoteCardAction: (NoteCardAction) -> Unit
-    ) : ListAdapter<NotificationViewData, NotificationListAdapter.NotificationHolder>(diffUtilCallBack){
-    class NotificationHolder(val binding: ItemNotificationBinding) : RecyclerView.ViewHolder(binding.root)
+) : ListAdapter<NotificationViewData, NotificationListAdapter.NotificationHolder>(diffUtilCallBack) {
+    class NotificationHolder(val binding: ItemNotificationBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
-    val noteCardActionListenerAdapter = NoteCardActionListenerAdapter(onNoteCardAction)
+    private val noteCardActionListenerAdapter = NoteCardActionListenerAdapter(onNoteCardAction)
 
     override fun onBindViewHolder(holder: NotificationHolder, position: Int) {
         holder.binding.notification = getItem(position)
@@ -36,7 +41,7 @@ class NotificationListAdapter constructor(
         holder.binding.noteCardActionListener = noteCardActionListenerAdapter
 
         val note = getItem(position).noteViewData
-        note?: return
+        note ?: return
 
         setReactionCounter(note, holder.binding.simpleNote.reactionView)
         holder.binding.lifecycleOwner = lifecycleOwner
@@ -46,13 +51,18 @@ class NotificationListAdapter constructor(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationHolder {
-        val binding = DataBindingUtil.inflate<ItemNotificationBinding>(LayoutInflater.from(parent.context), R.layout.item_notification, parent, false)
+        val binding = DataBindingUtil.inflate<ItemNotificationBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.item_notification,
+            parent,
+            false
+        )
         return NotificationHolder(binding)
     }
 
-    private fun setReactionCounter(note: PlaneNoteViewData, reactionView: RecyclerView){
+    private fun setReactionCounter(note: PlaneNoteViewData, reactionView: RecyclerView) {
 
-        val reactionList = note.reactionCounts.value?.toList()?: emptyList()
+        val reactionList = note.reactionCounts.value?.toList() ?: emptyList()
         val adapter = ReactionCountAdapter(lifecycleOwner) {
             noteCardActionListenerAdapter.onReactionCountAction(it)
         }
@@ -67,7 +77,7 @@ class NotificationListAdapter constructor(
         note.reactionCounts.observe(lifecycleOwner, observer)
 
         val exLayoutManager = reactionView.layoutManager
-        if(exLayoutManager !is FlexboxLayoutManager){
+        if (exLayoutManager !is FlexboxLayoutManager) {
             val flexBoxLayoutManager = FlexboxLayoutManager(reactionView.context)
             flexBoxLayoutManager.flexDirection = FlexDirection.ROW
             flexBoxLayoutManager.flexWrap = FlexWrap.WRAP
@@ -76,9 +86,93 @@ class NotificationListAdapter constructor(
             reactionView.layoutManager = flexBoxLayoutManager
         }
 
-        if(reactionList.isNotEmpty()){
+        if (reactionList.isNotEmpty()) {
             reactionView.visibility = View.VISIBLE
         }
 
+    }
+}
+
+sealed class NotificationBaseViewHolder<T : NotificationListItem>(view: View) :
+    RecyclerView.ViewHolder(view) {
+    abstract fun onBind(item: T)
+}
+
+class NotificationViewHolder(
+    val binding: ItemNotificationBinding,
+    private val lifecycleOwner: LifecycleOwner,
+    private val notificationViewModel: NotificationViewModel,
+    private val noteCardActionListenerAdapter: NoteCardActionListenerAdapter,
+) : NotificationBaseViewHolder<NotificationListItem.Notification>(binding.root) {
+    override fun onBind(item: NotificationListItem.Notification) {
+        binding.notification = item.notificationViewData
+        binding.notificationViewModel = notificationViewModel
+        binding.noteCardActionListener = noteCardActionListenerAdapter
+
+        val note = item.notificationViewData.noteViewData
+        note ?: return
+
+        setReactionCounter(note, binding.simpleNote.reactionView)
+        binding.lifecycleOwner = lifecycleOwner
+        binding.executePendingBindings()
+    }
+
+    private fun setReactionCounter(note: PlaneNoteViewData, reactionView: RecyclerView) {
+
+        val reactionList = note.reactionCounts.value?.toList() ?: emptyList()
+        val adapter = ReactionCountAdapter(lifecycleOwner) {
+            noteCardActionListenerAdapter.onReactionCountAction(it)
+        }
+        adapter.note = note
+        reactionView.adapter = adapter
+
+        adapter.submitList(reactionList)
+
+        val observer = Observer<List<ReactionCount>> {
+            adapter.submitList(it.toList())
+        }
+        note.reactionCounts.observe(lifecycleOwner, observer)
+
+        val exLayoutManager = reactionView.layoutManager
+        if (exLayoutManager !is FlexboxLayoutManager) {
+            val flexBoxLayoutManager = FlexboxLayoutManager(reactionView.context)
+            flexBoxLayoutManager.flexDirection = FlexDirection.ROW
+            flexBoxLayoutManager.flexWrap = FlexWrap.WRAP
+            flexBoxLayoutManager.justifyContent = JustifyContent.FLEX_START
+            flexBoxLayoutManager.alignItems = AlignItems.STRETCH
+            reactionView.layoutManager = flexBoxLayoutManager
+        }
+
+        if (reactionList.isNotEmpty()) {
+            reactionView.visibility = View.VISIBLE
+        }
+
+    }
+}
+
+class NotificationLoading(view: View) :
+    NotificationBaseViewHolder<NotificationListItem.Loading>(view) {
+    override fun onBind(item: NotificationListItem.Loading) = Unit
+}
+
+class NotificationErrorViewHolder(val binding: ItemNotificationErrorBinding) :
+    NotificationBaseViewHolder<NotificationListItem.Error>(binding.root) {
+    override fun onBind(item: NotificationListItem.Error) {
+        binding.errorItem = item
+        binding.errorView.isVisible = false
+        binding.showErrorMessageButton.isVisible = true
+        binding.errorView.text = item.throwable.toString()
+        binding.showErrorMessageButton.setOnClickListener {
+            binding.errorView.isVisible = true
+        }
+    }
+}
+
+class NotificationEmptyViewHolder(val binding: ItemTimelineEmptyBinding, val onRetryButtonClicked: () -> Unit) :
+    NotificationBaseViewHolder<NotificationListItem.Empty>(binding.root) {
+    override fun onBind(item: NotificationListItem.Empty) {
+        binding.retryLoadButton.setOnClickListener {
+            onRetryButtonClicked()
+        }
     }
 }
