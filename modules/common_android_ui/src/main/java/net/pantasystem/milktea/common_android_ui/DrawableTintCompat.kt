f68501a6b30947670404a80e57a9d9338c2c63d1
Panta <kinoshita062323@gmail.com>
2023-02-10 02:53:24 +0900

    fix: crush bugs

--- modules/common_android_ui/src/main/java/net/pantasystem/milktea/common_android_ui/DrawableTintCompat.kt
+++ modules/common_android_ui/src/main/java/net/pantasystem/milktea/common_android_ui/DrawableTintCompat.kt
@@ -2,6 +2,7 @@ package net.pantasystem.milktea.common_android_ui
 
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.widget.TextView
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.databinding.BindingAdapter
@@ -16,11 +17,13 @@ object DrawableTintCompat {
 
         val drawables = this.compoundDrawables
         drawables.forEachIndexed { i, d ->
-            val drawable: Drawable? = DrawableCompat.wrap(d)
-            if(drawable != null){
-                drawable.setTint(color)
-                DrawableCompat.setTintMode(drawable, PorterDuff.Mode.SRC_IN)
-                drawables[i] = drawable
+            if (d != null) {
+                val drawable: Drawable = DrawableCompat.wrap(d)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    drawable.setTint(color)
+                    DrawableCompat.setTintMode(drawable, PorterDuff.Mode.SRC_IN)
+                    drawables[i] = drawable
+                }
             }
 
         }
