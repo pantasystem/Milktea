ed0c821fbfef5c922daf375b87dca3f02446c5c8
Panta <kinoshita062323@gmail.com>
2023-02-12 03:07:36 +0900

    feat: 一件ずつ追加できるようにした

--- modules/model/src/main/java/net/pantasystem/milktea/model/emoji/UserEmojiConfigRepository.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/emoji/UserEmojiConfigRepository.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface UserEmojiConfigRepository {
     suspend fun saveAll(configs: List<UserEmojiConfig>): Result<Unit>
+    suspend fun save(config: UserEmojiConfig): Result<Unit>
     suspend fun findByInstanceDomain(instanceDomain: String): List<UserEmojiConfig>
     suspend fun deleteAll(settings: List<UserEmojiConfig>): Result<Unit>
     suspend fun delete(setting: UserEmojiConfig): Result<Unit>
