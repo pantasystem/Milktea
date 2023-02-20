ce7c8eaa1d6ea02f9acd09558b4e7dcf593251ed
Panta <kinoshita062323@gmail.com>
2023-02-20 09:43:26 +0900

    ViewData側の処理を実装

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/viewmodel/PlaneNoteViewData.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/viewmodel/PlaneNoteViewData.kt
@@ -208,6 +208,10 @@ open class PlaneNoteViewData(
         it.isEnableInstanceTicker
     }.distinctUntilChanged().asLiveData(coroutineScope.coroutineContext)
 
+    val isVisibleSubNoteMediaPreview = subContentFolding.map { folding ->
+        !(folding || subNoteFiles.isEmpty() || subNoteMedia.isOver4Files)
+    }
+
     fun changeContentFolding() {
         val isFolding = contentFolding.value ?: return
         contentFolding.value = !isFolding
