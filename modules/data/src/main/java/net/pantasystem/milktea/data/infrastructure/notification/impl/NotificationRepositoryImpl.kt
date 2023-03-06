84f0bd0ebb3a2bd4b15f9c35dc53a0ed9525f90e
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-07 07:48:59 +0900

    feat: 通知を既読にできるようにした

--- modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/notification/impl/NotificationRepositoryImpl.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/notification/impl/NotificationRepositoryImpl.kt
@@ -1,12 +1,20 @@
 package net.pantasystem.milktea.data.infrastructure.notification.impl
 
 import kotlinx.coroutines.flow.Flow
+import net.pantasystem.milktea.api.misskey.I
 import net.pantasystem.milktea.api_streaming.Send
 import net.pantasystem.milktea.api_streaming.toJson
 import net.pantasystem.milktea.common.runCancellableCatching
+import net.pantasystem.milktea.common.throwIfHasError
+import net.pantasystem.milktea.data.api.mastodon.MastodonAPIProvider
+import net.pantasystem.milktea.data.api.misskey.MisskeyAPIProvider
 import net.pantasystem.milktea.data.infrastructure.notification.db.UnreadNotificationDAO
 import net.pantasystem.milktea.data.streaming.SocketWithAccountProvider
+import net.pantasystem.milktea.model.account.Account
 import net.pantasystem.milktea.model.account.AccountRepository
+import net.pantasystem.milktea.model.markers.MarkerRepository
+import net.pantasystem.milktea.model.markers.Markers
+import net.pantasystem.milktea.model.markers.SaveMarkerParams
 import net.pantasystem.milktea.model.notification.Notification
 import net.pantasystem.milktea.model.notification.NotificationDataSource
 import net.pantasystem.milktea.model.notification.NotificationRepository
@@ -16,11 +24,34 @@ class NotificationRepositoryImpl @Inject constructor(
     private val notificationDataSource: NotificationDataSource,
     private val socketProvider: SocketWithAccountProvider,
     private val accountRepository: AccountRepository,
-    private val unreadNotificationDAO: UnreadNotificationDAO
+    private val unreadNotificationDAO: UnreadNotificationDAO,
+    private val misskeyAPIProvider: MisskeyAPIProvider,
+    private val markerRepository: MarkerRepository,
 ) : NotificationRepository {
 
 
-
+    override suspend fun markAsRead(accountId: Long): Result<Unit> = runCancellableCatching {
+        val account = accountRepository.get(accountId).getOrThrow()
+        when(account.instanceType) {
+            Account.InstanceType.MISSKEY -> {
+                misskeyAPIProvider.get(account).markAllAsReadNotifications(
+                    I(
+                        account.token
+                    )
+                ).throwIfHasError()
+            }
+            Account.InstanceType.MASTODON -> {
+                val latest = unreadNotificationDAO.getLatestUnreadId(accountId)
+                    ?: return@runCancellableCatching
+                markerRepository.save(
+                    accountId,
+                    SaveMarkerParams(
+                        notifications = latest.notificationId
+                    )
+                ).getOrThrow()
+            }
+        }
+    }
 
     override suspend fun read(notificationId: Notification.Id) {
         runCancellableCatching {
