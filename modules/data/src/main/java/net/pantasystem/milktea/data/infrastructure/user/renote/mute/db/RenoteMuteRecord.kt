41ef3f2a674f9e296ab261d3f80fa64fb2c18d6f
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-12 16:12:35 +0900

    feat: Modelに相互変換できるようにした

--- modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/user/renote/mute/db/RenoteMuteRecord.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/user/renote/mute/db/RenoteMuteRecord.kt
@@ -3,6 +3,8 @@ package net.pantasystem.milktea.data.infrastructure.user.renote.mute.db
 import androidx.room.Entity
 import androidx.room.Index
 import kotlinx.datetime.Instant
+import net.pantasystem.milktea.model.user.User
+import net.pantasystem.milktea.model.user.renote.mute.RenoteMute
 
 @Entity(
     tableName = "renote_mute_users",
@@ -18,5 +20,25 @@ data class RenoteMuteRecord(
     val accountId: Long,
     val userId: String,
     val createdAt: Instant,
-    val postedAt: Instant?
-)
+    val postedAt: Instant?,
+) {
+
+    fun toModel(): RenoteMute {
+        return RenoteMute(
+            User.Id(accountId, userId),
+            createdAt = createdAt,
+            postedAt = postedAt,
+        )
+    }
+
+    companion object {
+        fun from(model: RenoteMute): RenoteMuteRecord {
+            return RenoteMuteRecord(
+                accountId = model.userId.accountId,
+                userId = model.userId.id,
+                createdAt = model.createdAt,
+                postedAt = model.postedAt,
+            )
+        }
+    }
+}
