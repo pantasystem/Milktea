66b7bf457c952fb80fbb126c2f375f27bf1cae31
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-12 16:12:46 +0900

    fix

--- modules/model/src/main/java/net/pantasystem/milktea/model/user/renote/mute/RenoteMuteRepository.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/user/renote/mute/RenoteMuteRepository.kt
@@ -1,5 +1,6 @@
 package net.pantasystem.milktea.model.user.renote.mute
 
+import kotlinx.coroutines.flow.Flow
 import net.pantasystem.milktea.model.user.User
 
 interface RenoteMuteRepository {
@@ -18,12 +19,14 @@ interface RenoteMuteRepository {
 
     suspend fun findBy(accountId: Long): Result<List<RenoteMute>>
 
+    suspend fun findOne(userId: User.Id): Result<RenoteMute>
+
     suspend fun delete(userId: User.Id): Result<Unit>
 
     suspend fun create(userId: User.Id): Result<RenoteMute>
 
     suspend fun exists(userId: User.Id): Result<Boolean>
 
-    fun observeBy(accountId: Long): Result<List<RenoteMute>>
+    fun observeBy(accountId: Long): Flow<List<RenoteMute>>
 
 }
