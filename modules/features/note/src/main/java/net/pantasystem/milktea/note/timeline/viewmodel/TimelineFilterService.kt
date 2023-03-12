796350b96256515026038fd9f07031cea4dacec3
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-12 18:57:10 +0900

    feat: リノートのフィルタリング処理をタイムラインのフィルタリング処理に反映

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/timeline/viewmodel/TimelineFilterService.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/timeline/viewmodel/TimelineFilterService.kt
@@ -2,19 +2,22 @@ package net.pantasystem.milktea.note.timeline.viewmodel
 
 import net.pantasystem.milktea.model.account.page.Pageable
 import net.pantasystem.milktea.model.filter.WordFilterService
+import net.pantasystem.milktea.model.user.renote.mute.RenoteMuteRepository
 import net.pantasystem.milktea.note.viewmodel.PlaneNoteViewData
 import javax.inject.Inject
 
 class TimelineFilterService(
     private val pageable: Pageable,
     private val wordFilterService: WordFilterService,
+    private val renoteMuteRepository: RenoteMuteRepository,
 ) {
 
     class Factory @Inject constructor(
-        private val wordFilterService: WordFilterService
+        private val wordFilterService: WordFilterService,
+        private val renoteMuteRepository: RenoteMuteRepository,
     ){
         fun create(pageable: Pageable): TimelineFilterService {
-            return TimelineFilterService(pageable, wordFilterService)
+            return TimelineFilterService(pageable, wordFilterService, renoteMuteRepository)
         }
     }
 
@@ -25,7 +28,15 @@ class TimelineFilterService(
                     if (wordFilterService.isShouldFilterNote(pageable, note.note)) {
                         PlaneNoteViewData.FilterResult.ShouldFilterNote
                     } else {
-                        PlaneNoteViewData.FilterResult.Pass
+                        if (note.note.note.isRenoteOnly()
+                            && renoteMuteRepository.exists(
+                                note.note.note.userId
+                            ).getOrElse { false }
+                        ) {
+                            PlaneNoteViewData.FilterResult.ShouldFilterNote
+                        } else {
+                            PlaneNoteViewData.FilterResult.Pass
+                        }
                     }
                 }
                 PlaneNoteViewData.FilterResult.ShouldFilterNote -> result
