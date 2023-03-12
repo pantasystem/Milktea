3f2455ff4b20e9aa4d9f713eb00255dddb85dfc0
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-12 16:13:20 +0900

    feat: RenoteMuteRepositoryを実装

---
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/user/renote/mute/RenoteMuteRepositoryImpl.kt
@@ -0,0 +1,195 @@
+package net.pantasystem.milktea.data.infrastructure.user.renote.mute
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
+import kotlinx.datetime.Clock
+import net.pantasystem.milktea.api.misskey.users.renote.mute.CreateRenoteMuteRequest
+import net.pantasystem.milktea.api.misskey.users.renote.mute.DeleteRenoteMuteRequest
+import net.pantasystem.milktea.api.misskey.users.renote.mute.RenoteMuteDTO
+import net.pantasystem.milktea.api.misskey.users.renote.mute.RenoteMutesRequest
+import net.pantasystem.milktea.common.APIError
+import net.pantasystem.milktea.common.runCancellableCatching
+import net.pantasystem.milktea.common.throwIfHasError
+import net.pantasystem.milktea.common_android.hilt.IODispatcher
+import net.pantasystem.milktea.data.api.misskey.MisskeyAPIProvider
+import net.pantasystem.milktea.data.infrastructure.user.renote.mute.db.RenoteMuteDao
+import net.pantasystem.milktea.data.infrastructure.user.renote.mute.db.RenoteMuteRecord
+import net.pantasystem.milktea.model.account.AccountRepository
+import net.pantasystem.milktea.model.user.User
+import net.pantasystem.milktea.model.user.renote.mute.RenoteMute
+import net.pantasystem.milktea.model.user.renote.mute.RenoteMuteRepository
+import javax.inject.Inject
+
+class RenoteMuteRepositoryImpl @Inject constructor(
+    private val accountRepository: AccountRepository,
+    private val misskeyAPIProvider: MisskeyAPIProvider,
+    private val renoteMuteDao: RenoteMuteDao,
+    @IODispatcher private val coroutineDispatcher: CoroutineDispatcher,
+) : RenoteMuteRepository {
+
+    override suspend fun syncBy(accountId: Long): Result<Unit> = runCancellableCatching{
+        withContext(coroutineDispatcher) {
+            if (!isSupportRenoteMuteInstance()) {
+                return@withContext
+            }
+
+            val account = accountRepository.get(accountId).getOrThrow()
+
+            var mutes: List<RenoteMuteDTO> = emptyList()
+            while (true) {
+                val res = misskeyAPIProvider.get(account).getRenoteMutes(
+                    RenoteMutesRequest(
+                        i = account.token,
+                        untilId = mutes.lastOrNull()?.id
+                    )
+                ).throwIfHasError().body() ?: emptyList()
+                mutes = mutes + res
+                if (res.isEmpty() || res.size < 11) {
+                    break
+                }
+            }
+
+            val unPushedRenoteMutes = renoteMuteDao.findByUnPushed(account.accountId)
+                .map {
+                    it.toModel()
+                }
+                .filterNot { mute ->
+                    mutes.any { dto ->
+                        mute.userId.id == dto.muteeId
+                    }
+                }
+
+            coroutineScope {
+                unPushedRenoteMutes.map {
+                    async {
+                        create(it.userId)
+                    }
+                }
+            }
+
+            renoteMuteDao.deleteBy(account.accountId)
+            renoteMuteDao.insertAll(
+                mutes.map {
+                    RenoteMute(
+                        User.Id(accountId, it.id),
+                        createdAt = it.createdAt,
+                        postedAt = it.createdAt
+                    )
+                }.map {
+                    RenoteMuteRecord.from(it)
+                }
+            )
+
+        }
+    }
+
+    override suspend fun syncBy(userId: User.Id): Result<Unit> = runCancellableCatching {
+        withContext(coroutineDispatcher) {
+            if (!isSupportRenoteMuteInstance()) {
+                return@withContext
+            }
+        }
+    }
+
+    override suspend fun findBy(accountId: Long): Result<List<RenoteMute>> = runCancellableCatching {
+        withContext(coroutineDispatcher) {
+            renoteMuteDao.findByAccount(accountId).map {
+                it.toModel()
+            }
+        }
+    }
+
+    override suspend fun delete(userId: User.Id): Result<Unit> = runCancellableCatching {
+        withContext(coroutineDispatcher) {
+            val account = accountRepository.get(userId.accountId).getOrThrow()
+            val existing = findOne(userId).getOrNull()
+
+            if (existing != null) {
+                renoteMuteDao.delete(userId.accountId, userId.id)
+            }
+
+            try {
+                misskeyAPIProvider.get(account).deleteRenoteMute(
+                    DeleteRenoteMuteRequest(
+                        i = account.token,
+                        userId = userId.id,
+                    )
+                ).throwIfHasError().body()
+            } catch (_: APIError.NotFoundException) {}
+        }
+    }
+
+    override suspend fun findOne(userId: User.Id): Result<RenoteMute> = runCancellableCatching {
+        withContext(coroutineDispatcher) {
+            // NOTE: APIにIdを指定して取得する仕組みがないのでローカルのみから取得する
+            renoteMuteDao.findByUser(userId.accountId, userId.id)?.toModel()
+                ?: throw NoSuchElementException("RenoteMuteは存在しません:$userId")
+        }
+    }
+
+    override suspend fun create(userId: User.Id): Result<RenoteMute> = runCancellableCatching {
+        withContext(coroutineDispatcher) {
+            val account = accountRepository.get(userId.accountId).getOrThrow()
+            val isNeedPush = when (val exists = findOne(userId).getOrNull()) {
+                null -> {
+                    renoteMuteDao.insert(
+                        RenoteMuteRecord.from(
+                            RenoteMute(
+                                userId,
+                                createdAt = Clock.System.now(),
+                                postedAt = null,
+                            )
+                        )
+                    )
+                    true
+                }
+                else -> {
+                    exists.postedAt == null
+                }
+            }
+            val created = findOne(userId).getOrThrow()
+            if (isNeedPush && isSupportRenoteMuteInstance()) {
+                misskeyAPIProvider.get(account).createRenoteMute(
+                    CreateRenoteMuteRequest(
+                        i = account.token,
+                        userId = userId.id,
+                    )
+                ).throwIfHasError()
+
+                // APIへの送信に成功したのでpostedAtに成功した日時を記録する
+                renoteMuteDao.update(
+                    RenoteMuteRecord.from(
+                        created.copy(
+                            postedAt = Clock.System.now()
+                        )
+                    )
+                )
+            }
+
+            findOne(userId).getOrThrow()
+        }
+    }
+
+    override suspend fun exists(userId: User.Id): Result<Boolean> = runCancellableCatching {
+        findOne(userId).getOrNull() != null
+    }
+
+    override fun observeBy(accountId: Long): Flow<List<RenoteMute>> {
+        return renoteMuteDao.observeBy(accountId).map { list ->
+            list.map {
+                it.toModel()
+            }
+        }
+    }
+
+    private suspend fun isSupportRenoteMuteInstance(): Boolean {
+        // TODO: 実装する
+        return false
+    }
+
+
+}
