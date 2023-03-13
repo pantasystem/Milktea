a060cfd5f278caaa3be90a7228c00cdcd81c6d44
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-14 07:58:41 +0900

    feat: 設定画面に必要になりそうなViewModelを作成

---
+++ modules/features/setting/src/main/java/net/pantasystem/milktea/setting/activities/RenoteMuteSettingActivity.kt
@@ -0,0 +1,11 @@
+package net.pantasystem.milktea.setting.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RenoteMuteSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
