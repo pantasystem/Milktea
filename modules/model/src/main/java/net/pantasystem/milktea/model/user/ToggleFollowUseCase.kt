d228dd1c5b1f4df4e4c47088198e28c39f9778a1
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-17 12:06:02 +0900

    fix: フォローを解除するの時の条件を修正

--- modules/model/src/main/java/net/pantasystem/milktea/model/user/ToggleFollowUseCase.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/user/ToggleFollowUseCase.kt
@@ -2,21 +2,23 @@ package net.pantasystem.milktea.model.user
 
 import net.pantasystem.milktea.common.runCancellableCatching
 import net.pantasystem.milktea.model.UseCase
+import net.pantasystem.milktea.model.user.follow.FollowRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class ToggleFollowUseCase @Inject constructor(
-    val userRepository: UserRepository,
+    private val followRepository: FollowRepository,
+    private val userRepository: UserRepository,
 ) : UseCase {
 
     suspend operator fun invoke(userId: User.Id): Result<Unit> {
         return runCancellableCatching {
             val state = userRepository.find(userId, true) as User.Detail
-            if (state.related?.isFollowing == true) {
-                userRepository.unfollow(userId)
+            if (state.related?.isFollowing == true || state.related?.hasPendingFollowRequestFromYou == true) {
+                followRepository.delete(userId)
             } else {
-                userRepository.follow(userId)
+                followRepository.create(userId)
             }
         }
     }
