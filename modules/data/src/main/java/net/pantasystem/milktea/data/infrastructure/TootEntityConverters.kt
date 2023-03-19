18e5674dc915e45ce095e776bd1d5adcc7a21386
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-19 23:41:46 +0900

    feat: 絵文字リアクション仕様可否の情報を入れるようにした

--- modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/TootEntityConverters.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/TootEntityConverters.kt
@@ -7,6 +7,7 @@ import net.pantasystem.milktea.api.mastodon.notification.MstNotificationDTO
 import net.pantasystem.milktea.api.mastodon.poll.TootPollDTO
 import net.pantasystem.milktea.api.mastodon.status.StatusVisibilityType
 import net.pantasystem.milktea.api.mastodon.status.TootStatusDTO
+import net.pantasystem.milktea.data.converters.TootDTOEntityConverter
 import net.pantasystem.milktea.model.account.Account
 import net.pantasystem.milktea.model.drive.FileProperty
 import net.pantasystem.milktea.model.instance.MastodonInstanceInfo
@@ -14,7 +15,6 @@ import net.pantasystem.milktea.model.nodeinfo.NodeInfo
 import net.pantasystem.milktea.model.notes.Note
 import net.pantasystem.milktea.model.notes.Visibility
 import net.pantasystem.milktea.model.notes.poll.Poll
-import net.pantasystem.milktea.model.notes.reaction.ReactionCount
 import net.pantasystem.milktea.model.notification.*
 import net.pantasystem.milktea.model.user.User
 
@@ -54,88 +54,31 @@ fun TootMediaAttachment.toFileProperty(account: Account, isSensitive: Boolean): 
     )
 }
 
-fun TootStatusDTO.toNote(account: Account, nodeInfo: NodeInfo?): Note {
-    return Note(
-        id = Note.Id(account.accountId, id),
-        text = this.content,
-        cw = this.spoilerText.takeIf {
-            it.isNotBlank()
-        },
-        userId = User.Id(account.accountId, this.account.id),
-        replyId = this.inReplyToId?.let { Note.Id(account.accountId, this.inReplyToId!!) },
-        renoteId = this.quote?.id?.let { Note.Id(account.accountId, it) }
-            ?: this.reblog?.id?.let { Note.Id(account.accountId, this.reblog?.id!!) },
-        viaMobile = null,
-        visibility = Visibility(visibility, circleId, visibilityEx),
-        localOnly = null,
-        emojis = emojis.map {
-            it.toEmoji()
-        } + (emojiReactions?.mapNotNull {
-            it.getEmoji()
-        } ?: emptyList()),
-        app = null,
-        reactionCounts = emojiReactions?.map {
-            ReactionCount(
-                reaction = it.reaction,
-                count = it.count
-            )
-        } ?: emptyList(),
-        repliesCount = repliesCount,
-        renoteCount = reblogsCount,
-        uri = this.uri,
-        url = this.url,
-        visibleUserIds = emptyList(),
-        myReaction = emojiReactions?.firstOrNull {
-            it.myReaction() != null
-        }?.myReaction(),
-        channelId = null,
-        createdAt = createdAt,
-        fileIds = mediaAttachments.map {
-            FileProperty.Id(account.accountId, it.id)
-        },
-        poll = poll.toPoll(),
-        type = Note.Type.Mastodon(
-            favorited = favourited,
-            reblogged = reblogged,
-            bookmarked = bookmarked,
-            muted = muted,
-            favoriteCount = favouritesCount,
-            tags = tags?.map {
-                it.toModel()
-            } ?: emptyList(),
-            mentions = mentions?.map {
-                it.toModel()
-            } ?: emptyList(),
-            isFedibirdQuote = quote != null,
-            pollId = poll?.id,
-            isSensitive = sensitive,
-            pureText = text
-        ),
-        nodeInfo = nodeInfo,
-    )
-}
 
-fun TootStatusDTO.toEntities(account: Account, nodeInfo: NodeInfo?): NoteRelationEntities {
+suspend fun TootStatusDTO.toEntities(
+    converter: TootDTOEntityConverter,
+    account: Account, nodeInfo: NodeInfo?): NoteRelationEntities {
     val users = mutableListOf<User>()
     val notes = mutableListOf<Note>()
     val files = mutableListOf<FileProperty>()
-    pickEntities(account, notes, users, files, nodeInfo)
+    pickEntities(converter, account, notes, users, files, nodeInfo)
     return NoteRelationEntities(
-        note = toNote(account, nodeInfo),
+        note = converter.convert(this, account, nodeInfo),
         files = files,
         users = users,
         notes = notes,
     )
 }
 
-fun TootStatusDTO.pickEntities(
+suspend fun TootStatusDTO.pickEntities(
+    converter: TootDTOEntityConverter,
     account: Account,
     notes: MutableList<Note>,
     users: MutableList<User>,
     files: MutableList<FileProperty>,
     nodeInfo: NodeInfo?
 ) {
-    val (note, user) = toNote(account, nodeInfo) to this.account.toModel(account)
+    val (note, user) = converter.convert(this, account, nodeInfo) to this.account.toModel(account)
     notes.add(note)
     users.add(user)
     files.addAll(
@@ -143,8 +86,8 @@ fun TootStatusDTO.pickEntities(
             it.toFileProperty(account, sensitive)
         }
     )
-    this.reblog?.pickEntities(account, notes, users, files, nodeInfo)
-    this.quote?.pickEntities(account, notes, users, files, nodeInfo)
+    this.reblog?.pickEntities(converter, account, notes, users, files, nodeInfo)
+    this.quote?.pickEntities(converter, account, notes, users, files, nodeInfo)
 }
 
 fun MastodonAccountRelationshipDTO.toUserRelated(): User.Related {
