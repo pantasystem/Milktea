7e58450763251a2d729021ad7f07a55acc20d45a
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-21 00:56:22 +0900

    feat: SerialNameを割り当てるようにしました

--- modules/api/src/main/java/net/pantasystem/milktea/api/mastodon/report/MstReportDTO.kt
+++ modules/api/src/main/java/net/pantasystem/milktea/api/mastodon/report/MstReportDTO.kt
@@ -6,14 +6,33 @@ import net.pantasystem.milktea.api.mastodon.accounts.MastodonAccountDTO
 
 @kotlinx.serialization.Serializable
 data class MstReportDTO(
+    @SerialName("id")
     val id: String,
-    @SerialName("action_taken") val actionTaken: Boolean? = null,
-    @SerialName("action_taken_at") val actionTakenAt: Instant? = null,
+
+    @SerialName("action_taken")
+    val actionTaken: Boolean? = null,
+
+    @SerialName("action_taken_at")
+    val actionTakenAt: Instant? = null,
+
+    @SerialName("category")
     val category: String? = null,
+
+    @SerialName("comment")
     val comment: String? = null,
+
+    @SerialName("forwarded")
     val forwarded: Boolean? = null,
-    @SerialName("created_at") val createdAt: Instant? = null,
-    @SerialName("status_ids") val statusIds: List<String>? = null,
-    @SerialName("rule_ids") val ruleIds: List<String>? = null,
-    @SerialName("target_account") val targetAccount: MastodonAccountDTO? = null
+
+    @SerialName("created_at")
+    val createdAt: Instant? = null,
+
+    @SerialName("status_ids")
+    val statusIds: List<String>? = null,
+
+    @SerialName("rule_ids")
+    val ruleIds: List<String>? = null,
+
+    @SerialName("target_account")
+    val targetAccount: MastodonAccountDTO? = null
 )
