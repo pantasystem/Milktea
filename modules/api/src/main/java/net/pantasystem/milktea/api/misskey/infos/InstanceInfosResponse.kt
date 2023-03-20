86de6fcdb770ae3b21ee5f5b05fe3c7ad97be93f
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-21 03:16:36 +0900

    fix

--- modules/api/src/main/java/net/pantasystem/milktea/api/misskey/infos/InstanceInfosResponse.kt
+++ modules/api/src/main/java/net/pantasystem/milktea/api/misskey/infos/InstanceInfosResponse.kt
@@ -10,7 +10,7 @@ data class InstanceInfosResponse(
     @SerialName("date")
     val date: Instant,
 
-    @SerialName("instanceInfos")
+    @SerialName("instancesInfos")
     val instancesInfos: List<InstanceInfo>
 ) {
 
