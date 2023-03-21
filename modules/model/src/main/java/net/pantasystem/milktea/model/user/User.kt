3246d07cac85edacab9464e9ec3e9ad30db10032
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-22 04:53:18 +0900

    feat: hashCodeの結果をキャッシュするように

--- modules/model/src/main/java/net/pantasystem/milktea/model/user/User.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/user/User.kt
@@ -34,10 +34,38 @@ sealed interface User : Entity {
     val parsedResult: CustomEmojiParsedResult
 
 
-    data class Id(
+    class Id(
         val accountId: Long,
         val id: String,
-    ) : EntityId
+    ) : EntityId {
+
+        private var _hashCode: Int? = null
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Id
+
+            if (accountId != other.accountId) return false
+            if (id != other.id) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            if (_hashCode != null) return _hashCode!!
+            var result = accountId.hashCode()
+            result = 31 * result + id.hashCode()
+            _hashCode = result
+            return result
+        }
+
+        override fun toString(): String {
+            return "Id(accountId=$accountId, id='$id')"
+        }
+
+    }
 
     data class Simple(
         override val id: Id,
