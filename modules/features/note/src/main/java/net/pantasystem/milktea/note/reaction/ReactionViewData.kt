b0101efd0eb3d2b49afb687a0c8ca44b853334f6
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-22 05:27:26 +0900

    performance

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/reaction/ReactionViewData.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/reaction/ReactionViewData.kt
@@ -19,9 +19,7 @@ data class ReactionViewData(
             note: Note,
             instanceEmojis: Map<String, Emoji>?,
         ): List<ReactionViewData> {
-            val noteEmojis = note.emojis?.associateBy {
-                it.name
-            }
+            val noteEmojis = note.emojiNameMap
             return reactions.map { reactionCount ->
 
                 val textReaction = LegacyReaction.reactionMap[reactionCount.reaction] ?: reactionCount.reaction
