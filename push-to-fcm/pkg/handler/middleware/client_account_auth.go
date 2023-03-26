41e3cec895816079c54bc5a9d885bcbd2dd3820a
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-26 22:29:39 +0900

    feat: アカウントの存在状況に関わらずそのまま通過させるMiddlwareを追加

--- push-to-fcm/pkg/handler/middleware/client_account_auth.go
+++ push-to-fcm/pkg/handler/middleware/client_account_auth.go
@@ -12,6 +12,26 @@ type ClientAccountAuthMiddleware struct {
 	RepositoryModule repository.Module
 }
 
+func (r *ClientAccountAuthMiddleware) GetTokenWithAccount() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := GetToken(c)
+		if token == nil {
+			c.Next()
+			return
+		}
+		account, err := r.RepositoryModule.GetClientAccountRepository().FindByToken(c, *token)
+		if account != nil && err == nil {
+			c.Set("client_account", account)
+			c.Set("client_account_id", account.ID)
+			c.Next()
+			return
+		} else {
+			c.Next()
+			return
+		}
+	}
+}
+
 func (r *ClientAccountAuthMiddleware) CheckToken() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := GetToken(c)
@@ -22,6 +42,8 @@ func (r *ClientAccountAuthMiddleware) CheckToken() gin.HandlerFunc {
 		}
 		account, err := r.RepositoryModule.GetClientAccountRepository().FindByToken(c, *token)
 		if account != nil && err == nil {
+			c.Set("client_account", account)
+			c.Set("client_account_id", account.ID)
 			c.Next()
 			return
 		} else {
