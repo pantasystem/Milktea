0caaa4bf954195676c23ebf4f5aa3e2a67412760
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-26 22:25:31 +0900

    feat: impl middleware

--- push-to-fcm/pkg/handler/middleware/client_account_auth.go
+++ push-to-fcm/pkg/handler/middleware/client_account_auth.go
@@ -1,12 +1,35 @@
 package middleware
 
 import (
+	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"systems.panta.milktea/push-to-fcm/pkg/repository"
 )
 
 type ClientAccountAuthMiddleware struct {
+	RepositoryModule repository.Module
+}
+
+func (r *ClientAccountAuthMiddleware) CheckToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := GetToken(c)
+		if token == nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"type": "unauthorized", "message": "invalid token"})
+			c.Abort()
+			return
+		}
+		account, err := r.RepositoryModule.GetClientAccountRepository().FindByToken(c, *token)
+		if account != nil && err == nil {
+			c.Next()
+			return
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"type": "unauthorized", "message": "invalid token"})
+			c.Abort()
+			return
+		}
+	}
 }
 
 func GetToken(c *gin.Context) *string {
