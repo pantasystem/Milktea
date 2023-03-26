c3ec81e5cb2cdf862b60b16822a67cb90c19c669
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-27 07:12:56 +0900

    fix: struct

--- push-to-fcm/pkg/api/mastodon/account.go
+++ push-to-fcm/pkg/api/mastodon/account.go
@@ -59,9 +59,9 @@ type Account struct {
 }
 
 type TootEmojiDTO struct {
-	ShortCode string   `json:"shortcode"`
-	URL       string   `json:"url"`
-	StaticURL string   `json:"static_url"`
-	Category  string   `json:"category"`
-	VisibleIn []string `json:"visible_in_picker"`
+	ShortCode string `json:"shortcode"`
+	URL       string `json:"url"`
+	StaticURL string `json:"static_url"`
+	Category  string `json:"category"`
+	VisibleIn bool   `json:"visible_in_picker"`
 }
