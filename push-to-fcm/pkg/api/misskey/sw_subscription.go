f97544ec633f3ef04e3069d7a2298db1f6c66cff
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-26 23:32:49 +0900

    feat: 必要な構造体を定義

--- push-to-fcm/pkg/api/misskey/sw_subscription.go
+++ push-to-fcm/pkg/api/misskey/sw_subscription.go
@@ -1,1 +1,38 @@
 package misskey
+
+import "context"
+
+type State string
+
+const (
+	StateAlreadySubscribed State = "already-subscribed"
+	StateSubscribed        State = "subscribed"
+)
+
+type SWSubscription struct {
+	BaseUrl string
+	Token   string
+}
+
+type SubscribeReqest struct {
+	Endpoint  string `json:"endpoint"`
+	Auth      string `json:"auth"`
+	PublicKey string `json:"publicKey"`
+}
+
+type InternalSubscribeRequest struct {
+	I         string `json:"i"`
+	Endpoint  string `json:"endpoint"`
+	Auth      string `json:"auth"`
+	PublicKey string `json:"publicKey"`
+}
+
+type SubscribeResponse struct {
+	State State   `json:"state"`
+	Key   *string `json:"key"`
+}
+
+func (r *SWSubscription) Subscribe(ctx context.Context, req SubscribeReqest) (*SubscribeResponse, error) {
+	// TODO
+	return nil, nil
+}
