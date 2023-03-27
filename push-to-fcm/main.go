42228d52722621cb83bc9d816a9d63f732dd95f4
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-27 23:37:08 +0900

    feat: moduleを実装しコードレベルでは起動できる状態にした

--- push-to-fcm/main.go
+++ push-to-fcm/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"systems.panta.milktea/push-to-fcm/pkg/config"
+	"systems.panta.milktea/push-to-fcm/pkg/handler"
+	"systems.panta.milktea/push-to-fcm/pkg/root/impl"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	config := &config.Config{}
+	config.LoadFromEnv()
+
+	db, err := gorm.Open(postgres.Open(config.Dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	rootModule := impl.NewModule(db, config)
+	accountHandler := handler.ClientAccountHandler{
+		ServiceModule:    rootModule.GetServiceModule(),
+		RepositoryModule: rootModule.GetRepositoryModule(),
+	}
+	r := gin.Default()
+	accountHandler.RegisterHandlers(r)
+
+	r.Run()
+
 }
--- push-to-fcm/pkg/root/module.go
+++ push-to-fcm/pkg/root/module.go
@@ -1,7 +1,11 @@
 package root
 
-import "systems.panta.milktea/push-to-fcm/pkg/repository"
+import (
+	"systems.panta.milktea/push-to-fcm/pkg/repository"
+	"systems.panta.milktea/push-to-fcm/pkg/service"
+)
 
 type Module interface {
 	GetRepositoryModule() repository.Module
+	GetServiceModule() service.Module
 }
