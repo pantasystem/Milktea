614e4ef9249856233ca703b75c8fc0bf64d4beee
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-30 01:58:19 +0900

    feat: LRUキャッシュを使用するようにした

---
+++ modules/common/src/main/java/net/pantasystem/milktea/common/collection/LRUCache.kt
@@ -0,0 +1,40 @@
+package net.pantasystem.milktea.common.collection
+
+class LRUCache<K, V>(private val capacity: Int) {
+    val cache = linkedMapOf<K, V>()
+
+    fun get(key: K): V? {
+        synchronized(cache) {
+            val value = cache[key]
+
+            if(value != null) {
+                cache.remove(key)
+                cache[key] = value
+            }
+
+            return value
+        }
+    }
+
+    fun put(key: K, value: V) {
+        synchronized(cache) {
+            if(get(key) == null) {
+                if(cache.size >= capacity) {
+                    val firstKey = cache.iterator().next().key
+                    cache.remove(firstKey)
+                }
+
+                cache[key] = value
+            } else {
+                cache.remove(key)
+                cache[key] = value
+            }
+        }
+    }
+
+    fun remove(key: K) {
+        synchronized(cache) {
+            cache.remove(key)
+        }
+    }
+}
