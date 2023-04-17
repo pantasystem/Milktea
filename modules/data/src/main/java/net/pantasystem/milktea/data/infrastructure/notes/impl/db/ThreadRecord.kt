5199e2f352f21e303878fb738de25ae5464b005d
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-17 13:43:55 +0900

    feat: スレッドの構造をObjectBoxで表現できるように

---
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/notes/impl/db/ThreadRecord.kt
@@ -0,0 +1,25 @@
+package net.pantasystem.milktea.data.infrastructure.notes.impl.db
+
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+import io.objectbox.annotation.Index
+import io.objectbox.annotation.Unique
+import io.objectbox.relation.ToMany
+
+@Entity
+data class ThreadRecord(
+    @Id
+    var id: Long = 0,
+
+
+    var targetNoteId: String = "",
+    var accountId: Long = 0L,
+
+    @Index
+    @Unique
+    var targetNoteIdAndAccountId: String = "",
+) {
+    lateinit var ancestors: ToMany<NoteRecord>
+
+    lateinit var descendants: ToMany<NoteRecord>
+}
