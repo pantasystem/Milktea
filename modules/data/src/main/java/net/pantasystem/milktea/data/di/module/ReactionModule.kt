da0d6e7df7a7143469487adffc8dfe8da172e190
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-24 15:00:49 +0900

    rm: 不要になった処理を削除

--- modules/data/src/main/java/net/pantasystem/milktea/data/di/module/ReactionModule.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/di/module/ReactionModule.kt
@@ -5,13 +5,9 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import net.pantasystem.milktea.data.infrastructure.emoji.UserEmojiConfigRepositoryImpl
-import net.pantasystem.milktea.data.infrastructure.notes.reaction.impl.InMemoryReactionHistoryDataSource
-import net.pantasystem.milktea.data.infrastructure.notes.reaction.impl.ReactionHistoryPaginatorImpl
 import net.pantasystem.milktea.data.infrastructure.notes.reaction.impl.ReactionUserRepositoryImpl
 import net.pantasystem.milktea.data.infrastructure.notes.reaction.impl.history.ReactionHistoryRepositoryImpl
 import net.pantasystem.milktea.model.emoji.UserEmojiConfigRepository
-import net.pantasystem.milktea.model.notes.reaction.ReactionHistoryDataSource
-import net.pantasystem.milktea.model.notes.reaction.ReactionHistoryPaginator
 import net.pantasystem.milktea.model.notes.reaction.ReactionUserRepository
 import net.pantasystem.milktea.model.notes.reaction.history.ReactionHistoryRepository
 import javax.inject.Singleton
@@ -19,15 +15,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class ReactionModule {
-
-    @Binds
-    @Singleton
-    abstract fun bindReactionHistoryDataSource(ds: InMemoryReactionHistoryDataSource): ReactionHistoryDataSource
-
-    @Binds
-    @Singleton
-    abstract fun bindReactionHistoryPaging(impl: ReactionHistoryPaginatorImpl.Factory): ReactionHistoryPaginator.Factory
-
+    
     @Binds
     @Singleton
     abstract fun bindReactionHistoryRepository(impl: ReactionHistoryRepositoryImpl): ReactionHistoryRepository
