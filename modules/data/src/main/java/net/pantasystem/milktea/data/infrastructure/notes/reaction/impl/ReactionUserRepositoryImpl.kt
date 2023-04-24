3190d6d0c4a82814648480fa8b5d1d8b2f374ab2
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-24 14:27:28 +0900

    feat: Repositoryを実装

---
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/notes/reaction/impl/ReactionUserRepositoryImpl.kt
@@ -0,0 +1,86 @@
+package net.pantasystem.milktea.data.infrastructure.notes.reaction.impl
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
+import net.pantasystem.milktea.api.misskey.notes.reaction.ReactionHistoryDTO
+import net.pantasystem.milktea.api.misskey.notes.reaction.RequestReactionHistoryDTO
+import net.pantasystem.milktea.common.runCancellableCatching
+import net.pantasystem.milktea.common.throwIfHasError
+import net.pantasystem.milktea.data.api.mastodon.MastodonAPIProvider
+import net.pantasystem.milktea.data.api.misskey.MisskeyAPIProvider
+import net.pantasystem.milktea.model.account.Account
+import net.pantasystem.milktea.model.account.AccountRepository
+import net.pantasystem.milktea.model.notes.Note
+import net.pantasystem.milktea.model.notes.reaction.ReactionUserRepository
+import net.pantasystem.milktea.model.user.User
+import net.pantasystem.milktea.model.user.UserDataSource
+import net.pantasystem.milktea.model.user.UserRepository
+import javax.inject.Inject
+
+class ReactionUserRepositoryImpl @Inject constructor(
+    private val accountRepository: AccountRepository,
+    private val misskeyAPIProvider: MisskeyAPIProvider,
+    private val mastodonAPIProvider: MastodonAPIProvider,
+    private val userRepository: UserRepository,
+    private val userDataSource: UserDataSource,
+    private val dao: ReactionUserDAO,
+) : ReactionUserRepository {
+
+    override suspend fun syncBy(noteId: Note.Id, reaction: String): Result<Unit> = runCancellableCatching {
+        val account = accountRepository.get(noteId.accountId).getOrThrow()
+        dao.remove(noteId, reaction)
+        dao.createEmptyIfNotExists(noteId, reaction)
+        when(account.instanceType) {
+            Account.InstanceType.MISSKEY -> {
+                var reactions: List<ReactionHistoryDTO>
+                do {
+                    reactions = requireNotNull(
+                        misskeyAPIProvider.get(account).reactions(
+                            RequestReactionHistoryDTO(
+                                i = account.token,
+                                noteId = noteId.noteId,
+                                type = reaction,
+                            )
+                        ).throwIfHasError().body()
+                    )
+                    dao.appendAccountIds(noteId, reaction, reactions.map { it.user.id })
+                } while (reactions.isNotEmpty())
+
+            }
+            Account.InstanceType.MASTODON, Account.InstanceType.PLEROMA -> {
+                val resBody = requireNotNull(
+                    mastodonAPIProvider.get(account).getStatus(noteId.noteId)
+                        .throwIfHasError()
+                        .body()
+                )
+                val emojiReaction = resBody.emojiReactions?.firstOrNull {
+                    it.reaction == reaction
+                }
+                val accountIds = emojiReaction?.accountIds?: emptyList()
+                userRepository.syncIn(accountIds.map {
+                    User.Id(account.accountId, it)
+                })
+                dao.update(noteId, reaction, accountIds)
+            }
+
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override suspend fun observeBy(noteId: Note.Id, reaction: String): Flow<List<User>> {
+        return flow {
+            dao.createEmptyIfNotExists(noteId, reaction)
+            emit(accountRepository.get(noteId.accountId).getOrThrow())
+        }.flatMapLatest { account ->
+            dao.observeBy(noteId, reaction).filterNotNull().flatMapLatest {
+                userDataSource.observeIn(account.accountId, it.accountIds)
+            }
+        }
+    }
+
+    override suspend fun findBy(noteId: Note.Id, reaction: String): Result<List<User>> = runCancellableCatching {
+        val accountIds = dao.findBy(noteId, reaction)?.accountIds ?: emptyList()
+        userDataSource.getIn(noteId.accountId, accountIds).getOrThrow()
+    }
+
+}
