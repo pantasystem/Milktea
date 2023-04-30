071cbce60ed137f7d73adfe8be398965d8cead90
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-30 13:42:19 +0900

    feat: mastodonのリブログしたユーザを扱えるようにした

--- modules/model/src/main/java/net/pantasystem/milktea/model/notes/renote/RenotesPagingService.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/notes/renote/RenotesPagingService.kt
@@ -10,7 +10,7 @@ interface RenotesPagingService {
         fun create(noteId: Note.Id): RenotesPagingService
     }
 
-    val state: Flow<PageableState<List<Renote>>>
+    val state: Flow<PageableState<List<RenoteType>>>
     suspend fun next()
     suspend fun refresh()
     suspend fun clear()
