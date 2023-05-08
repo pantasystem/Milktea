d2b6a3f488dbb86ca42e5480faeab94ce2930545
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-05-08 19:40:22 +0900

    feat: viewModelとFragmentを作成

---
+++ modules/features/search/src/main/java/net/pantasystem/milktea/search/trend/TrendFragment.kt
@@ -0,0 +1,34 @@
+package net.pantasystem.milktea.search.trend
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.google.android.material.composethemeadapter.MdcTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TrendFragment : Fragment() {
+
+    private val viewModel by viewModels<TrendViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MdcTheme {
+                    val uiState by viewModel.uiState.collectAsState()
+
+                }
+            }
+        }
+    }
+}
---
+++ modules/features/search/src/main/java/net/pantasystem/milktea/search/trend/TrendViewModel.kt
@@ -0,0 +1,58 @@
+package net.pantasystem.milktea.search.trend
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
+import net.pantasystem.milktea.app_store.account.AccountStore
+import net.pantasystem.milktea.common.ResultState
+import net.pantasystem.milktea.common.StateContent
+import net.pantasystem.milktea.common.asLoadingStateFlow
+import net.pantasystem.milktea.model.account.Account
+import net.pantasystem.milktea.model.hashtag.Hashtag
+import net.pantasystem.milktea.model.hashtag.HashtagRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class TrendViewModel @Inject constructor(
+    val accountStore: AccountStore,
+    val hashtagRepository: HashtagRepository,
+) : ViewModel() {
+    private val currentAccount = accountStore.observeCurrentAccount.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        null,
+    )
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val trends = currentAccount.filterNotNull().flatMapLatest {
+        suspend {
+            hashtagRepository.trends(it.accountId).getOrThrow()
+        }.asLoadingStateFlow()
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        ResultState.Loading(StateContent.NotExist())
+    )
+
+    val uiState = combine(currentAccount, trends) { ca, t ->
+        TrendUiState(
+            currentAccount = ca,
+            trendTags = t
+        )
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        TrendUiState(
+            null,
+            ResultState.Loading(StateContent.NotExist())
+        )
+    )
+
+}
+
+data class TrendUiState(
+    val currentAccount: Account?,
+    val trendTags: ResultState<List<Hashtag>>,
+)
