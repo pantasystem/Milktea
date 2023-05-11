92969ed27963c01e495dd393b98b1dc229f1749a
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-05-11 15:05:32 +0900

    feat: 対象のアカウントがMastodonでも動作するようにした

--- modules/model/src/main/java/net/pantasystem/milktea/model/notes/repost/CreateRenoteMultipleAccountUseCase.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/notes/repost/CreateRenoteMultipleAccountUseCase.kt
@@ -3,14 +3,11 @@ package net.pantasystem.milktea.model.notes.repost
 import kotlinx.coroutines.coroutineScope
 import net.pantasystem.milktea.common.runCancellableCatching
 import net.pantasystem.milktea.model.UseCase
-import net.pantasystem.milktea.model.account.Account
 import net.pantasystem.milktea.model.account.AccountRepository
 import net.pantasystem.milktea.model.ap.ApResolver
 import net.pantasystem.milktea.model.ap.ApResolverRepository
-import net.pantasystem.milktea.model.notes.CreateNote
 import net.pantasystem.milktea.model.notes.Note
 import net.pantasystem.milktea.model.notes.NoteRepository
-import net.pantasystem.milktea.model.user.User
 import net.pantasystem.milktea.model.user.UserRepository
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -21,11 +18,12 @@ class CreateRenoteMultipleAccountUseCase @Inject constructor(
     private val apResolverRepository: ApResolverRepository,
     private val userRepository: UserRepository,
     private val noteRepository: NoteRepository,
+    private val checkCanRepostService: CheckCanRepostService,
 ) : UseCase {
 
     suspend operator fun invoke(
         noteId: Note.Id,
-        accountIds: List<Long>
+        accountIds: List<Long>,
     ): Result<List<Result<Note>>> = runCancellableCatching {
         coroutineScope {
             val accounts = accountIds.map {
@@ -38,45 +36,45 @@ class CreateRenoteMultipleAccountUseCase @Inject constructor(
         }
     }
 
-    private suspend fun resolveAndRenote(sourceNote: Note, accountId: Long): Result<Note> = runCancellableCatching {
-        val account = accountRepository.get(accountId).getOrThrow()
-        val relatedSourceNoteAccount = accountRepository.get(sourceNote.id.accountId).getOrThrow()
-        val user = userRepository.find(sourceNote.userId)
-        val noteUri = sourceNote.uri ?: "https://${user.host}/notes/${sourceNote.id.noteId}"
-        val relatedNote = if (account.getHost() != relatedSourceNoteAccount.getHost()) {
-            (apResolverRepository.resolve(accountId, noteUri).getOrThrow() as ApResolver.TypeNote)
-                .note
-        } else {
-            noteRepository.find(Note.Id(account.accountId, sourceNote.id.noteId)).getOrThrow()
+    private suspend fun resolveAndRenote(sourceNote: Note, accountId: Long): Result<Note> =
+        runCancellableCatching {
+            val account = accountRepository.get(accountId).getOrThrow()
+            val relatedSourceNoteAccount =
+                accountRepository.get(sourceNote.id.accountId).getOrThrow()
+            val user = userRepository.find(sourceNote.userId)
+            val noteUri = sourceNote.uri ?: "https://${user.host}/notes/${sourceNote.id.noteId}"
+            val relatedNote = if (account.getHost() != relatedSourceNoteAccount.getHost()) {
+                (apResolverRepository.resolve(accountId, noteUri)
+                    .getOrThrow() as ApResolver.TypeNote)
+                    .note
+            } else {
+                noteRepository.find(Note.Id(account.accountId, sourceNote.id.noteId)).getOrThrow()
+            }
+
+            renote(relatedNote).getOrThrow()
         }
 
-        renote(account, relatedNote).getOrThrow()
-    }
 
-
-    private suspend fun renote(account: Account, note: Note): Result<Note> = runCancellableCatching {
-        if (note.canRenote(User.Id(account.accountId, account.remoteId))) {
-            noteRepository.create(
-                CreateNote(
-                    author = account,
-                    text = null,
-                    visibility = note.visibility,
-                    renoteId = note.id,
-                    channelId = note.channelId
-                )
-            ).getOrThrow()
-        } else {
-            throw IllegalArgumentException()
+    private suspend fun renote(note: Note): Result<Note> =
+        runCancellableCatching {
+            if (checkCanRepostService
+                    .canRepost(note.id)
+                    .getOrElse { false }
+            ) {
+                noteRepository.renote(note.id).getOrThrow()
+            } else {
+                throw IllegalArgumentException()
+            }
         }
-    }
 
-    private suspend fun recursiveSearchHasContentNote(noteId: Note.Id): Result<Note> = runCancellableCatching {
-        val note = noteRepository.find(noteId).getOrThrow()
-        if (note.hasContent()) {
-            note
-        } else {
-            recursiveSearchHasContentNote(note.renoteId!!).getOrThrow()
+    private suspend fun recursiveSearchHasContentNote(noteId: Note.Id): Result<Note> =
+        runCancellableCatching {
+            val note = noteRepository.find(noteId).getOrThrow()
+            if (note.hasContent()) {
+                note
+            } else {
+                recursiveSearchHasContentNote(note.renoteId!!).getOrThrow()
+            }
         }
-    }
 }
 
