e418c55671d9e7531c30f1d534b441928b1e9588
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-05-12 13:23:36 +0900

    feat: パフォーマンスの改善を行いました

--- modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/emoji/delegate/CustomEmojiUpInsertDelegate.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/emoji/delegate/CustomEmojiUpInsertDelegate.kt
@@ -1,5 +1,6 @@
 package net.pantasystem.milktea.data.infrastructure.emoji.delegate
 
+import androidx.room.Transaction
 import net.pantasystem.milktea.data.infrastructure.emoji.db.CustomEmojiAliasRecord
 import net.pantasystem.milktea.data.infrastructure.emoji.db.CustomEmojiDAO
 import net.pantasystem.milktea.data.infrastructure.emoji.db.toRecord
@@ -9,40 +10,49 @@ import javax.inject.Inject
 internal class CustomEmojiUpInsertDelegate @Inject constructor(
     private val customEmojiDAO: CustomEmojiDAO,
 ) {
+
+    @Transaction
     suspend operator fun invoke(host: String, emojis: List<Emoji>) {
         val record = emojis.map {
             it.toRecord(host)
         }
-        val ids = customEmojiDAO.insertAll(record)
+        val exists = customEmojiDAO.findBy(host).associateBy {
+            it.emoji.name
+        }
 
-        ids.mapIndexed { index, id ->
+        val insertResults = customEmojiDAO.insertAll(record)
+
+        val removedEmojis = emojis.mapNotNull {
+            exists[it.name]?.emoji
+        }
+        customEmojiDAO.deleteBy(removedEmojis)
+
+        val ids = insertResults.mapIndexed { index, id ->
             if (id == -1L) {
-                customEmojiDAO.findBy(host, emojis[index].name).firstOrNull()?.let { record ->
-                    customEmojiDAO.update(emojis[index].toRecord(host, record.emoji.id))
-                    customEmojiDAO.deleteAliasByEmojiId(record.emoji.id)
-                    emojis[index].aliases?.map {
-                        CustomEmojiAliasRecord(
-                            emojiId = record.emoji.id,
-                            it
-                        )
-                    }?.let {
-                        customEmojiDAO.insertAliases(it)
-                    }
-                    record.emoji.id
-                }
+                customEmojiDAO.findBy(host, emojis[index].name).firstOrNull()?.emoji?.id ?: -1
             } else {
-                emojis[index].aliases?.filterNot {
-                    it.isBlank()
-                }?.map {
-                    CustomEmojiAliasRecord(
-                        emojiId = id,
-                        it
-                    )
-                }?.let {
-                    customEmojiDAO.insertAliases(it)
-                }
                 id
             }
+        }
+
+        customEmojiDAO.deleteAliasByEmojiIds(ids.filterNot { it == -1L })
+        val aliasRecords = emojis.mapIndexedNotNull { index, emoji ->
+            val id = ids[index]
+            if (id == -1L) {
+                null
+            } else {
+                emoji.aliases?.filterNot {
+                    it.isBlank()
+                }?.map { alias ->
+                    CustomEmojiAliasRecord(
+                        emojiId = id,
+                        value = alias
+                    )
+                }
+            }
+        }.flatten()
+        aliasRecords.chunked(500).map {
+            customEmojiDAO.insertAliases(it)
         }
     }
 }
