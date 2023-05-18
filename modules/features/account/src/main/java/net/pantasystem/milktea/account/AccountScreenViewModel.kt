6695be72188f6f64defe3f6280279adff9701d4e
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-05-18 14:35:29 +0900

    fix

--- modules/features/account/src/main/java/net/pantasystem/milktea/account/AccountScreenViewModel.kt
+++ modules/features/account/src/main/java/net/pantasystem/milktea/account/AccountScreenViewModel.kt
@@ -8,8 +8,8 @@ import kotlinx.coroutines.flow.*
 import net.pantasystem.milktea.app_store.account.AccountStore
 import net.pantasystem.milktea.common.Logger
 import net.pantasystem.milktea.model.account.Account
-import net.pantasystem.milktea.model.instance.InstanceInfo
-import net.pantasystem.milktea.model.instance.InstanceInfoRepository
+import net.pantasystem.milktea.model.instance.InstanceInfoService
+import net.pantasystem.milktea.model.instance.InstanceInfoType
 import net.pantasystem.milktea.model.instance.online.user.count.OnlineUserCountRepository
 import net.pantasystem.milktea.model.instance.online.user.count.OnlineUserCountResult
 import net.pantasystem.milktea.model.user.User
@@ -23,7 +23,7 @@ class AccountScreenViewModel @Inject constructor(
     private val userRepository: UserRepository,
     private val userDataSource: UserDataSource,
     private val onlineUserCountRepository: OnlineUserCountRepository,
-    private val instanceInfoRepository: InstanceInfoRepository,
+    private val instanceInfoService: InstanceInfoService,
     private val loggerFactory: Logger.Factory,
 ) : ViewModel() {
 
@@ -46,13 +46,16 @@ class AccountScreenViewModel @Inject constructor(
         null
     )
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    private val instanceInfo = accountStore.observeCurrentAccount.filterNotNull().flatMapLatest {
-        instanceInfoRepository.observeByHost(it.getHost())
+    private val instanceInfo = accountStore.observeCurrentAccount.filterNotNull().map {
+        instanceInfoService.find(it.normalizedInstanceUri).getOrThrow()
+    }.catch {
+        logger.error("インスタンス情報の取得に失敗", it)
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), null)
 
     private val onlineUserCount = accountStore.observeCurrentAccount.filterNotNull().map {
-        onlineUserCountRepository.find(it.accountId).getOrNull()
+        onlineUserCountRepository.find(it.accountId).onFailure { e ->
+            logger.error("オンラインユーザー数の取得に失敗", e)
+        }.getOrNull()
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), null)
 
     val uiState = combine(currentAccount, user, instanceInfo, onlineUserCount) { a, u, info, count ->
@@ -76,6 +79,6 @@ class AccountScreenViewModel @Inject constructor(
 data class AccountUiState(
     val currentAccount: Account? = null,
     val userInfo: User? = null,
-    val instanceInfo: InstanceInfo? = null,
+    val instanceInfo: InstanceInfoType? = null,
     val onlineUserCount: OnlineUserCountResult? = null,
 )
