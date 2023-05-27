3385633852b913408f328a4e3a40529aad1109ff
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-05-28 01:14:23 +0900

    feat: 現在の購読位置に対応するNoteIdを保持できるようにした

--- modules/data/src/main/java/net/pantasystem/milktea/data/di/module/NoteModule.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/di/module/NoteModule.kt
@@ -1,24 +1,22 @@
 package net.pantasystem.milktea.data.di.module
 
+import android.content.Context
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import net.pantasystem.milktea.app_store.notes.NoteTranslationStore
 import net.pantasystem.milktea.app_store.notes.TimelineStore
-import net.pantasystem.milktea.data.infrastructure.notes.NoteStreamingImpl
-import net.pantasystem.milktea.data.infrastructure.notes.NoteTranslationStoreImpl
-import net.pantasystem.milktea.data.infrastructure.notes.ReplyStreamingImpl
-import net.pantasystem.milktea.data.infrastructure.notes.TimelineStoreImpl
+import net.pantasystem.milktea.common.getPreferences
+import net.pantasystem.milktea.data.infrastructure.notes.*
 import net.pantasystem.milktea.data.infrastructure.notes.draft.DraftNoteRepositoryImpl
 import net.pantasystem.milktea.data.infrastructure.notes.impl.DraftNoteServiceImpl
 import net.pantasystem.milktea.data.infrastructure.notes.impl.NoteRepositoryImpl
 import net.pantasystem.milktea.data.infrastructure.notes.impl.ObjectBoxNoteDataSource
 import net.pantasystem.milktea.data.infrastructure.notes.renote.RenotesPagingServiceImpl
-import net.pantasystem.milktea.model.notes.NoteDataSource
-import net.pantasystem.milktea.model.notes.NoteRepository
-import net.pantasystem.milktea.model.notes.NoteStreaming
-import net.pantasystem.milktea.model.notes.ReplyStreaming
+import net.pantasystem.milktea.model.notes.*
 import net.pantasystem.milktea.model.notes.draft.DraftNoteRepository
 import net.pantasystem.milktea.model.notes.draft.DraftNoteService
 import net.pantasystem.milktea.model.notes.repost.RenotesPagingService
@@ -60,6 +58,20 @@ abstract class NoteBindModule{
     @Singleton
     abstract fun bindReplyStreaming(impl: ReplyStreamingImpl): ReplyStreaming
 
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NoteProvideModule {
+    @Provides
+    @Singleton
+    fun provideTimelineScrollPositionRepository(
+        @ApplicationContext context: Context
+    ): TimelineScrollPositionRepository {
+        return TimelineScrollPositionRepositoryImpl(
+            context.getPreferences()
+        )
+    }
 }
 
 
