e501a829edf7291828c20268c8f9e97fcbde1f04
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-05-30 11:24:56 +0900

    feat: Flowで状態を制御するようにした

--- modules/features/antenna/src/main/java/net/pantasystem/milktea/antenna/viewmodel/AntennaListViewModel.kt
+++ modules/features/antenna/src/main/java/net/pantasystem/milktea/antenna/viewmodel/AntennaListViewModel.kt
@@ -1,31 +1,77 @@
 package net.pantasystem.milktea.antenna.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.plus
 import net.pantasystem.milktea.app_store.account.AccountStore
-import net.pantasystem.milktea.common.runCancellableCatching
+import net.pantasystem.milktea.common.ResultState
+import net.pantasystem.milktea.common.StateContent
+import net.pantasystem.milktea.common.asLoadingStateFlow
 import net.pantasystem.milktea.common_android.eventbus.EventBus
-import net.pantasystem.milktea.model.account.AccountRepository
+import net.pantasystem.milktea.model.account.Account
 import net.pantasystem.milktea.model.account.page.Pageable
 import net.pantasystem.milktea.model.account.page.PageableTemplate
 import net.pantasystem.milktea.model.antenna.Antenna
 import net.pantasystem.milktea.model.antenna.AntennaRepository
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class AntennaListViewModel @Inject constructor(
     private val accountStore: AccountStore,
-    private val accountRepository: AccountRepository,
     private val antennaRepository: AntennaRepository,
+    private val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
+    companion object {
+        const val EXTRA_SPECIFIED_ACCOUNT_ID = "AntennaListViewModel.EXTRA_SPECIFIED_ACCOUNT_ID"
+        const val EXTRA_ADD_TAB_TO_ACCOUNT_ID = "AntennaListViewModel.EXTRA_ADD_TAB_TO_ACCOUNT_ID"
+    }
 
-    val antennas = MediatorLiveData<List<Antenna>>()
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val currentAccount = savedStateHandle.getStateFlow<Long?>(
+        EXTRA_SPECIFIED_ACCOUNT_ID,
+        null
+    ).flatMapLatest { accountId ->
+        accountStore.state.map { state ->
+            accountId?.let {
+                state.get(it)
+            } ?: state.currentAccount
+        }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), null)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val addTabToAccount = savedStateHandle.getStateFlow<Long?>(
+        EXTRA_ADD_TAB_TO_ACCOUNT_ID,
+        null
+    ).flatMapLatest { accountId ->
+        accountStore.state.map { state ->
+            accountId?.let { accountId ->
+                state.get(accountId)
+            } ?: state.currentAccount
+        }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), null)
+
+    private val refreshAntennasEvents = MutableStateFlow(Date().time)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val antennasState = refreshAntennasEvents.flatMapLatest {
+        currentAccount.filterNotNull().flatMapLatest { account ->
+            suspend {
+                Log.d("AntennaListViewModel", "antenna account state: ${account.accountId}")
+                antennaRepository.findByAccountId(account.accountId).getOrThrow()
+            }.asLoadingStateFlow()
+        }
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        ResultState.Loading(StateContent.NotExist())
+    )
 
     val editAntennaEvent = EventBus<Antenna>()
 
@@ -33,12 +79,6 @@ class AntennaListViewModel @Inject constructor(
 
     private val openAntennasTimelineEvent = EventBus<Antenna>()
 
-    val isLoading = MutableLiveData(false)
-    private var mIsLoading: Boolean = false
-        set(value) {
-            field = value
-            isLoading.postValue(value)
-        }
 
     private val mPagedAntennaIds = MutableLiveData<Set<Antenna.Id>>()
     val pagedAntennaIds: LiveData<Set<Antenna.Id>> = mPagedAntennaIds
@@ -55,7 +95,6 @@ class AntennaListViewModel @Inject constructor(
                     if (pageable is Pageable.Antenna) {
                         it.accountId.let { accountId ->
                             Antenna.Id(accountId, pageable.antennaId)
-
                         }
                     } else {
                         null
@@ -70,16 +109,7 @@ class AntennaListViewModel @Inject constructor(
     private val deleteResultEvent = EventBus<Boolean>()
 
     fun loadInit() {
-        viewModelScope.launch {
-            runCancellableCatching {
-                val account = accountRepository.getCurrentAccount().getOrThrow()
-                antennaRepository.findByAccountId(account.accountId).getOrThrow()
-            }.onSuccess {
-                antennas.postValue(it)
-            }
-            mIsLoading = false
-        }
-
+        refreshAntennasEvents.tryEmit(Date().time)
     }
 
     fun toggleTab(antenna: Antenna?) {
@@ -127,3 +157,14 @@ class AntennaListViewModel @Inject constructor(
         }
     }
 }
+
+data class AntennaListItem(
+    val antenna: Antenna,
+    val isAddedToTab: Boolean,
+)
+
+data class AntennaListUiState(
+    val currentAccount: Account?,
+    val addTabToAccount: Account?,
+    val antennas: ResultState<List<AntennaListItem>>,
+)
