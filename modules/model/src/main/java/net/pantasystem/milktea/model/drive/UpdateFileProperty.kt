d636a751f77b0a11b89c18292fcb464917d91557
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-06-14 00:46:15 +0900

    feat: 3値(null, some, undefined)を表現できるようにしました

--- modules/model/src/main/java/net/pantasystem/milktea/model/drive/UpdateFileProperty.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/drive/UpdateFileProperty.kt
@@ -2,10 +2,10 @@ package net.pantasystem.milktea.model.drive
 
 data class UpdateFileProperty(
     val fileId: FileProperty.Id,
-    val folderId: String?,
-    val name: String,
-    val isSensitive: Boolean,
-    val comment: String?,
+    val folderId: ValueType<String>? = null,
+    val name: ValueType<String>? = null,
+    val isSensitive: ValueType<Boolean>? = null,
+    val comment: ValueType<String>? = null,
 )
 
 sealed interface ValueType<T> {
