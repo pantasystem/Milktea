66eb2efd282306b7942b8a4bc67f26cf4c81d75f
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-06-14 00:47:01 +0900

    feat: Kotlin serializationのclassのままでは3値を表現するのは困難だったため、直接JsonObjectを扱うようにした（地獄）

--- modules/api/src/main/java/net/pantasystem/milktea/api/misskey/drive/UpdateFileDTO.kt
+++ modules/api/src/main/java/net/pantasystem/milktea/api/misskey/drive/UpdateFileDTO.kt
@@ -1,39 +1,75 @@
 package net.pantasystem.milktea.api.misskey.drive
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.buildJsonObject
 import net.pantasystem.milktea.model.drive.UpdateFileProperty
+import net.pantasystem.milktea.model.drive.ValueType
 
-@Serializable
-data class UpdateFileDTO(
-    @SerialName("i")
-    val i: String,
+//@Serializable
+//data class UpdateFileDTO(
+//    @SerialName("i")
+//    val i: String,
+//
+//    @SerialName("fileId")
+//    val fileId: String,
+//
+//    @SerialName("folderId")
+//    val folderId: String?,
+//
+//    @SerialName("name")
+//    val name: String,
+//
+//    @SerialName("comment")
+//    val comment: String?,
+//
+//    @SerialName("isSensitive")
+//    val isSensitive: Boolean,
+//) {
+//    companion object
+//}
 
-    @SerialName("fileId")
-    val fileId: String,
+//fun UpdateFileDTO.Companion.from(token: String, model: UpdateFileProperty): UpdateFileDTO {
+//    return UpdateFileDTO(
+//        i = token,
+//        comment = model.comment,
+//        fileId = model.fileId.fileId,
+//        folderId = model.folderId,
+//        isSensitive = model.isSensitive,
+//        name = model.name
+//    )
+//}
 
-    @SerialName("folderId")
-    val folderId: String?,
+@OptIn(ExperimentalSerializationApi::class)
+fun UpdateFileProperty.toJsonObject(token: String): JsonObject {
+    return buildJsonObject {
+        put("i", JsonPrimitive(token))
+        put("fileId", JsonPrimitive(fileId.fileId))
 
-    @SerialName("name")
-    val name: String,
+        when(val v = comment) {
+            is ValueType.Empty -> put("comment", JsonPrimitive(null))
+            is ValueType.Some -> put("comment", JsonPrimitive(v.value))
+            null -> Unit
+        }
 
-    @SerialName("comment")
-    val comment: String?,
+        when(val v = folderId) {
+            is ValueType.Empty -> put("folderId", JsonPrimitive(null))
+            is ValueType.Some -> put("folderId", JsonPrimitive(v.value))
+            null -> Unit
+        }
 
-    @SerialName("isSensitive")
-    val isSensitive: Boolean,
-) {
-    companion object
-}
+        when(val v = name) {
+            is ValueType.Empty -> put("name", JsonPrimitive(null))
+            is ValueType.Some -> put("name", JsonPrimitive(v.value))
+            null -> Unit
+        }
 
-fun UpdateFileDTO.Companion.from(token: String, model: UpdateFileProperty): UpdateFileDTO {
-    return UpdateFileDTO(
-        i = token,
-        comment = model.comment,
-        fileId = model.fileId.fileId,
-        folderId = model.folderId,
-        isSensitive = model.isSensitive,
-        name = model.name
-    )
+        when(val v = isSensitive) {
+            is ValueType.Empty -> put("isSensitive", JsonPrimitive(null))
+            is ValueType.Some -> put("isSensitive", JsonPrimitive(v.value))
+            null -> Unit
+        }
+
+    }
 }
