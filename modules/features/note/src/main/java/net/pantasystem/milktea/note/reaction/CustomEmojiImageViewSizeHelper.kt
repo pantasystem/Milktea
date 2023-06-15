e06c5dd94e804ae61de73c0cbb68d77ad720db94
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-06-15 14:25:00 +0900

    fix

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/reaction/CustomEmojiImageViewSizeHelper.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/reaction/CustomEmojiImageViewSizeHelper.kt
@@ -2,31 +2,23 @@
 
 package net.pantasystem.milktea.note.reaction
 
-import android.content.Context
 import android.view.ViewGroup
 import android.widget.ImageView
+import kotlin.math.max
 
 object CustomEmojiImageViewSizeHelper {
 
     fun<T: ViewGroup.LayoutParams> ImageView.applySizeByAspectRatio(baseHeightDp: Int, aspectRatio: Float?) {
-        val (imageViewWidthPx, imageViewHeightPx) = context.calculateImageWidthAndHeightSize(baseHeightDp, aspectRatio)
-        val params = layoutParams as T
-        params.height = imageViewHeightPx.toInt()
-        params.width = imageViewWidthPx.toInt()
-        layoutParams = params
-    }
-
-    fun Context.calculateImageWidthAndHeightSize(baseHeightDp: Int, aspectRatio: Float?): Pair<Float, Float> {
         val metrics = resources.displayMetrics
         val heightPx = baseHeightDp * metrics.density
-        return calculateImageWidthAndHeightSize(heightPx, aspectRatio)
+        applySizeByAspectRatio<T>(heightPx, aspectRatio)
     }
 
     fun<T: ViewGroup.LayoutParams> ImageView.applySizeByAspectRatio(baseHeightPx: Float, aspectRatio: Float?) {
         val (imageViewWidthPx, imageViewHeightPx) = calculateImageWidthAndHeightSize(baseHeightPx, aspectRatio)
         val params = layoutParams as T
         params.height = imageViewHeightPx.toInt()
-        params.width = imageViewWidthPx.toInt()
+        params.width = max(imageViewWidthPx, imageViewHeightPx).toInt()
         layoutParams = params
     }
 
