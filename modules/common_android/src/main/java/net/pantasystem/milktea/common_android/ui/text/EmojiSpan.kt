308d9e4cbe14ffc3a62adc1cf7279d31a8219226
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-06-17 00:39:54 +0900

    refactor: 重複していた責務のロジックを共通化した

--- modules/common_android/src/main/java/net/pantasystem/milktea/common_android/ui/text/EmojiSpan.kt
+++ modules/common_android/src/main/java/net/pantasystem/milktea/common_android/ui/text/EmojiSpan.kt
@@ -45,32 +45,7 @@ abstract class EmojiSpan<T: Any?>(val key: T, val aspectRatio: Float? = null) : 
     ): Int {
         val textHeight = paint.textSize
 
-        val drawable = imageDrawable
-        val size = key?.let {
-            drawableSizeCache[key]
-        } ?: drawable?.let {
-            // NOTE: drawableSizeCacheに画像のサイズが登録されていない場合は、drawableからサイズを取得する
-            EmojiSizeCache(
-                intrinsicHeight = it.intrinsicHeight,
-                intrinsicWidth = it.intrinsicWidth
-            )
-        } ?: aspectRatio?.let {
-            // NOTE: drawableが読み込まれていない状態の時は、文字の高さと画像の比率から横幅のサイズを取得する
-            EmojiSizeCache(
-                intrinsicHeight = textHeight.toInt(),
-                intrinsicWidth = (textHeight * aspectRatio).toInt()
-            )
-        }
-
-        // NOTE: keyが存在しかつdrawableが存在する場合は、drawableSizeCacheを更新する
-        key?.run {
-            drawableSizeCache[key] ?: drawable?.let {
-                EmojiSizeCache(
-                    intrinsicHeight = it.intrinsicHeight,
-                    intrinsicWidth = it.intrinsicWidth
-                )
-            }
-        }
+        val size = calculateEmojiSize(textHeight)
         val metrics = paint.fontMetricsInt
         if (fm != null) {
             fm.top = metrics.top
@@ -84,20 +59,13 @@ abstract class EmojiSpan<T: Any?>(val key: T, val aspectRatio: Float? = null) : 
             beforeTextSize = (paint.textSize * 1.2).toInt()
             return beforeTextSize
         }
-        key?.run {
-            drawableSizeCache[key] = size
-        }
 
         // NOTE: 暫定的なサイズではない場合はbeforeTextSizeを0にする必要性がある
         beforeTextSize = 0
 
-        val imageWidth = size.intrinsicWidth
-        val imageHeight = size.intrinsicHeight
+        val imageWidth = size.first
 
-        // 画像がテキストの高さよりも大きい場合、画像をテキストと同じ高さに縮小する
-        val scale = textHeight / imageHeight.toFloat()
-        // テキストの高さに合わせた画像の幅
-        return (imageWidth * scale).toInt()
+        return imageWidth.toInt()
     }
 
     override fun updateDrawState(ds: TextPaint) {
@@ -138,27 +106,9 @@ abstract class EmojiSpan<T: Any?>(val key: T, val aspectRatio: Float? = null) : 
      */
     private fun updateImageDrawableSize(paint: Paint) {
         val emojiHeight = min((paint.textSize).toInt(), 128)
-        val drawable = imageDrawable
-
-        // drawableSizeCacheあるいはdrawableあるいはaspectRatioと文字サイズから画像のwidth, heightを得る処理
-        val size = key?.let {
-            drawableSizeCache[key]
-        } ?: drawable?.let {
-            EmojiSizeCache(
-                intrinsicWidth = it.intrinsicWidth,
-                intrinsicHeight = it.intrinsicHeight
-            )
-        } ?: aspectRatio?.let {
-            EmojiSizeCache(
-                intrinsicHeight = emojiHeight,
-                intrinsicWidth = (emojiHeight.toFloat() * aspectRatio).toInt()
-            )
-        } ?: return
-        key?.run {
-            drawableSizeCache[key] = size
-        }
-        val imageWidth = size.intrinsicWidth
-        val imageHeight = size.intrinsicHeight
+        val size = calculateEmojiSize(min((paint.textSize), 128f))
+        val imageWidth = size?.first ?: -1f
+        val imageHeight = size?.second?: -1f
 
         // 計算された画像サイズが適切なものかチェックする
         val unknownEmojiSize = imageWidth <= 0 || imageHeight <= 0
@@ -173,14 +123,58 @@ abstract class EmojiSpan<T: Any?>(val key: T, val aspectRatio: Float? = null) : 
             return
         }
 
-        val ratio = imageWidth.toFloat() / imageHeight.toFloat()
-
-        val scaledImageWidth = (emojiHeight * ratio).toInt()
-
         if (!isSizeComputed) {
             isSizeComputed = imageDrawable != null
-            imageDrawable?.setBounds(0, 0, scaledImageWidth, emojiHeight)
+            imageDrawable?.setBounds(0, 0, imageWidth.toInt(), imageHeight.toInt())
         }
+    }
+
+    private fun calculateEmojiSize(textSize: Float): Pair<Float, Float>? {
+        val drawable = imageDrawable
+        val size = key?.let {
+            drawableSizeCache[key]
+        } ?: drawable?.let {
+            // NOTE: drawableSizeCacheに画像のサイズが登録されていない場合は、drawableからサイズを取得する
+            EmojiSizeCache(
+                intrinsicHeight = it.intrinsicHeight,
+                intrinsicWidth = it.intrinsicWidth
+            )
+        } ?: aspectRatio?.let {
+            // NOTE: drawableが読み込まれていない状態の時は、文字の高さと画像の比率から横幅のサイズを取得する
+            EmojiSizeCache(
+                intrinsicHeight = textSize.toInt(),
+                intrinsicWidth = (textSize * aspectRatio).toInt()
+            )
+        }
+
+        // NOTE: keyが存在しかつdrawableが存在する場合は、drawableSizeCacheを更新する
+        key?.run {
+            drawableSizeCache[key] ?: drawable?.let {
+                EmojiSizeCache(
+                    intrinsicHeight = it.intrinsicHeight,
+                    intrinsicWidth = it.intrinsicWidth
+                )
+            }
+        }
+
+        // NOTE: 画像のサイズが不明なときはnullを返す
+        if (size == null) {
+            return null
+        }
+        key?.run {
+            drawableSizeCache[key] = size
+        }
+
+        val imageWidth = size.intrinsicWidth
+        val imageHeight = size.intrinsicHeight
+
+        // 画像がテキストの高さよりも大きい場合、画像をテキストと同じ高さに縮小する
+        val scale = textSize / imageHeight
+
+        // テキストの高さに合わせた画像の幅
+        val width = imageWidth * scale
+
+        return width to textSize
     }
 
 }
