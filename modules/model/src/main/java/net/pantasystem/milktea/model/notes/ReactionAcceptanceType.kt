d3c06ed45e8817997ad6fb85094e33fbef30c1fc
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-07-09 20:17:23 +0900

    feat: リアクションの受け入れ条件を表す定数を追加

---
+++ modules/model/src/main/java/net/pantasystem/milktea/model/notes/ReactionAcceptanceType.kt
@@ -0,0 +1,8 @@
+package net.pantasystem.milktea.model.notes
+
+enum class ReactionAcceptanceType {
+    LikeOnly,
+    LikeOnly4Remote,
+    NonSensitiveOnly,
+    NonSensitiveOnly4LocalOnly4Remote
+}
