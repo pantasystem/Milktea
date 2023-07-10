c04deeb473bb51509d7bdfb62ff237bbcb481645
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-07-10 10:03:24 +0900

    feat: 並び順を変更

--- modules/model/src/main/java/net/pantasystem/milktea/model/notes/ReactionAcceptanceType.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/notes/ReactionAcceptanceType.kt
@@ -1,8 +1,8 @@
 package net.pantasystem.milktea.model.notes
 
 enum class ReactionAcceptanceType {
+    LikeOnly4Remote,
     NonSensitiveOnly,
     NonSensitiveOnly4LocalOnly4Remote,
-    LikeOnly4Remote,
     LikeOnly,
 }
