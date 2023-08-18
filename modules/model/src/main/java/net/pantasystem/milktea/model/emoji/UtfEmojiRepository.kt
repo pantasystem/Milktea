ee0041594e65e93fcd3cc95dca83b2455ad1dc9c
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-08-18 21:40:27 +0900

    rm: 不要になったリソースを削除

--- modules/model/src/main/java/net/pantasystem/milktea/model/emoji/UtfEmojiRepository.kt
+++
@@ -1,6 +0,0 @@
-package net.pantasystem.milktea.model.emoji
-
-//interface UtfEmojiRepository {
-//    suspend fun findAll(): List<Utf8Emoji>
-//    suspend fun exists(emoji: CharSequence): Boolean
-//}
