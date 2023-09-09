6fd0a7b5cfe169c83ad0a17549eef8c1c7168532
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-09-09 15:30:48 +0900

    feat: foregroundとして実行し通知を表示するようにした。

--- modules/worker/src/main/java/net/pantasystem/milktea/worker/emoji/cache/CacheCustomEmojiImageWorker.kt
+++ modules/worker/src/main/java/net/pantasystem/milktea/worker/emoji/cache/CacheCustomEmojiImageWorker.kt
@@ -1,9 +1,15 @@
 package net.pantasystem.milktea.worker.emoji.cache
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.hilt.work.HiltWorker
 import androidx.work.Constraints
 import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequest
 import androidx.work.PeriodicWorkRequestBuilder
@@ -18,6 +24,7 @@ import net.pantasystem.milktea.model.account.AccountRepository
 import net.pantasystem.milktea.model.emoji.CustomEmojiRepository
 import net.pantasystem.milktea.model.emoji.SaveCustomEmojiImageUseCase
 import net.pantasystem.milktea.model.image.ImageCacheRepository
+import net.pantasystem.milktea.worker.R
 import java.util.concurrent.TimeUnit
 
 @HiltWorker
@@ -32,6 +39,8 @@ class CacheCustomEmojiImageWorker @AssistedInject constructor(
 ) : CoroutineWorker(context, params) {
 
     companion object {
+        const val NOTIFICATION_CHANNEL_ID = "CACHE_CUSTOM_EMOJI_IMAGE_WORKER"
+
         fun createPeriodicWorkRequest(): PeriodicWorkRequest {
             val constraints = Constraints.Builder()
                 .setRequiredNetworkType(NetworkType.UNMETERED) // Wi-Fi (or Ethernet etc) required
@@ -49,6 +58,7 @@ class CacheCustomEmojiImageWorker @AssistedInject constructor(
 
     override suspend fun doWork(): Result {
         try {
+            setForeground(createForegroundInfo())
             val hosts = accountRepository.findAll().getOrThrow().map {
                 it.getHost()
             }.distinct()
@@ -82,5 +92,38 @@ class CacheCustomEmojiImageWorker @AssistedInject constructor(
             return Result.failure()
         }
     }
+
+    private fun createForegroundInfo(): ForegroundInfo {
+        val title = applicationContext.getString(R.string.notification_sync_download_custom_emoji_title)
+
+        // Create a Notification channel if necessary
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel()
+        }
+
+        val notification = NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle(title)
+            .setTicker(title)
+            .setSmallIcon(android.R.drawable.stat_sys_download)
+            .setOngoing(true)
+            .addAction(android.R.drawable.ic_delete, applicationContext.getString(android.R.string.cancel), null)
+            .build()
+
+        return ForegroundInfo(7, notification)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createChannel() {
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        // Create a Notification channel
+        if (notificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID) == null) {
+            val channel =
+                NotificationChannel(NOTIFICATION_CHANNEL_ID, "Custom emoji download notification", NotificationManager.IMPORTANCE_HIGH)
+            channel.description = "Notification representing custom emoji download"
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 
 }
