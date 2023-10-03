e5348b42373f291fb5e470aac3f56237284108ce
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-10-03 18:17:49 +0900

    feat: Intとして求められるようにした

--- modules/common/src/main/java/net/pantasystem/milktea/common/dhash/ImageDHash.kt
+++ modules/common/src/main/java/net/pantasystem/milktea/common/dhash/ImageDHash.kt
@@ -4,12 +4,11 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import androidx.core.graphics.drawable.toBitmapOrNull
-import java.math.BigInteger
 
 object ImageDHasher {
 
     private var WINDOW_SIZE = 8
-    fun hashFromBitmap(bitmap: Bitmap?): String? {
+    fun hashFromBitmap(bitmap: Bitmap?): Int? {
         bitmap ?: return null
         val scaledBitmap = Bitmap.createScaledBitmap(
             bitmap, WINDOW_SIZE + 1, WINDOW_SIZE + 1, true
@@ -23,22 +22,21 @@ object ImageDHasher {
 
         //clean up
         scaledBitmap.recycle()
-        return dHash?.toString()
+        return dHash
     }
 
-    fun hashFromDrawable(drawable: Drawable): String? {
+    fun hashFromDrawable(drawable: Drawable): Int? {
         val bitmap = drawable.toBitmapOrNull(WINDOW_SIZE + 1, WINDOW_SIZE + 1)
             ?: return null
         return hashFromBitmap(bitmap)
     }
 
-    fun hashFromPath(path: String?): String? {
+    fun hashFromPath(path: String?): Int? {
         val bmOptions = BitmapFactory.Options()
         bmOptions.inDither = false
         val bitmap = BitmapFactory.decodeFile(path, bmOptions)
         val scaledBitmap =
             Bitmap.createScaledBitmap(bitmap, WINDOW_SIZE + 1, WINDOW_SIZE + 1, true)
-                ?: return null
         val imageDimention = WINDOW_SIZE + 1
         val pixels = IntArray(imageDimention * imageDimention)
         scaledBitmap.getPixels(pixels, 0, imageDimention, 0, 0, imageDimention, imageDimention)
@@ -49,13 +47,13 @@ object ImageDHasher {
         //clean up
         bitmap.recycle()
         scaledBitmap.recycle()
-        return dHash?.toString()
+        return dHash
     }
 
-    private fun generateHash(pixels: IntArray?): BigInteger? {
+    private fun generateHash(pixels: IntArray?): Int? {
         var count = 0
-        var result = BigInteger("0")
-        if (pixels == null || pixels.size == 0) {
+        var result = 0
+        if (pixels == null || pixels.isEmpty()) {
             return null
         }
         for (y in 0 until WINDOW_SIZE) {
@@ -71,8 +69,8 @@ object ImageDHasher {
                 B = pixels[index] and 0xff
                 val greyRight = (R + G + B) / 3
                 if (greyLeft > greyRight) {
-                    var shifted = BigInteger("1")
-                    shifted = shifted.shiftLeft(count)
+                    var shifted = 1
+                    shifted = shifted shl count
                     result = result.or(shifted)
                 }
                 count++
