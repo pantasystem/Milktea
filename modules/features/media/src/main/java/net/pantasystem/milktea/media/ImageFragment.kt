0e62b9d75d80628302e51c33a571f3f5f1e61a20
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-10-07 23:12:57 +0900

    feat: 画像読み込み時にはプログレスを表示するようにした

--- modules/features/media/src/main/java/net/pantasystem/milktea/media/ImageFragment.kt
+++ modules/features/media/src/main/java/net/pantasystem/milktea/media/ImageFragment.kt
@@ -4,6 +4,7 @@ import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
@@ -91,6 +92,12 @@ class ImageFragment : Fragment(R.layout.fragment_image){
                     }
                 })
                 .into(binding.imageView)
+        }.flowWithLifecycle(lifecycle).launchIn(lifecycleScope)
+
+        viewModel.uiState.map {
+            it.isLoading
+        }.distinctUntilChanged().onEach {
+            binding.progressBar.isVisible = it
         }.flowWithLifecycle(lifecycle).launchIn(lifecycleScope)
     }
 
