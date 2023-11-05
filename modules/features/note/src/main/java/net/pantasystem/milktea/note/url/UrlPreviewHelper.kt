970d9307558a921a63e20cdefb90967ec4ed0eee
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-11-05 22:28:40 +0900

    feat: 画面遷移できるようにした

--- modules/features/note/src/main/java/net/pantasystem/milktea/note/url/UrlPreviewHelper.kt
+++ modules/features/note/src/main/java/net/pantasystem/milktea/note/url/UrlPreviewHelper.kt
@@ -1,14 +1,24 @@
 package net.pantasystem.milktea.note.url
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.android.internal.managers.FragmentComponentManager
+import net.pantasystem.milktea.common_android_ui.NavigationEntryPointForBinding
+import net.pantasystem.milktea.common_navigation.MediaNavigationArgs
 import net.pantasystem.milktea.note.R
 import net.pantasystem.milktea.note.databinding.ItemUrlOrFilePreviewBinding
 import net.pantasystem.milktea.note.viewmodel.Preview
@@ -39,6 +49,7 @@ object UrlPreviewHelper {
 
     }
 
+    @SuppressLint("IntentReset")
     @JvmStatic
     @BindingAdapter("previewList")
     fun LinearLayout.setUrlPreviewList(previewList: List<Preview>?) {
@@ -63,11 +74,43 @@ object UrlPreviewHelper {
                     view.urlPreviewView.isVisible = false
                     view.filePreviewView.isVisible = true
                     view.filePreviewView.setOtherFile(preview.file)
+
+                    if(preview.file.type.startsWith("audio")){
+                        val activity = FragmentComponentManager.findActivity(view.root.context)
+                        if (activity is Activity) {
+                            val accessor = EntryPointAccessors.fromActivity(activity, NavigationEntryPointForBinding::class.java)
+                            val intent = accessor.mediaNavigation().newIntent(
+                                MediaNavigationArgs.AFile(
+                                preview.file
+                            ))
+                            context?.startActivity(intent)
+                        }
+                    }else{
+                        try {
+                            context?.startActivity(
+                                Intent().apply{
+                                    data = Uri.parse(preview.file.path)
+                                    type = preview.file.type
+                                }
+                            )
+                        } catch (e: ActivityNotFoundException) {
+                            Toast.makeText(context, context.getString(net.pantasystem.milktea.common_resource.R.string.no_app_available_to_open_this_file), Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
                 }
                 is Preview.UrlWrapper -> {
                     view.urlPreviewView.isVisible = true
                     view.filePreviewView.isVisible = false
                     view.urlPreviewView.setUrlPreview(preview.urlPreview)
+
+                    view.urlPreviewView.setOnClickListener {
+                        context?.startActivity(
+                            Intent(Intent.ACTION_VIEW).apply{
+                                data = Uri.parse(preview.urlPreview.url)
+                            }
+                        )
+                    }
                 }
             }
 
