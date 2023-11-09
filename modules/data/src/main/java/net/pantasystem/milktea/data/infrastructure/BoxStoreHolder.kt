85dd93f4714d97b8b501fcc16cae05e038404f0b
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-11-09 13:23:44 +0900

    refactor: 不要なオブジェクトを削除

---
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/BoxStoreHolder.kt
@@ -0,0 +1,17 @@
+package net.pantasystem.milktea.data.infrastructure
+
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import io.objectbox.BoxStore
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class BoxStoreHolder @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+
+    val boxStore: BoxStore by lazy {
+        MyObjectBox.builder().androidContext(context).build()
+    }
+}
