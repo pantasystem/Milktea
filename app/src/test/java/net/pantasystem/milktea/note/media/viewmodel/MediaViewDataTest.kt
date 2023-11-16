f0b3874299c5e58626e50ba6998e7d5c81f7b5ab
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-11-16 17:10:29 +0900

    feat: 単体テストを追加した

--- app/src/test/java/net/pantasystem/milktea/note/media/viewmodel/MediaViewDataTest.kt
+++ app/src/test/java/net/pantasystem/milktea/note/media/viewmodel/MediaViewDataTest.kt
@@ -1,5 +1,6 @@
 package net.pantasystem.milktea.note.media.viewmodel
 
+import kotlinx.coroutines.test.runTest
 import net.pantasystem.milktea.model.drive.FileProperty
 import net.pantasystem.milktea.model.drive.make
 import net.pantasystem.milktea.model.file.AppFile
@@ -169,4 +170,140 @@ class MediaViewDataTest {
             value.map { it.initialVisibleType }
         )
     }
+
+    /**
+     * toggleVisibilityのテストコード
+     */
+    @Test
+    fun toggleVisibility_GiveConfigAsAuto() = runTest {
+        val config = DefaultConfig.config.copy(
+            mediaDisplayMode = MediaDisplayMode.AUTO
+        )
+        val files = listOf(
+            FilePreviewSource.Remote(
+                AppFile.Remote(id = FileProperty.Id(0L, "1")),
+                FileProperty.make(id = FileProperty.Id(0L, "1")),
+            ),
+            FilePreviewSource.Remote(
+                AppFile.Remote(id = FileProperty.Id(0L, "1")),
+                FileProperty.make(id = FileProperty.Id(0L, "1")),
+            ),
+            FilePreviewSource.Remote(
+                AppFile.Remote(id = FileProperty.Id(0L, "1")),
+                FileProperty.make(id = FileProperty.Id(0L, "1"), isSensitive = true),
+            ),
+            FilePreviewSource.Remote(
+                AppFile.Remote(id = FileProperty.Id(0L, "1")),
+                FileProperty.make(id = FileProperty.Id(0L, "1")),
+            )
+        )
+        val mediaViewData = MediaViewData(files, config)
+        mediaViewData.toggleVisibility(0)
+        Assertions.assertEquals(
+            listOf(
+                PreviewAbleFile.VisibleType.SensitiveHide,
+                PreviewAbleFile.VisibleType.Visible,
+                PreviewAbleFile.VisibleType.SensitiveHide,
+                PreviewAbleFile.VisibleType.Visible
+            ),
+            mediaViewData.files.value.map { it.visibleType }
+        )
+
+        mediaViewData.toggleVisibility(2)
+        Assertions.assertEquals(
+            listOf(
+                PreviewAbleFile.VisibleType.SensitiveHide,
+                PreviewAbleFile.VisibleType.Visible,
+                PreviewAbleFile.VisibleType.Visible,
+                PreviewAbleFile.VisibleType.Visible
+            ),
+            mediaViewData.files.value.map { it.visibleType }
+        )
+
+        mediaViewData.toggleVisibility(2)
+        Assertions.assertEquals(
+            listOf(
+                PreviewAbleFile.VisibleType.SensitiveHide,
+                PreviewAbleFile.VisibleType.Visible,
+                PreviewAbleFile.VisibleType.SensitiveHide,
+                PreviewAbleFile.VisibleType.Visible
+            ),
+            mediaViewData.files.value.map { it.visibleType }
+        )
+
+    }
+
+    /**
+     * toggleVisibilityのテストコード、modeがALWAYS_HIDE_WHEN_MOBILE_NETWORKの時
+     */
+    @Test
+    fun toggleVisibility_GiveConfigAsAlwaysHideWhenMobileNetwork() = runTest {
+        val config = DefaultConfig.config.copy(
+            mediaDisplayMode = MediaDisplayMode.ALWAYS_HIDE_WHEN_MOBILE_NETWORK
+        )
+        val files = listOf(
+            FilePreviewSource.Remote(
+                AppFile.Remote(id = FileProperty.Id(0L, "1")),
+                FileProperty.make(
+                    id = FileProperty.Id(0L, "1"),
+                    isSensitive = true
+                ),
+            ),
+            FilePreviewSource.Remote(
+                AppFile.Remote(id = FileProperty.Id(0L, "1")),
+                FileProperty.make(
+                    id = FileProperty.Id(0L, "1"),
+                    isSensitive = false
+                ),
+            ),
+            FilePreviewSource.Remote(
+                AppFile.Remote(id = FileProperty.Id(0L, "1")),
+                FileProperty.make(
+                    id = FileProperty.Id(0L, "1"),
+                    isSensitive = false
+                ),
+            ),
+            FilePreviewSource.Remote(
+                AppFile.Remote(id = FileProperty.Id(0L, "1")),
+                FileProperty.make(
+                    id = FileProperty.Id(0L, "1"),
+                    isSensitive = false
+                ),
+            )
+        )
+        val mediaViewData = MediaViewData(files, config)
+        mediaViewData.toggleVisibility(0)
+        Assertions.assertEquals(
+            listOf(
+                PreviewAbleFile.VisibleType.Visible,
+                PreviewAbleFile.VisibleType.HideWhenMobileNetwork,
+                PreviewAbleFile.VisibleType.HideWhenMobileNetwork,
+                PreviewAbleFile.VisibleType.HideWhenMobileNetwork
+            ),
+            mediaViewData.files.value.map { it.visibleType }
+        )
+
+        mediaViewData.toggleVisibility(1)
+        Assertions.assertEquals(
+            listOf(
+                PreviewAbleFile.VisibleType.Visible,
+                PreviewAbleFile.VisibleType.Visible,
+                PreviewAbleFile.VisibleType.HideWhenMobileNetwork,
+                PreviewAbleFile.VisibleType.HideWhenMobileNetwork
+            ),
+            mediaViewData.files.value.map { it.visibleType }
+        )
+        mediaViewData.toggleVisibility(1)
+        Assertions.assertEquals(
+            listOf(
+                PreviewAbleFile.VisibleType.Visible,
+                PreviewAbleFile.VisibleType.SensitiveHide,
+                PreviewAbleFile.VisibleType.HideWhenMobileNetwork,
+                PreviewAbleFile.VisibleType.HideWhenMobileNetwork
+            ),
+            mediaViewData.files.value.map { it.visibleType }
+        )
+
+    }
+
 }
