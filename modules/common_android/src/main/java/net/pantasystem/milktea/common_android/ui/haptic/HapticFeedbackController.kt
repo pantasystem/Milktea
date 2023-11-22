6679499341c22835b86f7fa16acdb3d33a5e385f
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-11-22 18:29:35 +0900

    feat: 感覚フィードバッククラスを効率的に扱うためのControllerクラスを作成

---
+++ modules/common_android/src/main/java/net/pantasystem/milktea/common_android/ui/haptic/HapticFeedbackController.kt
@@ -0,0 +1,29 @@
+package net.pantasystem.milktea.common_android.ui.haptic
+
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.view.HapticFeedbackConstants
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+object HapticFeedbackController {
+
+    fun performClickHapticFeedback(view: View) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            view.performHapticFeedback(HapticFeedbackConstants.CONTEXT_CLICK)
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    fun performTickVibrateHapticFeedback(context: Context) {
+        val vibratorManager =
+            context.getSystemService(AppCompatActivity.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK).let {
+                vibratorManager.vibrate(it)
+            }
+        }
+    }
+}
