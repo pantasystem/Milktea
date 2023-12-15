54d49ad7c8336fa4e2424a9e5895f9e2023a5fae
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-12-15 17:10:59 +0900

    feat: Result型を返すようにした

--- modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/image/ImageCacheRepositoryImpl.kt
+++ modules/data/src/main/java/net/pantasystem/milktea/data/infrastructure/image/ImageCacheRepositoryImpl.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.withContext
 import kotlinx.datetime.Clock
 import net.pantasystem.milktea.api.misskey.OkHttpClientProvider
 import net.pantasystem.milktea.common.Hash
+import net.pantasystem.milktea.common.runCancellableCatching
 import net.pantasystem.milktea.common_android.hilt.IODispatcher
 import net.pantasystem.milktea.data.infrastructure.BoxStoreHolder
 import net.pantasystem.milktea.model.image.ImageCache
@@ -38,16 +39,16 @@ class ImageCacheRepositoryImpl @Inject constructor(
         boxStoreHolder.boxStore.boxFor(ImageCacheRecord::class.java)
     }
 
-    override suspend fun save(url: String): ImageCache {
-        when (val cache = findBySourceUrl(url)) {
+    override suspend fun save(url: String) = runCancellableCatching {
+        when (val cache = findBySourceUrl(url).getOrThrow()) {
             null -> Unit
             else -> if (cache.cachedAt + cacheIgnoreUpdateDuration > Clock.System.now()) {
                 if (File(cache.cachePath).exists()) {
-                    return cache
+                    return@runCancellableCatching cache
                 }
             }
         }
-        return withContext(coroutineDispatcher) {
+        withContext(coroutineDispatcher) {
             val fileName = Hash.sha256(url)
             val file = File(context.filesDir, cacheDir).apply {
                 if (!exists()) {
@@ -69,8 +70,8 @@ class ImageCacheRepositoryImpl @Inject constructor(
         }
     }
 
-    override suspend fun findBySourceUrl(url: String): ImageCache? {
-        return withContext(coroutineDispatcher) {
+    override suspend fun findBySourceUrl(url: String): Result<ImageCache?> = runCancellableCatching {
+        withContext(coroutineDispatcher) {
             val now = Clock.System.now()
             val record = imageCacheStore.query().equal(
                 ImageCacheRecord_.sourceUrl,
@@ -87,7 +88,7 @@ class ImageCacheRepositoryImpl @Inject constructor(
         }
     }
 
-    override suspend fun deleteExpiredCaches() {
+    override suspend fun deleteExpiredCaches(): Result<Unit> = runCancellableCatching {
         withContext(coroutineDispatcher) {
             val now = Clock.System.now()
             val query = imageCacheStore.query().lessOrEqual(
@@ -110,15 +111,15 @@ class ImageCacheRepositoryImpl @Inject constructor(
         }
     }
 
-    override suspend fun clear() {
+    override suspend fun clear(): Result<Unit> = runCancellableCatching {
         withContext(coroutineDispatcher) {
             imageCacheStore.removeAll()
             File(context.filesDir, cacheDir).deleteRecursively()
         }
     }
 
-    override suspend fun findBySourceUrls(urls: List<String>): List<ImageCache> {
-        return withContext(coroutineDispatcher) {
+    override suspend fun findBySourceUrls(urls: List<String>): Result<List<ImageCache>> = runCancellableCatching {
+        withContext(coroutineDispatcher) {
             val now = Clock.System.now()
             urls.chunked(100) { urls ->
                 val records = imageCacheStore.query().inValues(
@@ -178,8 +179,8 @@ class ImageCacheRepositoryImpl @Inject constructor(
         }
     }
 
-    override suspend fun findCachedFileCount(reality: Boolean): Long {
-        return if (reality) {
+    override suspend fun findCachedFileCount(reality: Boolean): Result<Long> = runCancellableCatching {
+        if (reality) {
             val dir = File(context.filesDir, cacheDir).apply {
                 if (!exists()) {
                     mkdirs()
--- modules/model/src/main/java/net/pantasystem/milktea/model/image/ImageCacheRepository.kt
+++ modules/model/src/main/java/net/pantasystem/milktea/model/image/ImageCacheRepository.kt
@@ -1,12 +1,12 @@
 package net.pantasystem.milktea.model.image
 
 interface ImageCacheRepository {
-    suspend fun save(url: String): ImageCache
-    suspend fun findBySourceUrl(url: String): ImageCache?
-    suspend fun deleteExpiredCaches()
-    suspend fun clear()
+    suspend fun save(url: String): Result<ImageCache>
+    suspend fun findBySourceUrl(url: String): Result<ImageCache?>
+    suspend fun deleteExpiredCaches(): Result<Unit>
+    suspend fun clear(): Result<Unit>
 
-    suspend fun findBySourceUrls(urls: List<String>): List<ImageCache>
+    suspend fun findBySourceUrls(urls: List<String>): Result<List<ImageCache>>
 
-    suspend fun findCachedFileCount(reality: Boolean = false): Long
+    suspend fun findCachedFileCount(reality: Boolean = false): Result<Long>
 }
