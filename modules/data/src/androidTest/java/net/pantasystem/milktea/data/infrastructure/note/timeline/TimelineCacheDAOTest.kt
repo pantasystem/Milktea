3d072bea658d35ab5913e0145288612e44e6533e
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2024-09-18 00:00:02 +0900

    feat: テストを追加した

--- modules/data/src/androidTest/java/net/pantasystem/milktea/data/infrastructure/note/timeline/TimelineCacheDAOTest.kt
+++ modules/data/src/androidTest/java/net/pantasystem/milktea/data/infrastructure/note/timeline/TimelineCacheDAOTest.kt
@@ -89,6 +89,118 @@ class TimelineCacheDAOTest {
         )
     }
 
+    @Test
+    fun getTimelineItemsUntilId() = runTest {
+        val accountId = 1L
+        val pageId = 1L
+        timelineCacheDAO.clear(accountId, pageId)
+        val testData = (1 until 1000).map {
+            val noteId = it.toString().padStart(4, '0')
+            Assert.assertEquals(4, noteId.length)
+            TimelineItemEntity(
+                accountId = accountId,
+                pageId = pageId,
+                noteId = noteId,
+                noteLocalId = NoteEntity.makeEntityId(
+                    Note.Id(accountId, noteId)
+                ),
+                id = 0
+            )
+        }
+        noteDAO.insertAll(
+            testData.map {
+                NoteEntity.fromModel(Note.make(
+                    Note.Id(it.accountId, it.noteId),
+                    User.Id(it.accountId, "user")
+                ))
+            }
+        )
+        timelineCacheDAO.insertAll(testData)
+        Assert.assertEquals(
+            testData.size.toLong(), noteDAO.count()
+        )
+        Assert.assertEquals(
+            testData.size.toLong(),
+            timelineCacheDAO.count()
+        )
+        val items = timelineCacheDAO.getTimelineItemsUntilId(
+            accountId = accountId,
+            pageId = pageId,
+            untilId = "0400",
+            limit = 10,
+        )
+        Assert.assertTrue(items.isNotEmpty())
+        Assert.assertEquals(
+            "0399",
+            items[0].noteId,
+        )
+        Assert.assertEquals(
+            "0398",
+            items[1].noteId,
+        )
+        println(items)
+        Assert.assertEquals(
+            "0390",
+            items[9].noteId,
+        )
+    }
+
+    @Test
+    fun getTimelineItems() = runTest {
+        val accountId = 1L
+        val pageId = 1L
+        timelineCacheDAO.clear(accountId, pageId)
+        val testData = (1 until 1000).map {
+            val noteId = it.toString().padStart(4, '0')
+            Assert.assertEquals(4, noteId.length)
+            TimelineItemEntity(
+                accountId = accountId,
+                pageId = pageId,
+                noteId = noteId,
+                noteLocalId = NoteEntity.makeEntityId(
+                    Note.Id(accountId, noteId)
+                ),
+                id = 0
+            )
+        }
+        noteDAO.insertAll(
+            testData.map {
+                NoteEntity.fromModel(Note.make(
+                    Note.Id(it.accountId, it.noteId),
+                    User.Id(it.accountId, "user")
+                ))
+            }
+        )
+        timelineCacheDAO.insertAll(testData)
+        Assert.assertEquals(
+            testData.size.toLong(), noteDAO.count()
+        )
+        Assert.assertEquals(
+            testData.size.toLong(),
+            timelineCacheDAO.count()
+        )
+        val items = timelineCacheDAO.getTimelineItems(
+            accountId = accountId,
+            pageId = pageId,
+            limit = 10,
+        )
+        Assert.assertTrue(items.isNotEmpty())
+        Assert.assertEquals(
+            "0999",
+            items[0].noteId,
+        )
+        Assert.assertEquals(
+            "0998",
+            items[1].noteId,
+        )
+        println(items)
+        Assert.assertEquals(
+            "0990",
+            items[9].noteId,
+        )
+    }
+
+
     @After
     fun after() {
         db.close()
